8ea9e1ef7fca9f0e4cb918b34906f1bdffa7c9cb
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-08-16 23:19:09 +0600

    add Navbar component basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Header/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      <div className="rectangle" />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
           rel="noopener noreferrer">
           Learn React!
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
