f58818cb805d89eaae72df02cde4a50ab4ed908e
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-08-26 18:00:02 +0600

    add Main component description and tags styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,27 +7,26 @@ export const Main: React.FC = () => {
     <div className="main">
       <div className="title-container">
         <div className="title">The Rolling Scopes</div>
+
         <div className="title-description">
           <div>an international community of developers</div>
           <div>since 2013</div>
         </div>
       </div>
 
-      <div className="connecting-people-growing-together-having-fun">
-        Connecting people, growing together, having fun
-      </div>
-      <div>
+      <div className="description">Connecting people, growing together, having fun</div>
+      <div className="tags-container">
         <div className="tag">
           <span className="label">education</span>
         </div>
         <div className="asterix">
-          <span className="asterix_label">*</span>
+          <span className="label">*</span>
         </div>
         <div className="tag">
           <span className="label">events & meetups</span>
         </div>
         <div className="asterix">
-          <span className="asterix_label">*</span>
+          <span className="label">*</span>
         </div>
         <div className="tag">
           <span className="label">community building</span>
