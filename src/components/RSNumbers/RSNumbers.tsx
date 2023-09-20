84d94d7cb0ea6e0f7c4ac6052bcf246bdc3a58a6
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-09-20 12:41:18 +0300

    update RSNumbers component layout and styles

--- src/components/RSNumbers/RSNumbers.tsx
+++ src/components/RSNumbers/RSNumbers.tsx
@@ -3,10 +3,28 @@ import image from '../../assets/map.png';
 
 import './RSNumbers.css';
 
+interface InfoCellProps {
+  title: string;
+  description: string;
+}
+
+const InfoCell: React.FC<InfoCellProps> = ({ title, description }) => (
+  <div className="info-container">
+    <div>{title}</div>
+    <div>{description}</div>
+  </div>
+);
+
+const InfoCellDivider: React.FC = () => (
+  <svg width="3" height="182" viewBox="-1 -1 3 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="0.5" y1="180" x2="0.499984" y2="4.37255e-08" stroke="#E2E2E2"></line>
+  </svg>
+);
+
 export const RSNumbers: React.FC = () => {
   return (
     <div className="numbers">
-      <div className="container">
+      <div className="text-container">
         <div />
         <div>
           <div className="title">The Rolling Scopes in numbers</div>
@@ -19,6 +37,15 @@ export const RSNumbers: React.FC = () => {
             knowledge, enrich your network and just have fun.
           </div>
         </div>
+      </div>
+      <div className="info">
+        <InfoCell title="62k+" description="members" />
+        <InfoCellDivider />
+        <InfoCell title="500+" description="events" />
+        <InfoCellDivider />
+        <InfoCell title="600+" description="videos on YouTube" />
+        <InfoCellDivider />
+        <InfoCell title="1800+" description="RS School alumni per year" />
       </div>
       <img className="map" src={image} alt="map" />
     </div>
