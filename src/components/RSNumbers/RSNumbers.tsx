98f5b5870462920c6b90a184fefd0a65abd6ebdc
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-09-27 22:43:02 +0300

    add responsive styles to RSNumbers component

--- src/components/RSNumbers/RSNumbers.tsx
+++ src/components/RSNumbers/RSNumbers.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import image from '../../assets/map.png';
+import { Paragraph } from '../Paragraph/Paragraph';
+import { Subtitle } from '../Subtitle/Subtitle';
+import { Title } from '../Title/Title';
 
 import './RSNumbers.scss';
 
@@ -9,43 +12,42 @@ interface InfoCellProps {
 }
 
 const InfoCell: React.FC<InfoCellProps> = ({ title, description }) => (
-  <div className="info-container">
-    <div>{title}</div>
-    <div>{description}</div>
+  <div className="info-cell">
+    <div className="number">{title}</div>
+    <div className="text">{description}</div>
   </div>
 );
 
-const InfoCellDivider: React.FC = () => (
-  <svg width="3" height="182" viewBox="-1 -1 3 182" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <line x1="0.5" y1="180" x2="0.499984" y2="4.37255e-08" stroke="#E2E2E2"></line>
-  </svg>
-);
+const InfoCellDivider: React.FC = () => <div className="info-divider" />;
 
 export const RSNumbers: React.FC = () => {
   return (
-    <div className="numbers">
+    <div className="numbers container">
       <div className="text-container">
-        <div />
         <div>
-          <div className="title">The Rolling Scopes in numbers</div>
-          <div className="subtitle">
-            Everyone can discover our community, regardless of age, professional employment, or
-            place of residence.
-          </div>
-          <div className="description">
+          <Title text="The Rolling Scopes in numbers" asterix />
+          <Subtitle
+            text="Everyone can discover our community, regardless of age, professional employment, or
+            place of residence."
+          />
+          <Paragraph>
             Developers from different companies and countries are connected to pass on your
             knowledge, enrich your network and just have fun.
-          </div>
+          </Paragraph>
         </div>
       </div>
       <div className="info">
-        <InfoCell title="62k+" description="members" />
-        <InfoCellDivider />
-        <InfoCell title="500+" description="events" />
-        <InfoCellDivider />
-        <InfoCell title="600+" description="videos on YouTube" />
-        <InfoCellDivider />
-        <InfoCell title="1800+" description="RS School alumni per year" />
+        <div className="left">
+          <InfoCell title="62k+" description="members" />
+          <InfoCellDivider />
+          <InfoCell title="500+" description="events" />
+          <InfoCellDivider />
+        </div>
+        <div className="right">
+          <InfoCell title="600+" description="videos on YouTube" />
+          <InfoCellDivider />
+          <InfoCell title="1800+" description="RS School alumni per year" />
+        </div>
       </div>
       <img className="map" src={image} alt="map" />
     </div>
