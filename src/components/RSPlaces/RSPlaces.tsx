48fe2967021948d259c6993363766abd02cdb0cd
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-09-28 14:34:34 +0300

    update RSPlacec component, add responsive styles

--- src/components/RSPlaces/RSPlaces.tsx
+++ src/components/RSPlaces/RSPlaces.tsx
@@ -1,32 +1,30 @@
 import React from 'react';
 
-import './RSPlaces.css';
+import './RSPlaces.scss';
+
+const places = [
+  'Kazakhstan',
+  'Belarus',
+  'Latvia',
+  'Poland',
+  'Turkey',
+  'Georgia',
+  'Montenegro',
+  'Uzbekistan',
+  'Online',
+  'Kyrgyzstan',
+  'Lithuania'
+];
 
 export const RSPlaces: React.FC = () => {
   return (
     <div className="places">
-      <div className="divider">*</div>
-      <div className="place">Kazakhstan</div>
-      <div className="divider">*</div>
-      <div className="place">Belarus</div>
-      <div className="divider">*</div>
-      <div className="place">Latvia</div>
-      <div className="divider">*</div>
-      <div className="place">Poland</div>
-      <div className="divider">*</div>
-      <div className="place">Turkey</div>
-      <div className="divider">*</div>
-      <div className="place">Georgia</div>
-      <div className="divider">*</div>
-      <div className="place">Montenegro</div>
-      <div className="divider">*</div>
-      <div className="place">Uzbekistan</div>
-      <div className="divider">*</div>
-      <div className="place">Online</div>
-      <div className="divider">*</div>
-      <div className="place">Kyrgyzstan</div>
-      <div className="divider">*</div>
-      <div className="place">Lithuania</div>
+      {places.map((i) => (
+        <>
+          <div className="divider">*</div>
+          <div className="place">{i}</div>
+        </>
+      ))}
     </div>
   );
 };
