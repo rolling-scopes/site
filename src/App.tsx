6de6cee6f038bff2dd7f2be7c94f087675c28e5b
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-10-11 22:32:53 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import { Navbar } from './components/Header/Navbar';
-import { Main } from './components/Main/Main';
-import { About } from './components/About/About';
-import { RSNumbers } from './components/RSNumbers/RSNumbers';
-import { RSPlaces } from './components/RSPlaces/RSPlaces';
-import { RSPictures } from './components/RSPictures/RSPictures';
-import { RSSchool } from './components/RSSchool/RSSchool';
-import { RSSchoolPrinciples } from './components/RSSchoolPrinciples/RSSchoolPrinciples';
-import { OurAlumni } from './components/OurAlumni/OurAlumni';
-import { Events } from './components/Events/Events';
-import { Speakers } from './components/Speakers/Speakers';
-import { Merch } from './components/Merch/Merch';
-import { Community } from './components/RSCommunity/RSCommunity';
-import { Contribute } from './components/Contribute/Contribute';
-import { Support } from './components/Support/Support';
-import { Partnered } from './components/Partnered/Partnered';
-import { Footer } from './components/Footer/Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Home } from './pages';
 
 import './App.scss';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Main />
-      <About />
-      <RSNumbers />
-      <RSPlaces />
-      <RSPictures />
-      <RSSchool />
-      <RSSchoolPrinciples />
-      <OurAlumni />
-      <Events />
-      <Speakers />
-      <Merch />
-      <Community />
-      <Contribute />
-      <Support />
-      <Partnered />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Navbar } from '../components/Header/Navbar';
+import { Main } from '../components/Main/Main';
+
+export const Home: React.FC = () => (
+  <div className="App">
+    <Navbar />
+    <Main />
+    {/* <About />
+  <RSNumbers />
+  <RSPlaces />
+  <RSPictures />
+  <RSSchool />
+  <RSSchoolPrinciples />
+  <OurAlumni />
+  <Events />
+  <Speakers />
+  <Merch />
+  <Community />
+  <Contribute />
+  <Support />
+  <Partnered />
+  <Footer /> */}
+  </div>
+);
