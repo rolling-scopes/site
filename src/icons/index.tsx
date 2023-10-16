e98a3f89ec6cc82ac47670ffcbea21b97f2635b8
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-10-16 23:31:30 +0300

    add Principles icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -3,3 +3,6 @@ export * from './Instagram';
 export * from './LinkedIn';
 export * from './Telegram';
 export * from './YouTube';
+export * from './OpenToEveryoneIcon';
+export * from './OpenSourcePhilosophyIcon';
+export * from './TeachItForwardIcon';
