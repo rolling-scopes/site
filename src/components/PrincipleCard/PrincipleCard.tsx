d0cd356c4f813dc4db59c7a44a136e4b396756a2
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2023-10-20 17:16:20 +0300

    update PrincipleCard component

--- src/components/PrincipleCard/PrincipleCard.tsx
+++ src/components/PrincipleCard/PrincipleCard.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useWindowSize } from '../../hooks';
 import './PrincipleCard.scss';
 
 export interface PrincipleCardProps {
-  text: string;
+  title: string;
+  description: string;
   Icon: React.FC;
 }
 
-export const PrincipleCard: React.FC<PrincipleCardProps> = ({ text, Icon }: PrincipleCardProps) => (
-  <div className="principle-card">
-    <div className="accent" />
-    <Icon />
-    <div className="text">{text}</div>
-    <div className="accent-corner" />
-  </div>
-);
+export const PrincipleCard: React.FC<PrincipleCardProps> = ({
+  title,
+  description,
+  Icon
+}: PrincipleCardProps) => {
+  const [hovered, setHovered] = useState(false);
+
+  const size = useWindowSize();
+
+  const toggleHover = () => setHovered(!hovered);
+
+  return (
+    <div className="principle-card" onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+      <div className="accent" />
+      <Icon />
+      {size.width <= 810 ? (
+        <>
+          <div className="card-title">{title}</div>
+          <div className="card-description">{description}</div>
+        </>
+      ) : (
+        <>
+          {!hovered && <div className="card-title">{title}</div>}
+          {hovered && <div className="card-description">{description}</div>}
+        </>
+      )}
+
+      <div className="accent-corner" />
+    </div>
+  );
+};
