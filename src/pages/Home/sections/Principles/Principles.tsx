cde74e8803c95d39a427f62fb23217f5ce9087f1
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2023-11-30 12:53:43 +0300

    fix: add principle card animation

--- src/pages/Home/sections/Principles/Principles.tsx
+++ src/pages/Home/sections/Principles/Principles.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Title, PrincipleCard, PrincipleCardProps, TitleType } from '../../../../components';
 
 import {
@@ -14,23 +13,23 @@ const cards: PrincipleCardProps[] = [
     title: 'Open to everyone',
     description:
       'Free courses, no obligations, and no contracts. No age limit. Only students’ time and dedication are required. Students can repeatedly attend courses.',
-    Icon: OpenToEveryoneIcon
+    icon: <OpenToEveryoneIcon />
   },
   {
     title: 'Open source philosophy',
     description:
       'Our Learning Management System platform and educational materials are publicly available on GitHub and YouTube.',
-    Icon: OpenSourcePhilosophyIcon
+    icon: <OpenSourcePhilosophyIcon />
   },
   {
     title: '"Teach it forward"',
     description:
       'Students study at school for free, but we request that they return as mentors to pass on their knowledge to the next generation of students.',
-    Icon: TeachItForwardIcon
+    icon: <TeachItForwardIcon />
   }
 ];
 
-export const Principles: React.FC = () => (
+export const Principles = () => (
   <div className="principles container">
     <div className="principles content">
       <Title
@@ -40,8 +39,8 @@ export const Principles: React.FC = () => (
         hasLines={true}
       />
       <div className="cards column-3 ">
-        {cards.map((i) => (
-          <PrincipleCard key={i.title} {...i} />
+        {cards.map(({ title, description, icon }) => (
+          <PrincipleCard key={title} description={description} icon={icon} title={title} />
         ))}
       </div>
     </div>
