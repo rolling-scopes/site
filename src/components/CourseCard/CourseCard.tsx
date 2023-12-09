27beb0f5915e77bbeff2f1e3fd618520be0cf937
Kubatbekkk <kubineccastro@gmail.com>
2023-12-09 19:02:22 +0600

    fix: course-card to rs-course-card cuz of conflict

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -4,8 +4,8 @@ import noteIcon from '../../icons/noteIcon.svg';
 import micIcon from '../../icons/mic.svg';
 
 type CourseCardProps = {
-  title?: string;
-  iconSrc?: string;
+  title: string;
+  iconSrc: string;
   startDate: string;
   mode: string;
   language: ('en' | 'ru')[];
@@ -30,25 +30,25 @@ export const CourseCard: React.FC<CourseCardProps> = ({
   };
 
   return (
-    <div className="course-card">
-      <div className="course-card__top" style={cardStyle}>
+    <div className="rs-course-card">
+      <div className="rs-course-card__top" style={cardStyle}>
         <img src={iconSrc} alt={title} />
-        <h3 className="course-card__title">{title}</h3>
+        <h3 className="rs-course-card__title">{title}</h3>
       </div>
-      <div className="course-card__bottom">
-        <div className="course-card__left">
-          <div className="course-card__date-info">
-            <img src={noteIcon} alt="note-icon" className="course-card__note-icon" />
-            <div className="course-card__date">Start {startDate}</div>
+      <div className="rs-course-card__bottom">
+        <div className="rs-course-card__left">
+          <div className="rs-course-card__date-info">
+            <img src={noteIcon} alt="note-icon" className="rs-course-card__note-icon" />
+            <div className="rs-course-card__date">Start {startDate}</div>
           </div>
-          <div className="course-card__lang-info">
-            <img src={micIcon} className="course-card__mic-icon" />
-            <div className="course-card__lang">{lang}</div>
-            <div className="course-card__mode">• {mode}</div>
+          <div className="rs-course-card__lang-info">
+            <img src={micIcon} className="rs-course-card__mic-icon" />
+            <div className="rs-course-card__lang">{lang}</div>
+            <div className="rs-course-card__mode">• {mode}</div>
           </div>
         </div>
-        <div className="course-card__right">
-          <a href={detailsUrl} className="course-card__more">
+        <div className="rs-course-card__right">
+          <a href={detailsUrl} className="rs-course-card__more">
             View details<span className="material-symbols-outlined">arrow_forward</span>
           </a>
         </div>
