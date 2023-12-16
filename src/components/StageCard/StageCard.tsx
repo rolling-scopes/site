d88b060db065e9f603fe08cbdeca8179f6347a06
Kubatbekkk <kubineccastro@gmail.com>
2023-12-16 08:56:39 +0600

    refactor: move StageCard from StudyPath.tsx to /components

---
+++ src/components/StageCard/StageCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './StageCard.scss';
+
+type StagecardProps = {
+  id: number;
+  title: string;
+  description: string;
+  logoIcon: string;
+  links: { href: string; linkTitle: string; isActive?: boolean }[];
+};
+const StageCard: React.FC<StagecardProps> = ({ id, title, description, logoIcon, links }) => {
+  return (
+    <div key={id} className="stage">
+      <div className="stage-step">
+        <span className="stage-number">{id}</span>
+        <div className="stage-line" />
+      </div>
+      <div className="stage-info">
+        <h2 className="stage-title">{title}</h2>
+        <p className="stage-description">{description}</p>
+        <p className="stage-links">
+          {links.map(({ href, linkTitle, isActive = true }) => (
+            <a href={href} key={linkTitle} className={`${isActive || 'disabled'}`} target="blank">
+              {linkTitle}
+            </a>
+          ))}
+        </p>
+      </div>
+      <div className="stage-logo">
+        <img src={logoIcon} alt={title} />
+      </div>
+    </div>
+  );
+};
+
+export default StageCard;
--- src/pages/Courses/sections/StudyPath/StudyPath.tsx
+++ src/pages/Courses/sections/StudyPath/StudyPath.tsx
@@ -1,4 +1,5 @@
 import { Paragraph, Title } from '../../../../components';
+import StageCard from '../../../../components/StageCard/StageCard';
 import { pathCoursesList } from '../coursesData';
 
 import './StudyPath.scss';
@@ -16,31 +17,8 @@ const StudyPath = () => {
           faster product development.
         </Paragraph>
         <div className="stages">
-          {pathCoursesList.map(({ id, title, description, logoIcon, links }) => (
-            <div key={id} className="stage">
-              <div className="stage-step">
-                <span className="stage-number">{id}</span>
-                <div className="stage-line" />
-              </div>
-              <div className="stage-info">
-                <h2 className="stage-title">{title}</h2>
-                <p className="stage-description">{description}</p>
-                <p className="stage-links">
-                  {links.map(({ href, linkTitle, isActive = true }) => (
-                    <a
-                      href={href}
-                      key={linkTitle}
-                      className={`${isActive || 'disabled'}`}
-                      target="blank">
-                      {linkTitle}
-                    </a>
-                  ))}
-                </p>
-              </div>
-              <div className="stage-logo">
-                <img src={logoIcon} alt={title} />
-              </div>
-            </div>
+          {pathCoursesList.map((course) => (
+            <StageCard key={course.id} {...course} />
           ))}
         </div>
       </div>
