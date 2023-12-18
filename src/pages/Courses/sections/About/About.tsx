5955ff4b41c1fa5f9bc28401679bca79d27b6c67
Kubatbekkk <kubineccastro@gmail.com>
2023-12-18 13:34:31 +0600

    fix: About section layout

--- src/pages/Courses/sections/About/About.tsx
+++ src/pages/Courses/sections/About/About.tsx
@@ -7,28 +7,34 @@ export const About = () => {
   const [course] = coursesData.filter((course: { title: string }) =>
     course.title.startsWith('React')
   );
+
   const crumbs = [
     { label: 'Home', path: '/' },
     { label: 'RS School', path: '/rs-courses' }
   ];
+
   return (
-    <div className="about__container container">
+    <>
       <Breadcrumbs crumbs={crumbs} />
-      <div className="about__content">
-        <div className="about__card-wrapper">
-          <CourseCard {...course} />
-        </div>
-        <div className="about__text ">
-          <Title text="About RS School" hasAsterisk />
-          <Subtitle text="No matter your age, professional employment, or place of residence." />
-          <Paragraph>
-            RS School offers a unique learning experience as a free, community-based online
-            education initiative. The RS School has been run by the Rolling Scopes community since
-            2013. Today, over 600 developer-volunteers from various countries and companies assist
-            as mentors. We believe in important ideas that guide our mission.
-          </Paragraph>
+      <div className="rs-about container">
+        <div className="rs-about content">
+          <div className="column-2">
+            <div className="card-wrapper">
+              <CourseCard {...course} />
+            </div>
+            <div className="info">
+              <Title text="About RS School" hasAsterisk />
+              <Subtitle text="No matter your age, professional employment, or place of residence." />
+              <Paragraph>
+                RS School offers a unique learning experience as a free, community-based online
+                education initiative. The RS School has been run by the Rolling Scopes community
+                since 2013. Today, over 600 developer-volunteers from various countries and
+                companies assist as mentors. We believe in important ideas that guide our mission.
+              </Paragraph>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
