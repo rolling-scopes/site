2215eb63d03bc55c730b19b2d4ccf822ace15511
Kubatbekkk <kubineccastro@gmail.com>
2023-12-29 11:56:52 +0600

    feat: add useTitle to change document.title

--- src/pages/Courses/CoursesPage.tsx
+++ src/pages/Courses/CoursesPage.tsx
@@ -11,20 +11,24 @@ import {
   Mentoring,
   Requirements
 } from './sections';
+import { useTitle } from '../../hooks';
 
-export const CoursesPage: React.FC = () => (
-  <div className="App">
-    <Navbar />
-    <Main />
-    <About />
-    <Principles />
-    <Courses />
-    <StudyPath />
-    <General />
-    <Mentors />
-    <Mentoring />
-    <Requirements />
-    <Partnered />
-    <Footer />
-  </div>
-);
+export const CoursesPage: React.FC = () => {
+  useTitle('Courses');
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+      <About />
+      <Principles />
+      <Courses />
+      <StudyPath />
+      <General />
+      <Mentors />
+      <Mentoring />
+      <Requirements />
+      <Partnered />
+      <Footer />
+    </div>
+  );
+};
