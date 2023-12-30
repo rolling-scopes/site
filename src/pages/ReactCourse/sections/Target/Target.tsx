46a12d05b2260f0598aed4d60a1648f9e51a3a71
Yaroslav <y.silkin@netrika.ru>
2023-12-30 08:51:26 +0300

    feat: target section

--- src/pages/ReactCourse/sections/Target/Target.tsx
+++ src/pages/ReactCourse/sections/Target/Target.tsx
@@ -1,38 +1,40 @@
 import React from 'react';
-import { Title } from '../../../../components';
+import { Button, Paragraph, Title } from '../../../../components';
+
+import image from '../../../../assets/about.png';
 
 export const Target = () => {
   return (
     <div className="rs-target container">
-      <div className="rs-target content">
-        <Title text="Target audience" hasAsterisk />
-        <div>
-          Students of the RS School from the 2022Q3, which has passed RS School Stage #2 as well as
-          new students with practical experience and knowledge of:
-          <ul>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>
-              Git, GitHub (clone, add, commit, push, pull, merge, rebase, working with Pull Request)
-            </li>
-            <li>NPM, Webpack</li>
-            <li>CSS3 / HTML5</li>
-            <li>Chrome DevTools, Figma</li>
-            <li>Understanding of the REST</li>
-          </ul>
-        </div>
-        <div>
-          Attention! Mentors on this course will be first assigned to the graduates of the RS School
-          Stage #2.
-        </div>
+      <div className="rs-target content column-2">
+        <div className="left description">
+          <Title text="Target audience" hasAsterisk />
+          <Paragraph>
+            <div>
+              Students of the RS School from the 2022Q3, which has passed RS School Stage #2 as well
+              as new students with practical experience and knowledge of:
+            </div>
+            <ul>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>
+                Git, GitHub (clone, add, commit, push, pull, merge, rebase, working with Pull
+                Request)
+              </li>
+              <li>NPM, Webpack</li>
+              <li>CSS3 / HTML5</li>
+              <li>Chrome DevTools, Figma</li>
+              <li>Understanding of the REST</li>
+            </ul>
+          </Paragraph>
+          <Paragraph>
+            Attention! Mentors on this course will be first assigned to the graduates of the RS
+            School Stage #2.
+          </Paragraph>
 
-        <div>
-          <button>Register</button>
+          <Button label={'Register'} href="https://rs.school/" />
         </div>
-      </div>
-
-      <div>
-        <img src="" alt="" />
+        <img className="right picture" src={image} alt="Logo" />
       </div>
     </div>
   );
