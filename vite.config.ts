9259581cc6b0988ef42cbe077bd7509fdfd116ba
KubatBekkk <74785255+Kubatbekkk@users.noreply.github.com>
2024-01-28 10:13:41 +0600

    Fix/naming issue (#10)
    * fix: change dist to build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
