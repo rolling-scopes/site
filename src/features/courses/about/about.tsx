72302990e1bd8946f36b8aea641db00df15ee174
Illia Sakharau <sakharovia0204@gmail.com>
2024-04-11 10:56:09 +0400

    feat: remove nearest course from about section

--- src/features/courses/about/about.tsx
+++ src/features/courses/about/about.tsx
@@ -1,29 +1,11 @@
-import { Paragraph, Subtitle, Title, CourseCard } from '@/app/components';
-import { useNearestCourse } from '@/app/hooks';
-import { type Course } from '@/app/types';
+import { Paragraph, Subtitle, Title } from '@/app/components';
 import './about.scss';
 
 export const About = () => {
-  const { course: data, loading, hasError } = useNearestCourse();
-
-  const course = data as Course;
-
-  let courseContent;
-  if (loading) {
-    courseContent = <p>Loading...</p>;
-  } else if (hasError) {
-    courseContent = <p>Error loading courses.</p>;
-  } else if (course) {
-    courseContent = <CourseCard {...course} />;
-  } else {
-    courseContent = <p>No courses found.</p>;
-  }
-
   return (
     <div className="rs-about container">
       <div className="rs-about content">
         <div className="column-2">
-          <div className="card-wrapper">{courseContent}</div>
           <div className="info">
             <Title text="About RS School" hasAsterisk />
             <Subtitle text="No matter your age, professional employment, or place of residence." />
