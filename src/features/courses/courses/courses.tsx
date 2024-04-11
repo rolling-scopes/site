138695b8372317d94ddc1166bd899f7ca528bbe1
Illia Sakharau <sakharovia0204@gmail.com>
2024-04-11 11:56:32 +0400

    feat: add sorting for courses

--- src/features/courses/courses/courses.tsx
+++ src/features/courses/courses/courses.tsx
@@ -2,10 +2,12 @@ import { CourseCard, Title, TitleType } from '@/app/components';
 import { useDataByName, useNearestCourse } from '@/app/hooks';
 import './courses.scss';
 import { type Course } from '@/app/types';
+import { compareNumbers } from './utils/compare-courses';
 
 export const Courses = () => {
   const { data: courses, loading, error } = useDataByName('courses');
   const { course: nearestCourse, loading: nearestLoading, hasError: nearestHasError } = useNearestCourse();
+  const dateNow = Date.now();
 
   let courseContent;
   if (nearestLoading) {
@@ -22,6 +24,8 @@ export const Courses = () => {
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>{error.message}</h1>;
 
+  const sortedCourses = (courses as Course[]).sort(compareNumbers)
+
   return (
     <div className="rs-courses container" id="upcoming-courses">
       <div className="rs-courses content">
@@ -32,13 +36,14 @@ export const Courses = () => {
         
         <Title text="Other curses" type={TitleType.Regular} />
         <div className="rs-courses-wrapper">
-          {(courses as Course[]).map(
+          {sortedCourses.map(
             (course) => {
               if (course.id === nearestCourse?.id) return;
               return (
                 <CourseCard
                   key={course.id}
                   {...course}
+                  startDate={ dateNow < Date.parse(course.startDate) ? course.startDate : '(Upcoming)' }
                 />
               )
             }
