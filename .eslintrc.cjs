569842aaa9a46057475355b2efeeab0c327d3954
ansivgit <ansiv@yandex.ru>
2024-04-15 22:49:31 +0300

    fix: add eslint plugins & configs for code style check & imports sorting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,68 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:css-import-order/recommended',
   ],
+  plugins: ['react-refresh', 'css-import-order'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    'sourceType': 'module',
+    'ecmaVersion': 'latest',
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/no-unresolved': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups:
+        [['builtin', 'external'], ['internal', 'parent', 'sibling', 'index', 'type'], ['object']],
+        pathGroups: [
+          {
+            pattern: '{react,react-dom/**,redux}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '{.,..}/**/*\.(svg|webp)',
+            group: 'object',
+            position: 'after'
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'react-dom/**'],
+        'distinctGroup': false,
+        // 'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          orderImportKind: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        'ignoreCase': false,
+        'ignoreDeclarationSort': true,
+        'ignoreMemberSort': false,
+        'memberSyntaxSortOrder': ['all', 'multiple', 'single', 'none'],
+        'allowSeparatedGroups': false,
+    }],
   },
+  settings: {
+    'import/resolver': {
+      'typescript': {
+        'alwaysTryTypes': true,
+      }
+    },
+  }
 }
