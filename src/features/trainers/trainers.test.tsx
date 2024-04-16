02e84282c195b5435d6af9e14490726677fb6103
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-16 18:52:48 +0300

    refactor: change trainers test to use mocked data

--- src/features/trainers/trainers.test.tsx
+++ src/features/trainers/trainers.test.tsx
@@ -1,31 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import { Trainers } from './trainers';
+import {
+  MOCKED_MULTIPLE_TRAINERS,
+  MOCKED_ONE_TRAINER,
+  MOCKED_SEVERAL_TRAINERS,
+} from '@/__tests__/constants.ts';
 
 describe('Trainer', () => {
   it('renders the title correctly when 1 trainer', () => {
-    render(<Trainers courseName="nodejs" />);
+    render(<Trainers trainers={MOCKED_ONE_TRAINER} />);
     const titleElement = screen.getByText('Our trainer');
     expect(titleElement).toBeVisible();
   });
 
-  it('renders the title correctly with "reactRu" props', () => {
-    render(<Trainers courseName="reactRu" />);
+  it('renders the title correctly with lang ru prop', () => {
+    render(<Trainers trainers={MOCKED_MULTIPLE_TRAINERS} lang="ru" />);
     const titleElement = screen.getByText('Преподаватели курса');
     expect(titleElement).toBeVisible();
   });
 
   it('renders the title correctly when several trainers', () => {
-    render(<Trainers courseName="angular" />);
+    render(<Trainers trainers={MOCKED_MULTIPLE_TRAINERS} />);
     const titleElement = screen.getByText('Our mentors and trainers');
     expect(titleElement).toBeVisible();
   });
 
   it('renders the trainer info correctly with "nodejs" props', () => {
-    render(<Trainers courseName="nodejs" />);
+    render(<Trainers trainers={MOCKED_ONE_TRAINER} />);
     const trainerNameElement = screen.getByText('Maksim Shylau');
     const trainerTitleElement = screen.getByText('Senior Software Engineer at Epam');
     const trainerDescriptionElement = screen.getByText(
-      /Maksim Shylau is a professional with around 6 years/i
+      /Maksim Shylau is a professional with around 6 years/i,
     );
 
     expect(trainerNameElement).toBeVisible();
@@ -33,14 +38,14 @@ describe('Trainer', () => {
     expect(trainerDescriptionElement).toBeVisible();
   });
 
-  it('renders all the trainers if passed several (8 in angular)', () => {
-    const { container } = render(<Trainers courseName="angular" />);
+  it('renders all the trainers if passed several (8 in items)', () => {
+    const { container } = render(<Trainers trainers={MOCKED_SEVERAL_TRAINERS} />);
     const trainers = container.getElementsByClassName('trainer-card');
     expect(trainers).toHaveLength(8);
   });
 
   it('renders the image with correct alt text', () => {
-    render(<Trainers courseName="nodejs" />);
+    render(<Trainers trainers={MOCKED_ONE_TRAINER} />);
     const imageElement = screen.getByAltText('Maksim Shylau Senior Software Engineer at Epam');
     expect(imageElement).toBeInTheDocument();
   });
