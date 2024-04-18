f6e41033976ffdcb543752240be1256a4cf6a98a
Roman Shevyakov <rs.8@bk.ru>
2024-04-18 08:50:39 +0500

    refactor: add curly braces to if statement

--- src/app/hooks/use-title/use-title.ts
+++ src/app/hooks/use-title/use-title.ts
@@ -4,9 +4,13 @@ export const useTitle = (title: string) => {
   const documentDefined = typeof document !== 'undefined';
   const originalTitle = useRef(documentDefined ? document.title : 'RS Site');
   useEffect(() => {
-    if (!documentDefined) return;
+    if (!documentDefined) {
+      return;
+    }
 
-    if (document.title !== title) document.title = title;
+    if (document.title !== title) {
+      document.title = title;
+    }
 
     const titleElement = originalTitle.current;
 
