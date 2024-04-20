74b19fff453f6fd4201ce67d0c8c4e1d08d23de2
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-20 22:32:17 +0300

    refactor: change image component to use img tag instead of picture

--- src/features/image/image.tsx
+++ src/features/image/image.tsx
@@ -12,27 +12,29 @@ import './image.scss';
 const Image: FC<ImageProps> = ({ alt, src = '', lazy = 'true', ...props }) => {
   const srcNoExtension = src.slice(0, src.lastIndexOf('.'));
   const isLazy = lazy === 'true';
+
   const loading: LoadingAttr = isLazy ? 'lazy' : 'eager';
   const fetchPriority: FetchPriorityAttr = isLazy ? 'low' : 'high';
   const decoding: DecodingAttr = isLazy ? 'async' : 'auto';
 
+  const srcSet = `${srcNoExtension}-${MOBILE_W}.webp ${MOBILE_W}w, ${srcNoExtension}-${TABLET_W}.webp ${TABLET_W}w, ${src} 1280w`;
+  const sizes = `(max-width: ${MOBILE_W}px) ${MOBILE_W}px, (max-width: ${TABLET_W}px) ${TABLET_W}px, 1280px`;
+
   return (
-    <picture>
-      <source media={`(max-width: ${MOBILE_W}px)`} srcSet={`${srcNoExtension}-${MOBILE_W}.webp`} />
-      <source media={`(max-width: ${TABLET_W}px)`} srcSet={`${srcNoExtension}-${TABLET_W}.webp`} />
-      <img
-        // ⚠️ Firefox and Safari wants the loading attribute to be BEFORE the src, in order lazy loading to work
-        // see https://github.com/facebook/react/issues/25883#issuecomment-1410060269
-        loading={loading}
-        src={src}
-        alt={alt}
-        className="img"
-        draggable="false"
-        decoding={decoding}
-        fetchPriority={fetchPriority}
-        {...props}
-      />
-    </picture>
+    <img
+      // ⚠️ Firefox and Safari wants the loading attribute to be BEFORE the src, in order lazy loading to work
+      // see https://github.com/facebook/react/issues/25883#issuecomment-1410060269
+      loading={loading}
+      srcSet={srcSet}
+      sizes={sizes}
+      decoding={decoding}
+      fetchPriority={fetchPriority}
+      src={src}
+      alt={alt}
+      className="img"
+      draggable="false"
+      {...props}
+    />
   );
 };
 
