e26b0a4f302c74bf4463311178a7361140cbcfe3
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-20 19:06:58 +0300

    fest: add more attributes for efficient lazy loading

--- src/features/Image/image.tsx
+++ src/features/Image/image.tsx
@@ -1,18 +1,33 @@
 import { FC } from 'react';
 import { MOBILE_W, TABLET_W } from '@/features/Image/constants.ts';
-import { ImageProps, LoadingAttr } from '@/features/Image/types.ts';
+import {
+  DecodingAttr,
+  FetchPriorityAttr,
+  ImageProps,
+  LoadingAttr,
+} from '@/features/Image/types.ts';
 
 import './image.scss';
 
 const Image: FC<ImageProps> = ({ alt, src = '', lazy = true, ...props }) => {
   const srcNoExtension = src.slice(0, src.lastIndexOf('.'));
   const loading: LoadingAttr = lazy ? 'lazy' : 'eager';
+  const fetchPriority: FetchPriorityAttr = lazy ? 'low' : 'high';
+  const decoding: DecodingAttr = lazy ? 'async' : 'auto';
 
   return (
     <picture {...props}>
       <source media={`(max-width: ${MOBILE_W}px)`} srcSet={`${srcNoExtension}-${MOBILE_W}.webp`} />
       <source media={`(max-width: ${TABLET_W}px)`} srcSet={`${srcNoExtension}-${TABLET_W}.webp`} />
-      <img loading={loading} src={src} alt={alt} className="img" />
+      <img
+        src={src}
+        alt={alt}
+        className="img"
+        draggable="false"
+        decoding={decoding}
+        loading={loading}
+        fetchPriority={fetchPriority}
+      />
     </picture>
   );
 };
--- src/features/Image/types.ts
+++ src/features/Image/types.ts
@@ -5,3 +5,7 @@ export type ImageProps = ImgHTMLAttributes<HTMLImageElement> & {
 };
 
 export type LoadingAttr = ImgHTMLAttributes<HTMLImageElement>['loading'];
+
+export type FetchPriorityAttr = ImgHTMLAttributes<HTMLImageElement>['fetchPriority'];
+
+export type DecodingAttr = ImgHTMLAttributes<HTMLImageElement>['decoding'];
