6eb56e762ef554a27dc4e65b6ee6045777852b37
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-20 18:06:25 +0300

    refactor: remove unused code

--- src/features/Image/image.tsx
+++ src/features/Image/image.tsx
@@ -11,11 +11,9 @@ type LoadingAttr = ImgHTMLAttributes<HTMLImageElement>['loading'];
 const TABLET_W = import.meta.env.VITE_TABLET;
 const MOBILE_W = import.meta.env.VITE_MOBILE;
 
-const Image: FC<ImageProps> = ({ alt, src, lazy = true, ...props }) => {
-  const srcPath = src ?? '';
-  const srcNoExtension = srcPath.slice(0, srcPath.lastIndexOf('.'));
-  // const srcSet = `${srcPath} 1200w, ${srcNoExtension}-medium.webp 768w, ${srcNoExtension}-small.webp 425w`;
-  const srcSet = `${srcPath} 1200w, ${srcNoExtension}-${TABLET_W}.webp ${TABLET_W}w, ${srcNoExtension}-${MOBILE_W}.webp ${MOBILE_W}w`;
+const Image: FC<ImageProps> = ({ alt, src = '', lazy = true, ...props }) => {
+  const srcNoExtension = src.slice(0, src.lastIndexOf('.'));
+  const srcSet = `${src} 1200w, ${srcNoExtension}-${TABLET_W}.webp ${TABLET_W}w, ${srcNoExtension}-${MOBILE_W}.webp ${MOBILE_W}w`;
   const loading: LoadingAttr = lazy ? 'lazy' : 'eager';
 
   return <img {...props} loading={loading} srcSet={srcSet} alt={alt} className="img" />;
