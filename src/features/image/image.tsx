fe3251a2d987cb11ee2597c643e82673cbc9cc3f
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-22 12:53:17 +0300

    refactor: remove return logic

--- src/features/image/image.tsx
+++ src/features/image/image.tsx
@@ -12,13 +12,14 @@ import generateSrcSet from '@/features/image/utils/generateSrcSet.ts';
 
 const Image: FC<ImageProps> = ({ alt, src = '', lazy = 'true', ...props }) => {
   const srcWebp = convertToWebp(src);
-  const [srcSet, setSrcSet] = useState(() => generateSrcSet(srcWebp));
+  const [srcSet, setSrcSet] = useState(() => (IS_DEV ? undefined : generateSrcSet(srcWebp)));
 
   const isLazy = lazy === 'true';
   const loading: LoadingAttr = isLazy ? 'lazy' : 'eager';
   const fetchPriority: FetchPriorityAttr = isLazy ? 'low' : 'high';
   const decoding: DecodingAttr = isLazy ? 'async' : 'auto';
-  const sizes = generateSizes();
+  const sizes = IS_DEV ? undefined : generateSizes();
+  const srcAttr = IS_DEV ? src : srcWebp;
 
   const handleError = () => {
     // fallback to basic src if there are no responsive sizes for an image
@@ -30,11 +31,11 @@ const Image: FC<ImageProps> = ({ alt, src = '', lazy = 'true', ...props }) => {
       // ⚠️ Firefox and Safari wants the loading attribute to be BEFORE the src, in order lazy loading to work
       // see https://github.com/facebook/react/issues/25883#issuecomment-1410060269
       loading={loading}
-      srcSet={IS_DEV ? undefined : srcSet}
-      sizes={IS_DEV ? undefined : sizes}
+      srcSet={srcSet}
+      sizes={sizes}
       decoding={decoding}
       fetchPriority={fetchPriority}
-      src={IS_DEV ? src : srcWebp}
+      src={srcAttr}
       alt={alt}
       draggable="false"
       onError={handleError}
