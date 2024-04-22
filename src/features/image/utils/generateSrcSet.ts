fb6ab941ba942aab29017c45dd85b69ecd0b5d13
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-22 22:01:58 +0300

    fix: remove magic numbers

--- src/features/image/utils/generateSrcSet.ts
+++ src/features/image/utils/generateSrcSet.ts
@@ -1,4 +1,4 @@
-import { MOBILE_W, TABLET_W } from '@/features/image/constants.ts';
+import { DESKTOP_W, MOBILE_W, TABLET_W } from '@/features/image/constants.ts';
 
 /**
  * Generates the srcset for the given src image, based on the env breakpoints
@@ -7,7 +7,7 @@ import { MOBILE_W, TABLET_W } from '@/features/image/constants.ts';
  */
 const generateSrcSet = (src: string) => {
   const srcNoExtension = src.slice(0, src.lastIndexOf('.'));
-  const srcSet = `${srcNoExtension}-${MOBILE_W}.webp ${MOBILE_W}w, ${srcNoExtension}-${TABLET_W}.webp ${TABLET_W}w, ${src} 1280w`;
+  const srcSet = `${srcNoExtension}-${MOBILE_W}.webp ${MOBILE_W}w, ${srcNoExtension}-${TABLET_W}.webp ${TABLET_W}w, ${src} ${DESKTOP_W}w`;
   return srcSet;
 };
 
