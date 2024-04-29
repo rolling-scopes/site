7d6e2182b168449476cc0aeb25170b29426c1d80
Bogdan <bogdanscherbinadev@gmail.com>
2024-04-29 18:56:37 +0300

    refactor: change dynamic routes to not modify component names

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,73 +1,122 @@
 import { config } from '@/config';
-import { BaseLayout } from '@/features/base-layout';
-import { Courses, Home, Nodejs } from '@/pages';
-import { Angular } from '@/pages/angular';
-import { AwsDeveloper } from '@/pages/aws-developer';
-import { AwsFundamentals } from '@/pages/aws-fundamentals';
-import { JavaScript } from '@/pages/javascript';
-import { NotFound } from '@/pages/not-found';
-import { React } from '@/pages/react';
+
+const coursesRoute = {
+  path: '/courses',
+  children: [
+    {
+      index: true,
+      async lazy() {
+        const { Courses } = await import('../pages/courses.tsx');
+        return { Component: Courses };
+      },
+    },
+    {
+      path: 'nodejs',
+      async lazy() {
+        const { Nodejs } = await import('../pages/nodejs.tsx');
+        return { Component: Nodejs };
+      },
+    },
+    {
+      path: 'javascript-mentoring-program',
+      async lazy() {
+        const { JavaScript } = await import('../pages/javascript.tsx');
+        const boundedComponent = JavaScript.bind(null, { type: 'Mentoring Program' });
+        return { Component: boundedComponent };
+      },
+    },
+    {
+      path: 'javascript-preschool',
+      async lazy() {
+        const { JavaScript } = await import('../pages/javascript.tsx');
+        const boundedComponent = JavaScript.bind(null, { type: 'Pre-school' });
+        return { Component: boundedComponent };
+      },
+    },
+    {
+      path: 'angular',
+      lazy: async () => {
+        const { Angular } = await import('../pages/angular.tsx');
+        return { Component: Angular };
+      },
+    },
+    {
+      path: 'aws-cloud-developer',
+      lazy: async () => {
+        const { AwsDeveloper } = await import('../pages/aws-developer.tsx');
+        return { Component: AwsDeveloper };
+      },
+    },
+    {
+      path: 'aws-fundamentals',
+      lazy: async () => {
+        const { AwsFundamentals } = await import('../pages/aws-fundamentals.tsx');
+        return { Component: AwsFundamentals };
+      },
+    },
+    {
+      path: 'reactjs',
+      lazy: async () => {
+        const { React } = await import('../pages/react.tsx');
+        return { Component: React };
+      },
+    },
+  ],
+};
+
+const notFoundRoute = {
+  path: '*',
+  lazy: async () => {
+    const { NotFound } = await import('../pages/not-found.tsx');
+    return { Component: NotFound };
+  },
+};
 
 export const routes = config.isRollingScopesLanding
   ? [
       {
         path: '/',
-        element: <BaseLayout />,
+        lazy: async () => {
+          const { BaseLayout } = await import('../features/base-layout/base-layout.tsx');
+          return { Component: BaseLayout };
+        },
         children: [
-          { index: true, element: <Home /> },
           {
-            path: '/courses',
-            children: [
-              { index: true, element: <Courses /> },
-              { path: 'nodejs', element: <Nodejs /> },
-              {
-                path: 'javascript-mentoring-program',
-                element: <JavaScript type="Mentoring Program" />,
-              },
-              { path: 'javascript-preschool', element: <JavaScript type="Pre-school" /> },
-              { path: 'angular', element: <Angular /> },
-              { path: 'aws-cloud-developer', element: <AwsDeveloper /> },
-              { path: 'aws-fundamentals', element: <AwsFundamentals /> },
-              { path: 'reactjs', element: <React /> },
-            ],
+            index: true,
+            lazy: async () => {
+              const { Home } = await import('../pages/home.tsx');
+              return { Component: Home };
+            },
           },
+          coursesRoute,
         ],
       },
-      { path: '*', element: <NotFound /> },
+      notFoundRoute,
     ]
   : [
       {
         path: '/',
-        element: <BaseLayout />,
+        lazy: async () => {
+          const { BaseLayout } = await import('../features/base-layout/base-layout.tsx');
+          return { Component: BaseLayout };
+        },
         children: [
           {
             index: true,
-            element: <Courses />,
+            lazy: async () => {
+              const { Courses } = await import('../pages/courses.tsx');
+              return { Component: Courses };
+            },
           },
           {
             path: '/community',
-            element: <Home />,
+            lazy: async () => {
+              const { Home } = await import('../pages/home.tsx');
+              return { Component: Home };
+            },
           },
-          {
-            path: '/courses',
-            children: [
-              {
-                index: true,
-                element: <Courses />,
-              },
-              { path: 'nodejs', element: <Nodejs /> },
-              {
-                path: 'javascript-mentoring-program',
-                element: <JavaScript type="Mentoring Program" />,
-              },
-              { path: 'javascript-preschool', element: <JavaScript type="Pre-school" /> },
-              { path: 'angular', element: <Angular /> },
-              { path: 'aws-cloud-developer', element: <AwsDeveloper /> },
-              { path: 'aws-fundamentals', element: <AwsFundamentals /> },
-              { path: 'reactjs', element: <React /> },
-            ],
-          },
+          coursesRoute,
         ],
       },
-      { path: '*', element: <NotFound /> },
+      notFoundRoute,
     ];
