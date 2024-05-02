46d08690bada852faf2bb466b4ecd684c6b22b07
Bogdan <bogdanscherbinadev@gmail.com>
2024-05-02 18:46:00 +0300

    refactor: remove css loader

--- src/features/base-layout/base-layout.tsx
+++ src/features/base-layout/base-layout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import { Footer, Navbar, Partnered, ScrollToHashElement } from './components';
-import Loader from '@/features/loader/loader.tsx';
 
 export const BaseLayout = () => {
   return (
     <>
-      <Loader />
       <ScrollToHashElement />
       <Navbar />
       <Outlet />
