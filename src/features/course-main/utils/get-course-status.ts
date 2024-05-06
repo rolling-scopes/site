33fa27a435cc689761c3d015d2b2d3832106bdd6
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-05-06 15:12:59 +0500

    refactor: refactor getCourseStatus

--- src/features/course-main/utils/get-course-status.ts
+++ src/features/course-main/utils/get-course-status.ts
@@ -2,14 +2,19 @@ import dayjs from 'dayjs';
 import { hasDayInDate } from '.';
 import { CourseStatus } from '@/app/types';
 
-export function getCourseStatus(date: string): CourseStatus {
+export function getCourseStatus(courseStartDate: string): CourseStatus {
   const now = dayjs();
-  const startDate = dayjs(date);
+  const startDate = dayjs(courseStartDate);
 
+  // Status is "Available" if a start date is set and it is within 2 weeks before or after the current date
   const isAvailable =
-    hasDayInDate(date) && startDate.isBetween(now.subtract(2, 'week'), now.add(2, 'week'));
+    hasDayInDate(courseStartDate) &&
+    startDate.isBetween(now.subtract(2, 'week'), now.add(2, 'week'));
+
+  // Status is 'Upcoming' if the course will start within the next 3 months
   const isUpcoming = startDate.isBetween(now, now.add(3, 'month'));
 
+  // Other dates
   let label = CourseStatus.PLANNED;
 
   if (isAvailable) {
