57282633c9ebbb9d40bd2c0cb44be0d7a641140a
Roman Shevyakov <rs.8@bk.ru>
2024-05-06 22:06:40 +0500

    feat: add config to capture full page screen, update screens

--- src/__tests__/visual/example.spec.ts
+++ src/__tests__/visual/example.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 test('Capture or match screenshots', async ({ page }) => {
   // await page.goto('https://rs.school');
   await page.goto('http://localhost:4173');
-  await expect(page).toHaveScreenshot('origin.png');
+  await expect(page).toHaveScreenshot('origin.png', { fullPage: true, timeout: 30000 });
 });
 
 // test('Main page has title', async ({ page }) => {
