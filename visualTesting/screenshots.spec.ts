746b804b7e5959b2fc04cace7d4d969f5ef358ae
Roman Shevyakov <rs.8@bk.ru>
2024-05-07 15:40:10 +0500

    test: add Playwright testing to pre-push

--- visualTesting/screenshots.spec.ts
+++ visualTesting/screenshots.spec.ts
@@ -1,7 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('Capture or match screenshots', async ({ page }) => {
-  // await page.goto('https://rs.school');
   await page.goto('http://localhost:4173');
   await expect(page).toHaveScreenshot('origin.png', { fullPage: true, timeout: 10000 });
 });
