c4e55db7cd6288ccbc5d1f85cef1420daf9f604a
stroich <stroichmwork@gmail.com>
2024-05-07 20:05:06 +0400

    refactor: fix epam, facebook

--- src/icons/angular-icon.tsx
+++ src/icons/angular-icon.tsx
@@ -2,5 +2,5 @@ import angular from '@/assets/svg/angular-icon.svg';
 import Image from '@/features/image';
 
 export const AngularIcon = () => {
-  return <Image src={angular} alt="logo" />;
+  return <Image src={angular} alt="angular icon" />;
 };
--- src/icons/facebook.tsx
+++ src/icons/facebook.tsx
@@ -1,15 +1,6 @@
-import { FC } from 'react';
+import facebook from '@/assets/svg/facebook.svg';
+import Image from '@/features/image';
 
-export const FacebookIcon: FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="57" height="57">
-    <rect y="0.0683594" width="56" height="56" rx="28" fill="#EEEEEE" />
-    <path
-      d="M 34.834 18.068 L 31.834 18.068 C 30.507 18.068 29.236 18.595 28.298 19.533 C 27.36 20.471 26.834 21.742 26.834 23.068 L 26.834 26.068 L 23.834 26.068 L 23.834 30.068 L 26.834 30.068 L 26.834 38.068 L 30.834 38.068 L 30.834 30.068 L 33.834 30.068 L 34.834 26.068 L 30.834 26.068 L 30.834 23.068 C 30.834 22.803 30.939 22.549 31.126 22.361 C 31.314 22.174 31.568 22.068 31.834 22.068 L 34.834 22.068 Z"
-      fill="transparent"
-      strokeWidth="2"
-      stroke="rgb(0,0,0)"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeDasharray=""></path>
-  </svg>
-);
+export const FacebookIcon = () => {
+  return <Image src={facebook} alt="facebook icon" />;
+};
