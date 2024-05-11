c11766ae9481d9cfa498b28b5d7a2d6d35ab1e52
Roman Shevyakov <rs.8@bk.ru>
2024-05-11 14:42:16 +0500

    test: add separate EtE tests for header & footer

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:4173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'off',
   },
   outputDir: 'visualTestingResults',
   // timeout: 10000,
--- visualTesting/screenshots.spec.ts
+++ visualTesting/screenshots.spec.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '@playwright/test';
 
-test('Capture or match screenshots', async ({ page }) => {
-  await page.goto('http://localhost:4173');
-  await expect(page).toHaveScreenshot('origin.png', { fullPage: true, timeout: 10000 });
+test('Full page screenshots (match or capture if not exist)', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveScreenshot('origin-fullpage.png', { fullPage: true, timeout: 10000 });
+});
+
+test('Navbar sections screenshots (match or capture if not exist)', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.locator('.navbar')).toHaveScreenshot('navbar.png', { timeout: 10000 });
+});
+
+test('Footer section screenshots (match or capture if not exist)', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.locator('footer')).toHaveScreenshot('footer.png', { timeout: 10000 });
 });
