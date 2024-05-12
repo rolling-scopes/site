e938bcd4ee2104981ca67c9a060e95003d8e0c4f
--global <asdrogachev@gmail.com>
2024-05-12 08:14:03 +0300

    docs: add a comment about updating training-program file

--- src/app/components/subtitle/subtitle.tsx
+++ src/app/components/subtitle/subtitle.tsx
@@ -4,4 +4,5 @@ interface SubtitleProps {
   text: string;
 }
 
+//if component name or page type Pre-school is changed - you should update file training-program.tsx to correspond with rendering logic at const "filteredContent".
 export const Subtitle = ({ text }: SubtitleProps) => <div className="subtitle">{text}</div>;
