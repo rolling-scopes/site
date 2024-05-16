521242cf0dc435fd87c8ff7f16ca229cc697bf27
Roman Shevyakov <46446962+ramzes-84@users.noreply.github.com>
2024-05-16 13:18:57 +0500

    Test: visual testing integration (#189)
    * install: playwright
    
    * install: remove example tests, change vitest config
    
    * feat: add basic screenshots
    
    * feat: add config to capture full page screen, update screens
    
    * feat: improve test behaviour, change folder structure
    
    * test: add Playwright testing to pre-push
    
    * test: testing behaviour
    
    * docs: add readme for visual testing
    
    * docs: add assets to readme
    
    * fix: remove test from husky, update docs
    
    * test: add separate EtE tests for header & footer
    
    * test: move EtE tests to __test__ folder, update conf, update screenshots

---
+++ playwright.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'src/__tests__/visualTesting',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI
+    ? 'dot'
+    : [['html', { outputFolder: 'src/__tests__/visualTesting/report' }]],
+  use: {
+    baseURL: 'http://localhost:4173',
+    trace: 'off',
+  },
+  outputDir: 'src/__tests__/visualTesting/results',
+  projects: [
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  webServer: {
+    command: 'npm run preview',
+    url: 'http://localhost:4173',
+    reuseExistingServer: !process.env.CI,
+  },
+});
