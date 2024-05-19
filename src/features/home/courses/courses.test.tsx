5521b16129caeb20c9212a070ea118d189e4c22e
--global <asdrogachev@gmail.com>
2024-05-19 20:39:57 +0300

    feat: update test to correspond with updated component

--- src/features/home/courses/courses.test.tsx
+++ src/features/home/courses/courses.test.tsx
@@ -1,25 +1,68 @@
 import { render, screen } from '@testing-library/react';
 import { Mock, beforeEach, describe, expect, it, vi } from 'vitest';
 import { Courses } from './courses';
+import { renderWithRouter } from '@/__tests__/utils';
+import { ROUTES } from '@/app/const';
 import { useWindowSize } from '@/app/hooks';
+import { buildUrl } from '@/app/services/platform';
+
+const mockedData = [
+  {
+    id: '1',
+    title: 'JS / Front-end Pre-school RU',
+    startDate: 'Jun 24, 2024',
+    language: ['ru'],
+    detailsUrl: buildUrl(`/${ROUTES.COURSES}/${ROUTES.JS_PRESCHOOL_RU}`),
+  },
+  {
+    id: '2',
+    title: 'JS / Front-end EN',
+    startDate: 'Oct, 2024',
+    language: ['en'],
+    detailsUrl: buildUrl(`/${ROUTES.COURSES}/${ROUTES.JS}`),
+  },
+  {
+    id: '3',
+    title: 'JS / Front-end RU',
+    startDate: 'Oct, 2024',
+    language: ['ru'],
+    detailsUrl: buildUrl(`/${ROUTES.COURSES}/${ROUTES.JS_RU}`),
+  },
+  {
+    id: '4',
+    title: 'React',
+    startDate: 'Jul 1, 2024',
+    language: ['en'],
+    detailsUrl: buildUrl(`/${ROUTES.COURSES}/${ROUTES.REACT}`),
+  },
+  {
+    id: '5',
+    title: 'Angular',
+    startDate: 'Jul 1, 2024',
+    language: ['en'],
+    detailsUrl: buildUrl(`/${ROUTES.COURSES}/${ROUTES.ANGULAR}`),
+  },
+];
+
+vi.mock('@/app/hooks', () => {
+  return {
+    useDataByName: vi.fn().mockImplementation(() => ({
+      data: mockedData,
+      error: null,
+      loading: false,
+    })),
+    useWindowSize: vi.fn().mockReturnValue({ width: 1440, height: 900 }),
+  };
+});
 
 describe('Courses', () => {
-  vi.mock('@/app/hooks', () => ({
-    useWindowSize: vi.fn().mockReturnValue({ width: 1440, height: 900 }),
-  }));
-
   beforeEach(() => {
-    render(<Courses />);
+    renderWithRouter(<Courses />);
   });
 
   it('renders the title correctly', () => {
     const titleElement = screen.getByText('Upcoming courses');
     expect(titleElement).toBeInTheDocument();
-  });
-
-  it('renders course info correctly', () => {
-    const courseElement = screen.getByText('AWS Fundamentals');
-    expect(courseElement).toBeInTheDocument();
   });
 
   it('renders three course cards', () => {
