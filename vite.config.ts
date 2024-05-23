880a2806240a6273b10ef6e46472ebe2d36792be
Aliaksandr Yakubouski <stockupby@gmail.com>
2024-05-23 20:26:42 +0200

    feat: 215 - add normalize file to project

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "./src/styles/_reset.scss";
+          @import "./src/styles/_normalize.scss";
           @import "./src/styles/_constants.scss";
           @import "./src/styles/_mixins.scss";
           @import "./src/styles/_placeholders.scss";
