954f0996ded0093aa526c42b830d37946238e452
ansivgit <ansiv@yandex.ru>
2024-05-27 14:53:42 +0300

    feat: 49 - add sorting events

--- src/features/home/events/events.tsx
+++ src/features/home/events/events.tsx
@@ -18,17 +18,21 @@ import styles from './events.module.scss';
 
 const cx = classNames.bind(styles);
 
+const displayedCardsQuantity = 2;
+
 const actualEvents = getActualDataList({
   dataList: events as EventCardProps[],
   actualDelayInDays: 3,
 });
 
-const stub = <Image src={photo3} />;
+const nearestEvents = actualEvents.slice(0, displayedCardsQuantity);
+
+const Stub = <Image src={photo3} />;
 
 export const Events = () => (
-  <div id="events" className={cn(cx('events'), 'container')}>
+  <article id="events" className={cn(cx('events'), 'container')}>
     <div className={cn(cx('events', 'content'), 'content')}>
-      <div className={cx('info')}>
+      <section className={cx('info')}>
         <SectionLabel label="events & meetups" />
         <Title text="Meet us at events" hasAsterisk />
         <Subtitle text="For years we have been organizing meetups and conferences, where you can always learn something new, share your knowledge, discover new technologies, meet old and find new friends." />
@@ -36,14 +40,15 @@ export const Events = () => (
           During 9 years we have organized 150+ events for developers in different cities and
           countries.
         </Paragraph>
-      </div>
+      </section>
 
-      <div className={cx('cards')}>
-        {!actualEvents.length && stub}
-        {(actualEvents as EventCardProps[]).map((i) => (
+      <section className={cx('cards')}>
+        {!actualEvents.length && Stub}
+
+        {(nearestEvents as EventCardProps[]).map((i) => (
           <EventCard key={i.title} {...i} date={dayjs(i.date).format('DD MMM YYYY')} />
         ))}
-      </div>
+      </section>
     </div>
-  </div>
+  </article>
 );
--- src/utils/getActualDataList.ts
+++ src/utils/getActualDataList.ts
@@ -4,22 +4,28 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 type DataListType<T> = Array<T>;
 type DataItem = Record<string, string | HTMLElement>;
 interface DataItemWithDate extends DataItem {
-  date: string; // 'DD.MM.YYYY'
+  date: string; // 'YYYY-MM-DD'
 }
 
 export type ActualDataListProps = {
   dataList: DataListType<DataItemWithDate>;
   actualDelayInDays: number;
+  sorted?: boolean;
 };
 
 export const getActualDataList = ({
   dataList,
   actualDelayInDays,
+  sorted = true,
 }: ActualDataListProps): DataListType<DataItemWithDate> | [] => {
   extend(customParseFormat);
 
   const postponeDate = dayjs().subtract(actualDelayInDays, 'day');
   const actualDataList = dataList.filter((item) => dayjs(item.date, 'YYYY-MM-DD') >= postponeDate);
 
-  return actualDataList;
+  const sortedList = (actualDataList as DataListType<DataItemWithDate>).sort((a, b) =>
+    dayjs(a.date).diff(b.date),
+  );
+
+  return sorted ? sortedList : actualDataList;
 };
