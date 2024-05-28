124631d6d823bd14bff9b08e5749fd4e746c899e
NikitaStarmoussov <nikta.starmousov@gmail.com>
2024-05-28 14:46:15 +0500

    feat: update data in communication

--- src/features/communication/communication.tsx
+++ src/features/communication/communication.tsx
@@ -25,7 +25,7 @@ const localizedContent = {
     secondParagraphFirstHalf: 'There are channels in',
     telegramLink: 'Telegram',
     secondParagraphSecondHalf:
-      ', for discussing events related to your location. For example, offline lectures. And informal chats among students from the same location.',
+      ', for discussing events related to your location. For example, offline lectures or just informal chats among students from the same location.',
     thirdParagraphFirstHalf: 'Please read the information about communication in RS School in the',
     rsDocsLink: 'RS Docs',
     thirdParagraphSecondHalf: ', where you can find rules, descriptions of channels, FAQ.',
@@ -38,9 +38,9 @@ const localizedContent = {
     firstParagraphSecondHalf:
       ', где вы можете посмотреть последние новости, задать вопросы и общаться со студентами.',
     secondParagraphFirstHalf: 'Также есть каналы в',
-    telegramLink: 'телеграм',
+    telegramLink: 'Телеграм',
     secondParagraphSecondHalf:
-      ', для обсуждения мероприятий, относящихся к вашему городу. Например, офлайн лекции. И неформального общения студентов из одной локации',
+      ', для обсуждения мероприятий, относящихся к вашему городу. Например, офлайн лекции или просто для общения студентов из одной локации',
     thirdParagraphFirstHalf: 'Обязательно прочитайте информацию об общении в RS School в',
     rsDocsLink: 'RS Docs',
     thirdParagraphSecondHalf: ', где вы можете найти правила, описание каналов, FAQ.',
