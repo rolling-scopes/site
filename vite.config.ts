4dfad3c0652205d3795fe5db2157a74a17a403ee
Aliaksandr Yakubouski <stockupby@gmail.com>
2024-06-02 19:06:51 +0200

    fix: file name case

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
           [
             'babel-plugin-react-compiler',
             {
-              runtimeModule: '@/hooks/useC',
+              runtimeModule: '@/hooks/usec',
               environment: {
                 validateNoRefAccessRender: true,
               },
