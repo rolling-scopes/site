1ba31fa493e6347a9443071f79d5331d21b1f62b
ansivgit <ansiv@yandex.ru>
2024-06-13 15:30:04 +0300

    feat: 247 - add config final version

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
-/* eslint-disable no-useless-escape */
-/* eslint-disable @stylistic/quote-props */
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-// import prettierConfig from 'eslint-config-prettier';
 import cssImportOrder from 'eslint-plugin-css-import-order';
 import importPlugin from 'eslint-plugin-import';
-// import prettierPlugin from 'eslint-plugin-prettier';
 // import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -24,7 +20,9 @@ export default [
       'coverage/',
       'optimizeImages/',
       'src/__tests__/',
-      // 'eslint.config.js',
+      'vite.config.ts',
+      'vitest.config.ts',
+      'eslint.config.js',
     ],
   },
   stylistic.configs['recommended-flat'],
@@ -54,8 +52,6 @@ export default [
       'react-hooks': reactHooksPlugin,
       'react-refresh': reactRefresh,
       'import': importPlugin,
-      // 'prettier': prettierPlugin,
-      // 'config-prettier': prettierConfig,
       'vitest': vitestPlugin,
       'css-import-order': cssImportOrder,
       'sort-exports': sortExports,
@@ -73,8 +69,6 @@ export default [
     },
     rules: {
       ...js.configs['recommended'].rules,
-      // ...prettierPlugin.configs['recommended'].rules,
-      // ...prettierConfig.rules,
       ...tsPlugin.configs['recommended'].rules,
       // ...reactPlugin.configs['recommended'].rules,
       ...reactHooksPlugin.configs['recommended'].rules,
@@ -82,7 +76,6 @@ export default [
       ...vitestPlugin.configs['recommended'].rules,
       ...cssImportOrder.configs['recommended'].rules,
 
-      // https://github.com/typescript-eslint/typescript-eslint/blob/main/docs/troubleshooting/FAQ.mdx#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
       'no-undef': 'off',
       'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
@@ -121,7 +114,6 @@ export default [
           ],
           pathGroupsExcludedImportTypes: ['react', 'react-dom/**'],
           'distinctGroup': false,
-          // 'newlines-between': 'always',
           alphabetize: {
             order: 'asc',
             orderImportKind: 'asc',
@@ -157,8 +149,6 @@ export default [
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
 
       '@stylistic/jsx-curly-brace-presence': ['warn', { 'propElementValues': 'always' }],
-      // //* It either requires or disallows spaces between those braces and the values inside of them.
-      // ? '@stylistic/jsx-curly-spacing': ['error', { 'when': 'always', 'children': true }],
       '@stylistic/jsx-wrap-multilines': [
         'error',
         {
@@ -172,28 +162,28 @@ export default [
           'propertyValue': 'parens-new-line',
         },
       ],
-      '@stylistic/jsx/jsx-one-expression-per-line': ['off'],
-      // ? '@stylistic/jsx/jsx-one-expression-per-line': ['off', { 'allow': 'literal' }],
+      '@stylistic/jsx-one-expression-per-line': ['error', { 'allow': 'single-child' }],
       '@stylistic/jsx-max-props-per-line': ['error', { 'maximum': { 'single': 4, 'multi': 1 } }],
+      '@stylistic/jsx-child-element-spacing': 'warn',
 
-      '@stylistic/brace-style': ['error', '1tbs', { 'allowSingleLine': false }], //* for multiline 'if' etc.
+      '@stylistic/brace-style': ['error', '1tbs', { 'allowSingleLine': false }],
       '@stylistic/array-bracket-newline': ['error', 'consistent'],
       '@stylistic/quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
       '@stylistic/operator-linebreak': ['error', 'before', { 'overrides': { '=': 'after' } }],
       '@stylistic/newline-per-chained-call': ['error', { 'ignoreChainWithDepth': 4 }],
       '@stylistic/multiline-ternary': ['error', 'always-multiline'],
-      // ? '@stylistic/padding-line-between-statements': [
-      //   'error',
-      //   {
-      //     blankLine: 'always',
-      //     prev: ['const', 'let', 'var'],
-      //     next: '*',
-      //   },
-      //   { blankLine: 'any',
-      //     prev: ['const', 'let', 'var'],
-      //     next: ['const', 'let', 'var'],
-      //   },
-      // ],
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: ['const', 'let', 'var'],
+          next: '*',
+        },
+        { blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
       '@stylistic/array-element-newline': [
         'error',
         'consistent',
@@ -202,7 +192,11 @@ export default [
           'minItems': 5,
         },
       ],
-      // ? '@stylistic/object-property-newline': ['error', { 'allowAllPropertiesOnSameLine': false }],
+      '@stylistic/object-curly-newline': ['error', {
+        'ObjectExpression': { 'multiline': true, 'minProperties': 2 },
+        'ObjectPattern': { 'multiline': true, 'minProperties': 4 },
+      }],
+      '@stylistic/object-property-newline': ['error', { 'allowAllPropertiesOnSameLine': false }],
 
       '@stylistic/member-delimiter-style': [
         'error',
@@ -220,11 +214,17 @@ export default [
       '@stylistic/arrow-parens': ['error', 'always'],
       '@stylistic/no-mixed-operators': 'error',
       '@stylistic/quote-props': ['error', 'as-needed'],
+      '@stylistic/space-before-function-paren': ['error', {
+        'anonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'always'
+      }],
 
+      '@stylistic/no-floating-decimal': 'error',
       '@stylistic/max-len': [
         'error',
         {
-          'code': 100,
+          'code': 120,
           'tabWidth': 2,
           'comments': 120,
           'ignoreTrailingComments': true,
