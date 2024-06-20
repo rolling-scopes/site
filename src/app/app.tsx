21847ae04ea8804bb01772998cadadfda4af65fe
Aliaksandr Yakubouski <stockupby@gmail.com>
2024-06-20 17:31:10 +0200

    fix: 202 - loading indicator bug

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,7 +9,14 @@ const router = createBrowserRouter(routes);
 function App() {
   return (
     <div className="app-styles">
-      <NextTopLoader color="black" easing="ease-in-out" showSpinner={false} speed={300} />
+      <NextTopLoader
+        color="black"
+        initialPosition={0.0001}
+        crawl={false}
+        easing="ease-in-out"
+        showSpinner={false}
+        speed={300}
+      />
       <RouterProvider router={router} data-testid="router-provider" />
     </div>
   );
