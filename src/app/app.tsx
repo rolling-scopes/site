5a63650f7609bc3af2700215fea377e6d359bf46
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-06-23 01:24:30 +0500

    fix: 283 - fix 'rollingscopes.com' redirect

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import NextTopLoader from 'nextjs-toploader';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes';
 
 import './app.scss';
@@ -8,17 +8,22 @@ const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <div className="app-styles">
-      <NextTopLoader
-        color="black"
-        initialPosition={0.0001}
-        crawl={false}
-        easing="ease-in-out"
-        showSpinner={false}
-        speed={300}
-      />
-      <RouterProvider router={router} data-testid="router-provider" />
-    </div>
+    <>
+      {window.location.hostname === 'rollingscopes.com' && (
+        <Navigate to="https://rs.school" replace={true} />
+      )}
+      <div className="app-styles">
+        <NextTopLoader
+          color="black"
+          initialPosition={0.0001}
+          crawl={false}
+          easing="ease-in-out"
+          showSpinner={false}
+          speed={300}
+        />
+        <RouterProvider router={router} data-testid="router-provider" />
+      </div>
+    </>
   );
 }
 
--- src/widgets/breadcrumbs/ui/breadcrumbs.tsx
+++ src/widgets/breadcrumbs/ui/breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { breadcrumbNameMap } from './constants';
+import { breadcrumbNameMap } from '../constants';
 import { RouteValues } from '@/app/types/route.types.ts';
 
 import './breadcrumbs.scss';
