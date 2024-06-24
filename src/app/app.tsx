ea5c11525d0c376f555ed9673fe1ee4461741f56
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-06-24 14:42:04 +0500

    refactor: update redirect condition

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter(routes);
 
 function App() {
   // If we are on https://rollingscopes.com/, a redirect to https://rs.school/ is triggered.
-  if (window.location.hostname === 'rollingscopes.com') {
+  if (window.location.hostname.includes('rollingscopes.com')) {
     window.location.href = 'https://rs.school';
   }
 
