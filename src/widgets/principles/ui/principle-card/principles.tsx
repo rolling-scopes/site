56b9cfe6255c7fe21a7706decc7c1cbdd7db22cc
NikitaStarmoussov <nikta.starmousov@gmail.com>
2024-06-26 23:18:46 +0300

    refactor: 348 - Title to WidgetTitle in Principles

--- src/widgets/principles/ui/principle-card/principles.tsx
+++ src/widgets/principles/ui/principle-card/principles.tsx
@@ -1,18 +1,13 @@
 import { PrincipleCard } from './principle-card';
 import { cards } from '../../constants';
-import { Title, TitleType } from '@/shared/ui/title';
+import { WidgetTitle } from '@/shared/ui/widget-title/widget-title';
 
 import './principles.scss';
 
 export const Principles = () => (
   <div className="principles container">
     <div className="principles content">
-      <Title
-        text="RS School Principles are an ability to complete our mission"
-        type={TitleType.Big}
-        hasAsterisk={false}
-        hasLines={true}
-      />
+      <WidgetTitle size="large" mods="lines">RS School Principles are an ability to complete our mission</WidgetTitle>
       <div className="cards column-3 ">
         {cards.map(({ title, description, icon }) => (
           <PrincipleCard key={title} description={description} icon={icon} title={title} />
