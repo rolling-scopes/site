706509d1316c0bd65f56463080fafd6bb19f92c0
NikitaStarmoussov <nikta.starmousov@gmail.com>
2024-06-27 09:42:10 +0300

    refactor: 348 - Title to MainTitle in widgets/main

--- src/widgets/main/ui/main.tsx
+++ src/widgets/main/ui/main.tsx
@@ -1,16 +1,15 @@
+import { MainTitle } from '@/shared/ui/main-title/main-title';
 import { Subtitle } from '@/shared/ui/subtitle';
-import { Title, TitleType } from '@/shared/ui/title';
 
 import './main.scss';
 
-// FIXME title to h1
 export const Main = () => {
   return (
     <main id="main" className="school-main container">
       <div className="school-main content">
         <div className="left">
           <div className="title-container">
-            <Title text="RS School" type={TitleType.ExtraBig} />
+            <MainTitle>RS School</MainTitle>
             <Subtitle text="Free courses. High motivation" />
           </div>
           <h2 className="description">Connecting people, growing together, having fun</h2>
