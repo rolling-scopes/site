28fa4dadce3a3dd9966009c68fa65f47f11e51d1
Wystov <wystov@gmail.com>
2024-07-04 00:03:27 +0200

    refactor: 372 - use classNames lib for styles

--- src/shared/ui/date-lang/date-lang.tsx
+++ src/shared/ui/date-lang/date-lang.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable @stylistic/jsx-one-expression-per-line */
+import classNames from 'classnames/bind';
 import micIcon from '@/shared/assets/icons/mic.svg';
 import noteIcon from '@/shared/assets/icons/note-icon.svg';
 import Image from '@/shared/ui/image';
 
 import styles from './date-lang.module.scss';
+
+const cx = classNames.bind(styles);
 
 interface DateLangProps {
   startDate: string;
@@ -16,15 +19,15 @@ export const DateLang = ({ startDate, language, mode, type = '' }: DateLangProps
   const splittedLanguage = language.slice().join('/');
 
   return (
-    <div className={`${styles.info} ${styles[type]}`}>
-      <p className={styles.date}>
-        <Image className={styles.note} src={noteIcon} alt="note-icon" lazy="false" />
+    <div className={cx('info', type)}>
+      <p className={cx('date')}>
+        <Image className={cx('icon')} src={noteIcon} alt="note-icon" lazy="false" />
         <span>Start date {startDate ?? 'Not set'}</span>
       </p>
-      <p className={styles.lang}>
-        <Image className={styles.microphone} src={micIcon} alt="microphone icon" lazy="false" />
-        <span className={styles.language}>{splittedLanguage}</span>
-        <span className={styles.mode}> • {mode}</span>
+      <p className={cx('other')}>
+        <Image className={cx('icon')} src={micIcon} alt="microphone icon" lazy="false" />
+        <span className={cx('language')}>{splittedLanguage}</span>
+        <span className={cx('mode')}> • {mode}</span>
       </p>
     </div>
   );
