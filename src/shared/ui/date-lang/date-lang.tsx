ae2000b15a2b240c7931ba9262e753a4237dec2a
Wystov <wystov@gmail.com>
2024-07-04 00:13:43 +0200

    refactor: 372 - consistency in spans and their content

--- src/shared/ui/date-lang/date-lang.tsx
+++ src/shared/ui/date-lang/date-lang.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @stylistic/jsx-one-expression-per-line */
 import classNames from 'classnames/bind';
 import micIcon from '@/shared/assets/icons/mic.svg';
 import noteIcon from '@/shared/assets/icons/note-icon.svg';
@@ -20,12 +19,13 @@ export const DateLang = ({ startDate, language, mode, type = '' }: DateLangProps
     <div className={cx('info', type)}>
       <p className={cx('date')}>
         <Image className={cx('icon')} src={noteIcon} alt="note-icon" lazy="false" />
-        <span>Start date {startDate ?? 'Not set'}</span>
+        <span>{`Start date: ${startDate ?? 'not set'}`}</span>
       </p>
       <p className={cx('other')}>
         <Image className={cx('icon')} src={micIcon} alt="microphone-icon" lazy="false" />
         <span className={cx('language')}>{language}</span>
-        <span className={cx('mode')}> • {mode}</span>
+        <span>•</span>
+        <span className={cx('mode')}>{mode}</span>
       </p>
     </div>
   );
