d3e8c0cb5c7a8571f04faf63e2b59d464318a025
Wystov <wystov@gmail.com>
2024-07-05 17:56:14 +0200

    feat: add dateTime attribute

--- src/shared/ui/date-lang/date-lang.tsx
+++ src/shared/ui/date-lang/date-lang.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import { COURSE_STALE_AFTER_DAYS } from '@/app/const';
+import { dayJS } from '@/app/services/dayjs';
 import micIcon from '@/shared/assets/icons/mic.svg';
 import noteIcon from '@/shared/assets/icons/note-icon.svg';
 import { getCourseDate } from '@/shared/helpers/getCourseDate';
@@ -16,12 +17,15 @@ interface DateLangProps {
 }
 
 export const DateLang = ({ startDate, language, mode }: DateLangProps) => {
+  const dateAttr = dayJS(startDate).format('YYYY-MM-DD');
+  const dateContent = getCourseDate(startDate, COURSE_STALE_AFTER_DAYS);
+
   return (
     <section className={cx('info')}>
       <p className={cx('date')}>
         <Image className={cx('icon')} src={noteIcon} alt="note-icon" lazy="false" />
         <span>Start date:</span>
-        <time>{getCourseDate(startDate, COURSE_STALE_AFTER_DAYS)}</time>
+        <time dateTime={dateAttr}>{dateContent}</time>
       </p>
       <p className={cx('additional-info')}>
         <Image className={cx('icon')} src={micIcon} alt="microphone-icon" lazy="false" />
