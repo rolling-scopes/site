42228bc1bb38ace99c426e7aa5eb1f96d038dc0b
Wystov <wystov@gmail.com>
2024-07-05 15:59:24 +0200

    feat: validate date param in util func

--- src/shared/helpers/getCourseDate.ts
+++ src/shared/helpers/getCourseDate.ts
@@ -1,7 +1,8 @@
 import { dayJS } from '@/app/services/dayjs';
 
 export const getCourseDate = (startDate: string) => {
-  const isStale = dayJS(startDate) < dayJS().subtract(14, 'day');
+  const isValidDate = dayJS(startDate).isValid();
+  const isStale = isValidDate && dayJS(startDate) < dayJS().subtract(14, 'day');
 
   return isStale ? 'TBD' : startDate;
 };
