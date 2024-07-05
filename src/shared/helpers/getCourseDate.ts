2a6edfcb38fff402d8b6ec90dd5222fe6bbb647b
Wystov <wystov@gmail.com>
2024-07-05 16:04:40 +0200

    fix: date validation logic

--- src/shared/helpers/getCourseDate.ts
+++ src/shared/helpers/getCourseDate.ts
@@ -2,7 +2,7 @@ import { dayJS } from '@/app/services/dayjs';
 
 export const getCourseDate = (startDate: string) => {
   const isValidDate = dayJS(startDate).isValid();
-  const isStale = isValidDate && dayJS(startDate) < dayJS().subtract(14, 'day');
+  const isStale = !isValidDate || dayJS(startDate) < dayJS().subtract(14, 'day');
 
   return isStale ? 'TBD' : startDate;
 };
