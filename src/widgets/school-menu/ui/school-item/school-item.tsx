dcfde10136c41d81867a1850da2f27258fd5a814
Wystov <wystov@gmail.com>
2024-07-05 17:37:53 +0200

    fix: reverse prettier autoformat

--- src/widgets/school-menu/ui/school-item/school-item.tsx
+++ src/widgets/school-menu/ui/school-item/school-item.tsx
@@ -21,14 +21,9 @@ export const SchoolItem = ({ item, color }: SchoolItemProps) => {
     </>
   );
 
-  const descriptionBlock =
-    'description' in item
-      ? (
-          descriptionContent
-        )
-      : (
-        <div className="details">{descriptionContent}</div>
-        );
+  const descriptionBlock = ('description' in item)
+    ? (descriptionContent)
+    : (<div className="details">{descriptionContent}</div>);
 
   return (
     <li key={'id' in item ? item.id : item.title}>
