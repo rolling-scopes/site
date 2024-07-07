7f672557b34ff269d788af320f23ce9bb482a5cb
Sergey <seygorin@gmail.com>
2024-07-07 10:37:24 +0500

    refactor: 376 - update tests for section-label

--- src/shared/ui/section-label/section-label.test.tsx
+++ src/shared/ui/section-label/section-label.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
-import { SectionLabel } from './section-label';
+import { SectionLabel, cx } from './section-label';
 
 describe('SectionLabel component', () => {
   const props = { label: 'Test Label' };
@@ -20,5 +20,40 @@ describe('SectionLabel component', () => {
     const label = screen.getByText('Test Label');
 
     expect(label).toBeInTheDocument();
+  });
+
+  it('applies default variants correctly', () => {
+    render(<SectionLabel label="Default Variant Label" />);
+    const labelElement = screen.getByText('Default Variant Label');
+
+    expect(labelElement).toHaveClass(cx('margin-large'));
+    expect(labelElement).toHaveClass(cx('font-large'));
+  });
+
+  it('applies custom marginSize variant correctly', () => {
+    render(<SectionLabel label="Custom Margin" marginSize="small" />);
+    const labelElement = screen.getByText('Custom Margin');
+
+    expect(labelElement).toHaveClass(cx('margin-small'));
+  });
+
+  it('applies custom fontSize variant correctly', () => {
+    render(<SectionLabel label="Custom Font" fontSize="small" />);
+    const labelElement = screen.getByText('Custom Font');
+
+    expect(labelElement).toHaveClass(cx('font-small'));
+  });
+
+  it('merges className correctly', () => {
+    render(
+      <SectionLabel
+        label="Custom Class"
+        className="custom-class"
+      />,
+    );
+    const labelElement = screen.getByText('Custom Class');
+
+    expect(labelElement).toHaveClass(cx('label'));
+    expect(labelElement).toHaveClass('custom-class');
   });
 });
