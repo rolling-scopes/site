73011f22f767fcef4c3517fa81e21d01afc5e03a
Sergey <seygorin@gmail.com>
2024-07-07 09:47:57 +0500

    refactor: 376 - section-label component

--- src/shared/ui/section-label/section-label.tsx
+++ src/shared/ui/section-label/section-label.tsx
@@ -1,9 +1,49 @@
-import './section-label.scss';
+import { HTMLAttributes } from 'react';
+import { type VariantProps, cva } from 'class-variance-authority';
+import classNames from 'classnames/bind';
 
-type SectionLabelProps = {
-  label: string;
+import styles from './section-label.module.scss';
+
+type SectionLabelProps = HTMLAttributes<HTMLDivElement> &
+  VariantProps<typeof sectionLabelVariants> & { label: string };
+
+export const cx = classNames.bind(styles);
+
+const sectionLabelVariants = cva(cx('label'), {
+  variants: {
+    marginSize: {
+      small: cx('margin-small'),
+      medium: cx('margin-medium'),
+      large: cx('margin-large'),
+    },
+    fontSize: {
+      small: cx('font-small'),
+      large: cx('font-large'),
+    },
+  },
+  defaultVariants: {
+    marginSize: 'large',
+    fontSize: 'large',
+  },
+});
+
+export const SectionLabel = ({
+  label,
+  marginSize,
+  fontSize,
+  className,
+  ...props
+}: SectionLabelProps) => {
+  return (
+    <div
+      {...props}
+      className={sectionLabelVariants({
+        marginSize,
+        fontSize,
+        className,
+      })}
+    >
+      {label}
+    </div>
+  );
 };
-
-export const SectionLabel = ({ label }: SectionLabelProps) => (
-  <div className="section-label">{label}</div>
-);
