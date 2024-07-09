fda70110664b57a5b2c5d67f9b1b5a8c33b0b57e
Wystov <wystov@gmail.com>
2024-07-09 02:29:11 +0200

    test: add tests for getActualDataList

---
+++ src/shared/helpers/getActualDataList.test.ts
@@ -0,0 +1,108 @@
+import { describe, expect, it } from 'vitest';
+import { getActualDataList } from './getActualDataList';
+import { dayJS } from '@/app/services/dayjs';
+import { Course } from '@/app/types';
+import { isCourse } from '@/entities/courses/helpers/is-course';
+import { EventCardProps } from '@/entities/events';
+
+const staleAfterDays = 14;
+
+const dayInFuture = dayJS().add(2, 'month').toISOString();
+const nonStaleDayInpast = dayJS().subtract(1, 'day').toISOString();
+const staleDayInPast = dayJS().subtract(staleAfterDays + 100, 'day').toISOString();
+
+const coursesMock: Course[] = [
+  {
+    id: '1',
+    title: 'title',
+    altTitle: 'altTitle',
+    iconSrc: 'icon',
+    iconSmall: 'iconSmall',
+    secondaryIcon: 'secondaryIcon',
+    startDate: staleDayInPast,
+    language: ['ru'],
+    mode: 'online',
+    detailsUrl: `url`,
+    enroll: 'enroll',
+    backgroundStyle: {
+      backgroundColor: 'color',
+      accentColor: 'color',
+    },
+  },
+  {
+    id: '2',
+    title: 'title',
+    altTitle: 'altTitle',
+    iconSrc: 'icon',
+    iconSmall: 'iconSmall',
+    secondaryIcon: 'secondaryIcon',
+    startDate: dayInFuture,
+    language: ['ru'],
+    mode: 'online',
+    detailsUrl: `url`,
+    enroll: 'enroll',
+    backgroundStyle: {
+      backgroundColor: 'color',
+      accentColor: 'color',
+    },
+  },
+  {
+    id: '3',
+    title: 'title',
+    altTitle: 'altTitle',
+    iconSrc: 'icon',
+    iconSmall: 'iconSmall',
+    secondaryIcon: 'secondaryIcon',
+    startDate: nonStaleDayInpast,
+    language: ['ru'],
+    mode: 'online',
+    detailsUrl: `url`,
+    enroll: 'enroll',
+    backgroundStyle: {
+      backgroundColor: 'color',
+      accentColor: 'color',
+    },
+  },
+];
+
+const eventsMock = [
+  { date: staleDayInPast },
+  { date: dayInFuture },
+  { date: nonStaleDayInpast },
+] as unknown as EventCardProps[];
+
+describe('getActualDataList', () => {
+  it('can handle courses and filters stale by default', () => {
+    const data = getActualDataList({
+      dataList: coursesMock,
+      actualDelayInDays: staleAfterDays,
+    }).filter(isCourse);
+
+    const noTBDinCourses = !data.some((course) => course.startDate === 'TBD');
+
+    expect(noTBDinCourses).toBeTruthy();
+  });
+
+  it('can handle events and filters stale by default', () => {
+    const data = getActualDataList({
+      dataList: eventsMock,
+      actualDelayInDays: staleAfterDays,
+    });
+
+    const noTBDinEvents = !data.some((event) => 'date' in event && event.date === 'TBD');
+
+    expect(noTBDinEvents).toBeTruthy();
+  });
+
+  it('sorts by date ascending and puts TBD in end', () => {
+    const data = getActualDataList({
+      dataList: coursesMock,
+      actualDelayInDays: staleAfterDays,
+      filtered: false,
+    }).filter(isCourse);
+
+    expect(data[0].startDate).toBe(nonStaleDayInpast);
+    expect(data[1].startDate).toBe(dayInFuture);
+    expect(data[2].startDate).toBe('TBD');
+  });
+});
