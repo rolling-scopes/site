58e08598bec9e490a572c6d240e6d483ce59333f
Wystov <wystov@gmail.com>
2024-07-09 02:27:43 +0200

    refactor: use dayjs method for calculation

--- src/shared/helpers/getCourseDate.ts
+++ src/shared/helpers/getCourseDate.ts
@@ -1,8 +1,9 @@
 import dayjs from 'dayjs';
 
 export const getCourseDate = (startDate: string, staleAfterDays: number) => {
+  const staleDate = dayjs().subtract(staleAfterDays, 'day');
   const isValidDate = dayjs(startDate).isValid();
-  const isStale = !isValidDate || dayjs(startDate) < dayjs().subtract(staleAfterDays, 'day');
+  const isStale = !isValidDate || dayjs(startDate).isBefore(staleDate);
 
   return isStale ? 'TBD' : startDate;
 };
