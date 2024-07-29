35e7d2a993c8edb7c50eb4a26e39aaceb7324f95
thirdmadman <thirdmadman@gmail.com>
2024-07-29 18:38:23 +0200

    fix: 313 - correct display of partner icons

--- src/app/layouts/base-layout/components/partnered/partnered.tsx
+++ src/app/layouts/base-layout/components/partnered/partnered.tsx
@@ -8,10 +8,18 @@ export const Partnered = () => (
     <div className="partnered content">
       <WidgetTitle size="small">Partnered with</WidgetTitle>
       <div className="partners">
-        <EpamLogo />
-        <JetBrainsLogo />
-        <AwsLogo />
-        <GithubLogo />
+        <figure className="partner-logo-container">
+          <EpamLogo />
+        </figure>
+        <figure className="partner-logo-container">
+          <JetBrainsLogo />
+        </figure>
+        <figure className="partner-logo-container">
+          <AwsLogo />
+        </figure>
+        <figure className="partner-logo-container">
+          <GithubLogo />
+        </figure>
       </div>
     </div>
   </div>
