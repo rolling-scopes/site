b4f95962a4496573ef43bd97699d77771c0d8836
thirdmadman <thirdmadman@gmail.com>
2024-08-07 21:13:00 +0200

    fix: 313 - change figure to div

--- src/app/layouts/base-layout/components/partnered/partnered.tsx
+++ src/app/layouts/base-layout/components/partnered/partnered.tsx
@@ -8,18 +8,18 @@ export const Partnered = () => (
     <div className="partnered content">
       <WidgetTitle size="small">Partnered with</WidgetTitle>
       <div className="partners">
-        <figure className="partner-logo-container">
+        <div className="partner-logo-container">
           <EpamLogo />
-        </figure>
-        <figure className="partner-logo-container">
+        </div>
+        <div className="partner-logo-container">
           <JetBrainsLogo />
-        </figure>
-        <figure className="partner-logo-container">
+        </div>
+        <div className="partner-logo-container">
           <AwsLogo />
-        </figure>
-        <figure className="partner-logo-container">
+        </div>
+        <div className="partner-logo-container">
           <GithubLogo />
-        </figure>
+        </div>
       </div>
     </div>
   </div>
