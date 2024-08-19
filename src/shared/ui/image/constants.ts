6cbef0e08756bbcabd851cecab8d13af1df36730
Bohdan Shcherbyna <bshcherbyna@investorlift.com>
2024-08-19 16:22:52 +0300

    refactor: 452 - delete unnecessary constants & types files

--- src/shared/ui/image/constants.ts
+++
@@ -1,4 +0,0 @@
-export const TABLET_W = import.meta.env.VITE_TABLET;
-export const MOBILE_W = import.meta.env.VITE_MOBILE;
-export const DESKTOP_W = 1280;
-export const IS_DEV = import.meta.env.DEV;
