6e6507c2a9885df6f5803ee253eec2b582aba907
Bohdan Shcherbyna <bshcherbyna@investorlift.com>
2024-08-19 16:21:58 +0300

    refactor: 452 - move helpers to shared helpers

--- src/shared/helpers/generateSrcSet.ts
+++ src/shared/helpers/generateSrcSet.ts
@@ -1,15 +1,13 @@
-import { DESKTOP_W, MOBILE_W, TABLET_W } from '../constants';
+import { DESKTOP_W, MOBILE_W, TABLET_W } from '@/shared/constants';
 
 /**
  * Generates the srcset for the given src image, based on the env breakpoints
  * @param {string} src - An src value to generate the srcset from
  * @returns {string} - Returns the processed srcset
  */
-const generateSrcSet = (src: string) => {
+export const generateSrcSet = (src: string) => {
   const srcNoExtension = src.slice(0, src.lastIndexOf('.'));
   const srcSet = `${srcNoExtension}-${MOBILE_W}.webp ${MOBILE_W}w, ${srcNoExtension}-${TABLET_W}.webp ${TABLET_W}w, ${src} ${DESKTOP_W}w`;
 
   return srcSet;
 };
-
-export default generateSrcSet;
