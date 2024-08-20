24706ff4179a84deb7f54ab8daf0129f0a522d3d
Wystov <wystov@gmail.com>
2024-08-20 17:29:35 +0200

    feat: 268 - change styles and move to module.scss

--- src/widgets/mobile-view/ui/mobile-view.tsx
+++ src/widgets/mobile-view/ui/mobile-view.tsx
@@ -1,16 +1,19 @@
+import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '@/app/const';
 import { Logo } from '@/shared/ui/logo';
 import { SchoolMenu } from '@/widgets/school-menu';
 
-import './mobile-view.scss';
+import styles from './mobile-view.module.scss';
+
+const cx = classNames.bind(styles);
 
 interface MobileViewProps {
   type: 'header' | 'footer';
 }
 
 const Divider = ({ type }: MobileViewProps) => (
-  <div className={`divider ${type === 'header' ? 'dark' : 'light'}`} />
+  <div className={cx('divider', type === 'header' ? 'dark' : 'light')} />
 );
 
 export const MobileView = ({ type }: MobileViewProps) => {
@@ -18,14 +21,14 @@ export const MobileView = ({ type }: MobileViewProps) => {
   const logoView = type === 'header' ? null : 'with-border';
 
   return (
-    <div className="mobile-view" data-testid="mobile-view">
+    <div className={cx('mobile-view')} data-testid="mobile-view">
       <Link to={ROUTES.HOME}>
         <Logo type={logoView} />
       </Link>
 
       <Divider type={type} />
 
-      <Link to={ROUTES.HOME} className={`main-link ${color}`}>
+      <Link to={ROUTES.HOME} className={cx('category-link', color)}>
         RS School
       </Link>
 
@@ -33,7 +36,7 @@ export const MobileView = ({ type }: MobileViewProps) => {
 
       <Divider type={type} />
 
-      <Link to={ROUTES.COURSES} className={`main-link ${color}`}>
+      <Link to={ROUTES.COURSES} className={cx('category-link', color)}>
         Courses
       </Link>
 
@@ -41,7 +44,7 @@ export const MobileView = ({ type }: MobileViewProps) => {
 
       <Divider type={type} />
 
-      <Link to={ROUTES.COMMUNITY} className={`main-link mt ${color}`}>
+      <Link to={ROUTES.COMMUNITY} className={cx('category-link', color)}>
         Community
       </Link>
 
