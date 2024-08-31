acf5b0b4095b82445479b85c198566821fbd4f2b
Wystov <wystov@gmail.com>
2024-08-31 02:51:17 +0200

    fix: 282 - tests to render with router

--- src/shared/ui/social-media/social-media.test.tsx
+++ src/shared/ui/social-media/social-media.test.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { SocialMedia } from './social-media';
+import { renderWithRouter } from '@/shared/__tests__/utils';
 
 describe('SocialMedia component', () => {
   const mockIcon: ReactNode = <div>mockIcon</div>;
@@ -13,44 +14,44 @@ describe('SocialMedia component', () => {
   };
 
   it('renders without crashing', () => {
-    render(<SocialMedia {...props} />);
+    renderWithRouter(<SocialMedia {...props} />);
     const linkElement = screen.getByRole('link');
 
     expect(linkElement).toBeInTheDocument();
   });
 
   it('renders correct icon', () => {
-    render(<SocialMedia {...props} />);
+    renderWithRouter(<SocialMedia {...props} />);
     const icon = screen.getByText('mockIcon');
 
     expect(icon).toBeInTheDocument();
   });
 
   it('displays correct title', () => {
-    render(<SocialMedia {...props} />);
+    renderWithRouter(<SocialMedia {...props} />);
     const title = screen.getByText('TestSocialMedia');
 
     expect(title).toBeInTheDocument();
   });
 
   it('has correct href', () => {
-    render(<SocialMedia {...props} />);
+    renderWithRouter(<SocialMedia {...props} />);
     const linkElement = screen.getByRole('link');
 
     expect(linkElement).toHaveAttribute('href', 'https://test.com');
   });
 
   it('opens link in new window', () => {
-    render(<SocialMedia {...props} />);
+    renderWithRouter(<SocialMedia {...props} />);
     const linkElement = screen.getByRole('link');
 
     expect(linkElement).toHaveAttribute('target', '_blank');
   });
 
   it('has no referrer', () => {
-    render(<SocialMedia {...props} />);
+    renderWithRouter(<SocialMedia {...props} />);
     const linkElement = screen.getByRole('link');
 
-    expect(linkElement).toHaveAttribute('rel', 'noreferrer');
+    expect(linkElement).toHaveAttribute('rel', 'noopener noreferrer');
   });
 });
