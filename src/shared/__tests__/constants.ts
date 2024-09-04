ed3a5c79ed4f4b587cb03b7a09aa56107ff27c88
thirdmadman <thirdmadman@gmail.com>
2024-09-04 19:19:47 +0200

    test: 473 - add test for TrainerCard

--- src/shared/__tests__/constants.ts
+++ src/shared/__tests__/constants.ts
@@ -2,7 +2,7 @@ import nodejsImg1 from '@/shared/assets/mentors/m-shylau.webp';
 import type { Trainer } from '@/entities/trainer/types';
 
 export const MOCKED_IMAGE_PATH = 'mocked-image-path.webp';
-const MOCKED_TRAINER = {
+export const MOCKED_TRAINER = {
   name: 'Maksim Shylau',
   role: 'Senior Software Engineer at Epam',
   bio: "Maksim Shylau is a professional with around 6 years of programming experience. Initially a hobby, programming evolved into Maxim's current profession as a full-stack developer (proficient in JavaScript, TypeScript, React, Node.js, and AWS) at EPAM Systems, where he holds the position of Senior Software Engineer. Maksim actively contributes to RS School in his leisure time by leading a Node.js course, delivering lectures, creating educational tasks, and participating in various events. He is committed to continuous learning, constantly exploring new technologies, and extends his passion to helping students master web development.",
