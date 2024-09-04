0d57e2b2e2d42d07b024b087b950a31934ed41ce
MielomankA <79061120+MielomankA@users.noreply.github.com>
2024-09-04 22:46:07 +0300

    459-fix: Display logo (#461)
    * fix: 459 - change styles for logo
    
    * refactor: 459 - update logo component

--- src/shared/ui/logo/logo.test.tsx
+++ src/shared/ui/logo/logo.test.tsx
@@ -1,24 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 import { Logo, cx } from './logo.tsx';
 
 describe('Logo component', () => {
   it('renders without crashing', () => {
-    render(<Logo />);
+    render(<Logo />, { wrapper: BrowserRouter });
     const altLogoText = screen.getByAltText('RSS-logo');
 
     expect(altLogoText).toBeInTheDocument();
   });
 
   it('displays correctly when not have type', () => {
-    render(<Logo />);
+    render(<Logo />, { wrapper: BrowserRouter });
     const element = screen.getByTestId('logo');
 
     expect(element).not.toHaveClass(cx('with-border'));
   });
 
   it('displays correctly when type=with-border', () => {
-    render(<Logo type="with-border" />);
+    render(<Logo type="with-border" />, { wrapper: BrowserRouter });
     const element = screen.getByTestId('logo');
 
     expect(element).toHaveClass(cx('with-border'));
