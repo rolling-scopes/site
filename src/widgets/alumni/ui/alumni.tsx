a41f7c407ec204c3f5dcc3181582e0b1babd627a
Wystov <wystov@gmail.com>
2024-09-06 03:22:12 +0200

    refactor: 499 - use css for content amount istead of js

--- src/widgets/alumni/ui/alumni.tsx
+++ src/widgets/alumni/ui/alumni.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames/bind';
 import { alumni } from '../constants';
-import { useWindowSize } from '@/shared/hooks/use-window-size';
 import { Image } from '@/shared/ui/image';
 import { Paragraph } from '@/shared/ui/paragraph';
 import { WidgetTitle } from '@/shared/ui/widget-title';
@@ -10,28 +9,16 @@ import styles from './alumni.module.scss';
 const cx = classNames.bind(styles);
 
 export const Alumni = () => {
-  const size = useWindowSize();
-
-  let alumniArr = [];
-
-  if (size.width <= 1440 && size.width > 810) {
-    alumniArr = alumni.slice(0, 12);
-  } else if (size.width <= 810) {
-    alumniArr = alumni.slice(0, 6);
-  } else {
-    alumniArr = [...alumni];
-  }
-
   return (
     <article className={cx('container')}>
-      <section className={cx('content')}>
+      <section className={cx('content', 'info-wrapper')}>
         <WidgetTitle mods="asterisk">Our alumni</WidgetTitle>
         <Paragraph>
           We are immensely proud of RS School alumni who build their successful careers in ambitious
           IT companies
         </Paragraph>
         <section className={cx('alumni')}>
-          {alumniArr.map(({ id, image }) => (
+          {alumni.map(({ id, image }) => (
             <figure key={id} className={cx('logo-container')}>
               <Image className={cx('logo')} src={image} alt={id} />
             </figure>
