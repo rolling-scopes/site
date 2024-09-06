512719073ec3b75887db01c124bc8dcc8488cbdb
Wystov <wystov@gmail.com>
2024-09-06 20:07:34 +0200

    refactor: 499 - remove unit tests for old functionality

--- src/widgets/alumni/ui/alumni.test.tsx
+++ src/widgets/alumni/ui/alumni.test.tsx
@@ -1,15 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import { Mock, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { Alumni } from './alumni';
-import { useWindowSize } from '@/shared/hooks/use-window-size';
-
-vi.mock('@/shared/hooks/use-window-size', () => ({
-  useWindowSize: vi.fn().mockReturnValue({
-    width: 2000,
-    height: 2000,
-  }),
-}),
-);
 
 describe('Alumni', () => {
   it('renders the title correctly', () => {
@@ -24,40 +15,5 @@ describe('Alumni', () => {
     const paragraphElement = screen.getByText(/We are immensely proud of RS School alumni/i);
 
     expect(paragraphElement).toBeInTheDocument();
-  });
-
-  it('renders six images for small screens', () => {
-    // vi.mockImplementation(() => ({ width: 800, height: 600 }));
-    (useWindowSize as Mock).mockReturnValue({
-      width: 800,
-      height: 600,
-    });
-    render(<Alumni />);
-    const imageElements = screen.getAllByRole('img');
-
-    expect(imageElements).toHaveLength(6);
-  });
-
-  it('renders twelve images for medium screens', () => {
-    (useWindowSize as Mock).mockReturnValue({
-      width: 1440,
-      height: 900,
-    });
-    render(<Alumni />);
-    const imageElements = screen.getAllByRole('img');
-
-    expect(imageElements).toHaveLength(12);
-  });
-
-  it('renders all images for large screens', () => {
-    (useWindowSize as Mock).mockReturnValue({
-      width: 2000,
-      height: 2000,
-    });
-    render(<Alumni />);
-
-    const imageElements = screen.getAllByRole('img');
-
-    expect(imageElements).toHaveLength(18);
   });
 });
