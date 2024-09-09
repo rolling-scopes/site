af6f9e811d6ae54cdd0e301e527b493cd2d9f9d0
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-09-09 11:38:43 +0300

    fix: 338 - update include&exclude arrays

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
-      '**/cypress/**',
-      '**/__tests__/visual',
+      '**/__tests__/**',
       '**/.{idea,git,cache,output,temp}/**',
       '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
     ],
