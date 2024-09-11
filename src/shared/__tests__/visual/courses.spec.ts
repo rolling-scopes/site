fd6ab990836baa5988726643cb9fc3cd95f8c187
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-09-11 09:17:56 +0300

    fix: 338 - use constants for urls

--- src/shared/__tests__/visual/courses.spec.ts
+++ src/shared/__tests__/visual/courses.spec.ts
@@ -1,56 +1,57 @@
 import { test } from '@playwright/test';
 import { takeScreenshot } from './utils';
+import { ROUTES } from '@/app/const';
 
 test('Courses main page', async ({ page }) => {
-  await page.goto('/courses');
+  await page.goto(ROUTES.COURSES);
 
   await takeScreenshot(page, 'Courses main page');
 });
 
 test('Courses javascript-preschool-ru page', async ({ page }) => {
-  await page.goto('/courses/javascript-preschool-ru');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.JS_PRESCHOOL_RU}`);
 
   await takeScreenshot(page, 'Courses javascript-preschool-ru page');
 });
 
 test('Courses javascript-en page', async ({ page }) => {
-  await page.goto('/courses/javascript');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.JS}`);
 
   await takeScreenshot(page, 'Courses javascript-en page');
 });
 
 test('Courses javascript-ru page', async ({ page }) => {
-  await page.goto('/courses/javascript-ru');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.JS_RU}`);
 
   await takeScreenshot(page, 'Courses javascript-ru page');
 });
 
 test('Courses react page', async ({ page }) => {
-  await page.goto('/courses/reactjs');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.REACT}`);
 
   await takeScreenshot(page, 'Courses react page');
 });
 
 test('Courses angular page', async ({ page }) => {
-  await page.goto('/courses/angular');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.ANGULAR}`);
 
   await takeScreenshot(page, 'Courses angular page');
 });
 
 test('Courses aws-fundamentals page', async ({ page }) => {
-  await page.goto('/courses/aws-fundamentals');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.AWS_FUNDAMENTALS}`);
 
   await takeScreenshot(page, 'Courses aws-fundamentals page');
 });
 
 test('Courses aws-cloud-developer page', async ({ page }) => {
-  await page.goto('/courses/aws-cloud-developer');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.AWS_DEVELOPER}`);
 
   await takeScreenshot(page, 'Courses aws-cloud-developer page');
 });
 
 test('Courses aws-devops page', async ({ page }) => {
-  await page.goto('/courses/aws-devops');
+  await page.goto(`${ROUTES.COURSES}/${ROUTES.AWS_DEVOPS}`);
 
   await takeScreenshot(page, 'Courses aws-devops page');
 });
