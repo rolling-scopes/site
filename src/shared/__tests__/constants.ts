bafb54d13e29ed87db302a259d4ba0f54e8f5e69
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-09-12 16:24:29 +0300

    fix: visual testing workflow (#514)
    * fix: visual testing workflow
    
    * fix: eslint errors

--- src/shared/__tests__/constants.ts
+++ src/shared/__tests__/constants.ts
@@ -1,11 +1,11 @@
-import nodejsImg1 from '@/shared/assets/mentors/m-shylau.webp';
 import type { Trainer } from '@/entities/trainer';
+import nodejsImg1 from '@/shared/assets/mentors/m-shylau.webp';
 
 export const MOCKED_IMAGE_PATH = 'mocked-image-path.webp';
 export const MOCKED_TRAINER = {
   name: 'Max Power',
   role: 'Executive Pastry Chef at The Cloud Cafe',
-  bio: "Max Power is a pastry master with a passion for crafting sweet treats. With over 7 years of experience whipping up delicious pastries and desserts, Max has honed their skills as the Head Pastry Chef on the prestigious Cloud Cafe. When not busy creating new recipes, Max enjoys leading baking classes and participating in friendly cooking competitions.",
+  bio: 'Max Power is a pastry master with a passion for crafting sweet treats. With over 7 years of experience whipping up delicious pastries and desserts, Max has honed their skills as the Head Pastry Chef on the prestigious Cloud Cafe. When not busy creating new recipes, Max enjoys leading baking classes and participating in friendly cooking competitions.',
   photo: nodejsImg1,
 };
 
