fc2644e93b28fa4dbbbe4b6eb3b36a8996153319
Kristina <93883470+KristiBo@users.noreply.github.com>
2024-09-12 13:57:43 +0300

    feat: 511 - change course titles (#512)

--- src/widgets/breadcrumbs/constants.ts
+++ src/widgets/breadcrumbs/constants.ts
@@ -3,13 +3,13 @@ import { BreadcrumbNameMap } from './breadcrumbs.types';
 export const breadcrumbNameMap: BreadcrumbNameMap = {
   courses: 'Courses',
   nodejs: 'Node.js Course',
-  javascript: 'JavaScript Mentoring Program',
-  'javascript-ru': 'JavaScript Mentoring Program RU',
+  javascript: 'JavaScript Course',
+  'javascript-ru': 'JavaScript Course RU',
   'javascript-preschool-ru': 'JavaScript Pre-school RU',
   angular: 'Angular Course',
   'aws-cloud-developer': 'AWS Cloud Developer Course',
   'aws-fundamentals': 'AWS Fundamentals Course',
-  reactjs: 'React course',
+  reactjs: 'React Course',
   community: 'Community',
   'aws-devops': 'AWS DevOps',
 } as const;
--- src/widgets/course-main/course-main.tsx
+++ src/widgets/course-main/course-main.tsx
@@ -18,7 +18,7 @@ import styles from './course-main.module.scss';
 interface CourseMainProps {
   courseName: string;
   lang?: 'ru' | 'en';
-  type?: 'Mentoring Program EN' | 'Pre-school RU' | 'Менторская программа RU';
+  type?: 'Pre-school RU';
 }
 
 const localizedContent = {
