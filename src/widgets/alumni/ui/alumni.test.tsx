82a19769c15e7f60331dbf5e93ebbfc9194ec388
Wystov <wystov@gmail.com>
2024-09-13 07:32:46 +0200

    refactor: 499 - review suggestions implemented

--- src/widgets/alumni/ui/alumni.test.tsx
+++ src/widgets/alumni/ui/alumni.test.tsx
@@ -3,17 +3,25 @@ import { describe, expect, it } from 'vitest';
 import { Alumni } from './alumni';
 
 describe('Alumni', () => {
-  it('renders the title correctly', () => {
+  beforeEach(() => {
     render(<Alumni />);
+  });
+
+  it('renders the title correctly', () => {
     const titleElement = screen.getByText('Our alumni');
 
     expect(titleElement).toBeInTheDocument();
   });
 
   it('renders the paragraph correctly', () => {
-    render(<Alumni />);
     const paragraphElement = screen.getByText(/We are immensely proud of RS School alumni/i);
 
     expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it('renders all images for large screens', () => {
+    const imageElements = screen.getAllByRole('img');
+
+    expect(imageElements).toHaveLength(18);
   });
 });
