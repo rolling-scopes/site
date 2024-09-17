25f548f0fc5baf7a6aab205e1eab8ac6bc97567c
thirdmadman <thirdmadman@gmail.com>
2024-09-17 20:07:30 +0200

    refactor: 521 - move locales to separate file

---
+++ src/widgets/communication/ui/locales.ts
@@ -0,0 +1,31 @@
+export const communicationSectionLocales = {
+  en: {
+    title: 'Communication',
+    subTitle: 'Discord is the main communication channel in RS School',
+    firstParagraphFirstHalf: 'Here is link for the',
+    discordLink: 'course Discord server',
+    firstParagraphSecondHalf: ', where you can see latest news and chat with students.',
+    secondParagraphFirstHalf: 'There are channels in',
+    telegramLink: 'Telegram',
+    secondParagraphSecondHalf:
+      ' for discussing events related to your location. For example, offline lectures or just informal chats among students from the same location.',
+    thirdParagraphFirstHalf: 'Please read the information about communication in RS School in the',
+    rsDocsLink: 'RS Docs',
+    thirdParagraphSecondHalf: ', where you can find rules, descriptions of channels, FAQ.',
+  },
+  ru: {
+    title: 'Общение',
+    subTitle: 'Дискорд — основной способ общения в RS School',
+    firstParagraphFirstHalf: 'Вот ссылка на',
+    discordLink: 'Дискорд сервер курса',
+    firstParagraphSecondHalf:
+      ', где вы можете посмотреть последние новости, задать вопросы и общаться со студентами.',
+    secondParagraphFirstHalf: 'Также есть каналы в',
+    telegramLink: 'Телеграм',
+    secondParagraphSecondHalf:
+      ' для обсуждения мероприятий, относящихся к вашему городу. Например, офлайн лекции или просто для общения студентов из одной локации.',
+    thirdParagraphFirstHalf: 'Обязательно прочитайте информацию об общении в RS School в',
+    rsDocsLink: 'RS Docs',
+    thirdParagraphSecondHalf: ', где вы можете найти правила, описание каналов, FAQ.',
+  },
+};
