1ac311ab535cadddad1446f0a136434567ef2135
YulikK <yulikpoch@gmail.com>
2024-09-24 10:24:56 +0200

    refactor: 533 - move title to localization variable

--- src/widgets/courses/ui/courses.tsx
+++ src/widgets/courses/ui/courses.tsx
@@ -7,7 +7,14 @@ import { WidgetTitle } from '@/shared/ui/widget-title';
 
 import './courses.scss';
 
-export const Courses = () => {
+type CoursesProps = { lang?: 'en' | 'ru' };
+
+const localizedContent = {
+  en: { title: 'All courses' },
+  ru: { title: 'Все курсы' },
+};
+
+export const Courses = ({ lang = 'en' }: CoursesProps) => {
   const { data: courses, loading, error } = useDataByName('courses');
 
   if (loading) {
@@ -31,7 +38,7 @@ export const Courses = () => {
   return (
     <section className="rs-courses container" id="upcoming-courses">
       <div className="rs-courses content">
-        <WidgetTitle>All courses</WidgetTitle>
+        <WidgetTitle>{localizedContent[lang].title}</WidgetTitle>
         <div className="rs-courses-wrapper" data-testid="courses-fancy">
           {sortedCourses.map((course) => {
             return <CourseCard key={course.id} {...course} />;
