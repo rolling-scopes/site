5292058bcf38b9922d5da15926d7fe6c593ecb8b
thirdmadman <thirdmadman@gmail.com>
2024-09-24 21:52:39 +0200

    test: 521 - refactor and update

--- src/widgets/communication/ui/communication.test.tsx
+++ src/widgets/communication/ui/communication.test.tsx
@@ -4,60 +4,50 @@ import { renderWithRouter } from '@/shared/__tests__/utils';
 import { DISCORD_LINKS, communicationText } from 'data';
 
 describe('Communication section', () => {
-  it('should render "Communication" section texts correctly', async () => {
-    const firstCourse = Object.keys(DISCORD_LINKS)[0] as keyof typeof DISCORD_LINKS;
+  it.each([
+    {
+      languageProp: undefined,
+      texts: communicationText.en,
+    },
+    {
+      languageProp: 'en',
+      texts: communicationText.en,
+    },
+    {
+      languageProp: 'ru',
+      texts: communicationText.ru,
+    },
+  ])(
+    'should render component correctly with $languageProp language prop',
+    ({ languageProp, texts }) => {
+      const firstCourse = Object.keys(DISCORD_LINKS)[0] as keyof typeof DISCORD_LINKS;
 
-    const {
-      title,
-      subTitle,
-      firstParagraphFirstHalf,
-      discordLink,
-    } = communicationText.en;
+      const { title, subTitle, firstParagraphFirstHalf, discordLink } = texts;
 
-    renderWithRouter(<Communication courseName={firstCourse} />);
-    const titleElement = screen.getByText(title);
-    const subtitleElement = screen.getByText(subTitle);
-    const firstParagraphElement = screen.getByText(`${firstParagraphFirstHalf}`, { exact: false });
-    const linkElement = screen.getByText(discordLink);
+      renderWithRouter(<Communication courseName={firstCourse} lang={languageProp as 'en' | 'ru' | undefined} />);
+      const titleElement = screen.getByText(title);
+      const subtitleElement = screen.getByText(subTitle);
+      const firstParagraphElement = screen.getByText(`${firstParagraphFirstHalf}`, { exact: false });
+      const linkElement = screen.getByText(discordLink);
 
-    expect(titleElement).toBeVisible();
-    expect(subtitleElement).toBeVisible();
-    expect(firstParagraphElement).toBeVisible();
-    expect(linkElement).toBeVisible();
-    expect(linkElement.getAttribute('href')).toMatch(DISCORD_LINKS[firstCourse]);
-  });
+      expect(titleElement).toBeVisible();
+      expect(subtitleElement).toBeVisible();
+      expect(firstParagraphElement).toBeVisible();
+      expect(linkElement).toBeVisible();
+      expect(linkElement.getAttribute('href')).toMatch(DISCORD_LINKS[firstCourse]);
+      cleanup();
+    },
+  );
 
-  it('should render "Communication" section texts correctly on russian language', async () => {
-    const firstCourse = Object.keys(DISCORD_LINKS)[0] as keyof typeof DISCORD_LINKS;
-
-    const {
-      title,
-      subTitle,
-      firstParagraphFirstHalf,
-      discordLink,
-    } = communicationText.ru;
-
-    renderWithRouter(<Communication courseName={firstCourse} lang="ru" />);
-    const titleElement = screen.getByText(title);
-    const subtitleElement = screen.getByText(subTitle);
-    const firstParagraphElement = screen.getByText(`${firstParagraphFirstHalf}`, { exact: false });
-    const linkElement = screen.getByText(discordLink);
-
-    expect(titleElement).toBeVisible();
-    expect(subtitleElement).toBeVisible();
-    expect(firstParagraphElement).toBeVisible();
-    expect(linkElement).toBeVisible();
-    expect(linkElement.getAttribute('href')).toMatch(DISCORD_LINKS[firstCourse]);
-  });
-
-  Object.entries(DISCORD_LINKS).forEach(([courseName, link]) => {
-    it(`should render correct link of ${courseName}`, () => {
+  it.each(Object.entries(DISCORD_LINKS))(
+    'should render correct link of %s course',
+    (courseName, link) => {
       renderWithRouter(<Communication courseName={courseName as keyof typeof DISCORD_LINKS} />);
       const linkElement = screen.getByText(/course discord server/i);
 
       expect(linkElement).toBeVisible();
       expect(linkElement.getAttribute('href')).toMatch(link);
       cleanup();
-    });
-  });
+    },
+  );
 });
