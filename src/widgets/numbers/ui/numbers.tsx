bb5275439b43bed3c44adadb66c86724f54d997c
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-10-02 18:33:23 +0300

    refactor: 557 - change scss to modules

--- src/widgets/numbers/ui/numbers.tsx
+++ src/widgets/numbers/ui/numbers.tsx
@@ -1,9 +1,12 @@
+import classnames from 'classnames/bind';
 import image from '@/shared/assets/map.webp';
 import { Image } from '@/shared/ui/image';
 import { Paragraph } from '@/shared/ui/paragraph';
 import { WidgetTitle } from '@/shared/ui/widget-title';
 
-import './numbers.scss';
+import styles from './numbers.module.scss';
+
+const cx = classnames.bind(styles);
 
 interface InfoCellProps {
   title: string;
@@ -11,45 +14,43 @@ interface InfoCellProps {
 }
 
 const InfoCell = ({ title, description }: InfoCellProps) => (
-  <div className="info-cell">
-    <div className="number">{title}</div>
-    <div className="text">{description}</div>
+  <div className={cx('info-cell')}>
+    <div className={cx('number')}>{title}</div>
+    <div className={cx('text')}>{description}</div>
   </div>
 );
 
-const InfoCellDivider = () => <div className="info-divider" />;
+const InfoCellDivider = () => <div className={cx('info-divider')} />;
 
 export const Numbers = () => {
   return (
-    <div className="numbers container">
-      <div className="numbers content">
-        <div className="text-container">
-          <div>
-            <WidgetTitle mods="asterisk">The Rolling Scopes in numbers</WidgetTitle>
-            <Paragraph fontSize="large">
-              Everyone can discover our community, regardless of age, professional employment, or
-              place of residence.
-            </Paragraph>
-            <Paragraph>
-              Developers from different companies and countries are connected to pass on your
-              knowledge, enrich your network and just have fun.
-            </Paragraph>
-          </div>
+    <div className={cx('container')}>
+      <div className={cx('content')}>
+        <div className={cx('text-container')}>
+          <WidgetTitle mods="asterisk">The Rolling Scopes in numbers</WidgetTitle>
+          <Paragraph fontSize="large">
+            Everyone can discover our community, regardless of age, professional employment, or
+            place of residence.
+          </Paragraph>
+          <Paragraph>
+            Developers from different companies and countries are connected to pass on your
+            knowledge, enrich your network and just have fun.
+          </Paragraph>
         </div>
-        <div className="info">
-          <div className="left">
+        <div className={cx('info')}>
+          <div className={cx('left')}>
             <InfoCell title="62k+" description="members" />
             <InfoCellDivider />
             <InfoCell title="500+" description="events" />
             <InfoCellDivider />
           </div>
-          <div className="right">
+          <div className={cx('right')}>
             <InfoCell title="600+" description="videos on YouTube" />
             <InfoCellDivider />
             <InfoCell title="1800+" description="RS School alumni per year" />
           </div>
         </div>
-        <Image className="map" src={image} alt="map" />
+        <Image className={cx('map')} src={image} alt="map" />
       </div>
     </div>
   );
