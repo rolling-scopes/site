9989e26cbc295a65719f12fc7c6e250ab359475c
LinderJK <slepchenkovia@gmail.com>
2024-10-02 08:06:27 +0000

    refactor: 347 - rename events constant

--- src/widgets/events/constants.ts
+++ src/widgets/events/constants.ts
@@ -3,12 +3,12 @@ import { RS_FOUNDATION_YEAR } from '@/shared/constants';
 import { getActualData } from '@/shared/helpers/getActualData';
 import { events } from 'data';
 
-export const maxEventsCount = 2;
+export const maxUpcomingEventsQuantity = 2;
 export const actualEvents = getActualData({
   data: events,
   staleAfter: 3,
 });
 
-export const nearestEvents = actualEvents.slice(0, maxEventsCount);
+export const nearestEvents = actualEvents.slice(0, maxUpcomingEventsQuantity);
 
 export const rsLifetime = dayjs().diff(RS_FOUNDATION_YEAR, 'year');
