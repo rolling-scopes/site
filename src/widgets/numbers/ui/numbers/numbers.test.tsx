9df8aa217f55d20d07b3608de9d89c72af5ad034
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-10-03 14:44:13 +0300

    refactor: 557 - move numbers to separate ui folder

--- src/widgets/numbers/ui/numbers/numbers.test.tsx
+++ src/widgets/numbers/ui/numbers/numbers.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from 'vitest';
-import { Numbers } from './ui/numbers';
+import { Numbers } from '../numbers/numbers.tsx';
 
 describe('Numbers', () => {
   beforeEach(() => {
