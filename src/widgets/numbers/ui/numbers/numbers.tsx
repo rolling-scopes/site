37ae784b351d6a487c6a12d754979f8034c656bb
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-10-03 15:12:05 +0300

    refactor: 557 - remove unnecessary nesting

--- src/widgets/numbers/ui/numbers/numbers.tsx
+++ src/widgets/numbers/ui/numbers/numbers.tsx
@@ -12,31 +12,29 @@ const cx = classnames.bind(styles);
 
 export const Numbers = () => {
   return (
-    <section className={cx('container')}>
-      <div className={cx('numbers-content', 'content')}>
-        <article className={cx('text-container')}>
-          <WidgetTitle mods="asterisk">The Rolling Scopes in numbers</WidgetTitle>
-          <Paragraph fontSize="large">
-            Everyone can discover our community, regardless of age, professional employment, or
-            place of residence.
-          </Paragraph>
-          <Paragraph>
-            Developers from different companies and countries are connected to pass on your
-            knowledge, enrich your network and just have fun.
-          </Paragraph>
-        </article>
-        <div className={cx('numbers-info')}>
-          {rsInNumbers.map(({ id, title, description }) => (
-            <InfoCell key={id} title={title} description={description} />
-          ))}
-        </div>
-        <Image
-          className={cx('numbers-map')}
-          src={map}
-          alt="World map showing yellow dots marking various countries"
-          data-testid="numbers-map"
-        />
+    <section className={cx('numbers-content', 'content', 'container')}>
+      <article className={cx('text-container')}>
+        <WidgetTitle mods="asterisk">The Rolling Scopes in numbers</WidgetTitle>
+        <Paragraph fontSize="large">
+          Everyone can discover our community, regardless of age, professional employment, or place
+          of residence.
+        </Paragraph>
+        <Paragraph>
+          Developers from different companies and countries are connected to pass on your knowledge,
+          enrich your network and just have fun.
+        </Paragraph>
+      </article>
+      <div className={cx('numbers-info')}>
+        {rsInNumbers.map(({ id, title, description }) => (
+          <InfoCell key={id} title={title} description={description} />
+        ))}
       </div>
+      <Image
+        className={cx('numbers-map')}
+        src={map}
+        alt="World map showing yellow dots marking various countries"
+        data-testid="numbers-map"
+      />
     </section>
   );
 };
