bb70f468d160ba1535bc4f9777393de520e75b8d
KristiBo <kristina.boroh@gmail.com>
2024-10-04 20:05:50 +0300

    feat: 587 - update course trainers

--- dev-data/javascript-en.data.ts
+++ dev-data/javascript-en.data.ts
@@ -1,7 +1,14 @@
 import type { Trainer } from '@/entities/trainer';
 import andreiMilashevichImg from '@/shared/assets/mentors/a-milashevich.webp';
+import hannaPratasevichImg from '@/shared/assets/mentors/h-pratasevich.webp';
 
 export const javaScriptEn: Trainer[] = [
+  {
+    name: 'Hanna Pratasevich',
+    role: 'Software Engineering Team Leader @EPAM',
+    bio: 'Hello, I\'m Anna - the course coordinator. Ten years ago, I graduated from Rolling Scopes School. Shortly thereafter, I returned to the school, but this time as a mentor and lecturer. However, that still felt insufficient, so I became a course coordinator. Today, I am a Team Lead at EPAM, working as a front-end developer. My heart is devoted to Angular, but I also respect other frameworks/libraries. I look forward to meeting each of you and hope to see you after graduation, perhaps as colleagues on projects!',
+    photo: hannaPratasevichImg,
+  },
   {
     name: 'Andrei Milashevich',
     role: '',
