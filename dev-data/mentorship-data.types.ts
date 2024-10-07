23b6287eed1a02646a3b2702f2ef013f58b6bb3f
Mikhail Aleinik <6424156@tut.by>
2024-10-08 00:27:09 +0300

    feat: 580 - added types and data for mentorship

---
+++ dev-data/mentorship-data.types.ts
@@ -0,0 +1,23 @@
+import { COURSE_TITLES } from './courseTitles.data.ts';
+import { ROUTES } from '@/app/const';
+import { Stage } from '@/widgets/study-path/ui/stage-card';
+
+type MentorActions = Pick<Stage, 'id' | 'title' | 'description' | 'links'>;
+
+type CourseTitleKey = keyof typeof COURSE_TITLES;
+type CourseTitle = typeof COURSE_TITLES[CourseTitleKey];
+
+type RoutesKey = keyof typeof ROUTES;
+export type RouteName = typeof ROUTES[RoutesKey];
+
+export type MentorshipCourse = {
+  id: number;
+  courseTitle: CourseTitle;
+  lang: 'en' | 'ru';
+  pageUrl: RouteName;
+  description: string[];
+  mentorActivities: MentorActions[];
+  mentorDocsUrl: string;
+  courseDocsUrl: string;
+  benefits: string[];
+};
