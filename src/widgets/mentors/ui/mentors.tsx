147b8e83624b2b8310cb28768297a70f38ccd5a3
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2024-10-08 21:04:12 +0200

    refactor: 549 - change styles to scss module

--- src/widgets/mentors/ui/mentors.tsx
+++ src/widgets/mentors/ui/mentors.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames/bind';
 import { LINKS } from '@/app/const';
 import mentorImg from '@/shared/assets/mentors-wanted.webp';
 import { Image } from '@/shared/ui/image';
@@ -5,15 +6,17 @@ import { LinkCustom } from '@/shared/ui/link-custom';
 import { Paragraph } from '@/shared/ui/paragraph';
 import { WidgetTitle } from '@/shared/ui/widget-title';
 
-import './mentors.scss';
+import styles from './mentors.module.scss';
+
+const cx = classNames.bind(styles);
 
 export const Mentors = () => {
   return (
-    <section className="mentors container" id="mentors-wanted">
-      <div className="mentors content column-2">
-        <div className="mentors-info">
+    <section className={cx('container', 'mentors-container')} id="mentors-wanted">
+      <div className={cx('content', 'column-2', 'mentors-content')}>
+        <div className={cx('mentors-info')}>
           <WidgetTitle size="large" mods="lines">Mentors wanted!</WidgetTitle>
-          <Paragraph fontSize="large" className="paragraph">
+          <Paragraph fontSize="large" className={cx('paragraph')}>
             The Rolling Scopes School is constantly looking for mentors from all over the world to
             teach everyone who wants to learn the JavaScript language and the world of Front-end.
             Over the past few years, over 1500+ people have successfully completed our six month
@@ -27,7 +30,7 @@ export const Mentors = () => {
             Become a mentor
           </LinkCustom>
         </div>
-        <div className="picture">
+        <div className={cx('picture')}>
           <Image src={mentorImg} alt="Sloth - mascot dressed as a detective" />
         </div>
       </div>
