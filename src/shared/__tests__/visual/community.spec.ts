aa3885894c08e7b03cd1bd50e008b8ef1af18d1f
YulikK <yulikpoch@gmail.com>
2024-10-08 12:08:50 +0200

    refactor: 554 - delete tests file

--- src/shared/__tests__/visual/community.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { takeScreenshot } from './utils';
-import { ROUTES } from '@/core/const';
-
-test('Community page', async ({ page }) => {
-  await page.goto(ROUTES.COMMUNITY);
-
-  await takeScreenshot(page, 'Community page');
-});
