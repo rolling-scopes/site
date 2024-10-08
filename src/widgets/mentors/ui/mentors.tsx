a2a595b69ea6d752a9b993c8bea1a146113f269a
Mikhail Aleinik <6424156@tut.by>
2024-10-09 00:26:05 +0300

    refactor: 580 - added locale and mentorship url to mentors widget

--- src/widgets/mentors/ui/mentors.tsx
+++ src/widgets/mentors/ui/mentors.tsx
@@ -4,26 +4,41 @@ import { Image } from '@/shared/ui/image';
 import { LinkCustom } from '@/shared/ui/link-custom';
 import { Paragraph } from '@/shared/ui/paragraph';
 import { WidgetTitle } from '@/shared/ui/widget-title';
+import { RouteName } from 'data';
 
 import './mentors.scss';
 
-export const Mentors = () => {
+type MentorsProps = {
+  route?: RouteName;
+  lang?: 'en' | 'ru';
+};
+const textContent = {
+  en: {
+    info: 'The Rolling Scopes School is constantly looking for mentors from all over the world to teach everyone who wants to learn the JavaScript language and the world of Front-end. Over the past few years, over 1500+ people have successfully completed our six month training program.',
+    button: 'Read more',
+  },
+  ru: {
+    info: 'Школа Rolling Scopes постоянно ищет наставников со всего мира, чтобы обучать всех желающих языку JavaScript и миру Front-end. За последние несколько лет более 1500 человек успешно завершили нашу шестимесячную программу обучения.',
+    button: 'Подробнее',
+  },
+};
+
+export const Mentors = ({ route = ROUTES.MENTORSHIP, lang = 'en' }: MentorsProps) => {
+  const pageUrl = route === ROUTES.MENTORSHIP ? ROUTES.MENTORSHIP : `/${ROUTES.MENTORSHIP}/${route}`;
+
   return (
     <section className="mentors container" id="mentors-wanted">
       <div className="mentors content column-2">
         <div className="mentors-info">
           <WidgetTitle size="large" mods="lines">Mentors wanted!</WidgetTitle>
           <Paragraph fontSize="large" className="paragraph">
-            The Rolling Scopes School is constantly looking for mentors from all over the world to
-            teach everyone who wants to learn the JavaScript language and the world of Front-end.
-            Over the past few years, over 1500+ people have successfully completed our six month
-            training program.
+            {textContent[lang].info}
           </Paragraph>
           <LinkCustom
-            href={ROUTES.MENTORSHIP}
+            href={pageUrl}
             variant="primary"
           >
-            Read more
+            {textContent[lang].button}
           </LinkCustom>
         </div>
         <div className="picture">
