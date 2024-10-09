346a08545bda274b83b62d0cc410e07ee20ef6de
KristiBo <kristina.boroh@gmail.com>
2024-10-09 23:30:03 +0300

    refactor: 550 - update tests

--- src/widgets/mentors-wanted/ui/mentors-wanted.tsx
+++ src/widgets/mentors-wanted/ui/mentors-wanted.tsx
@@ -12,7 +12,7 @@ const cx = classNamesBind.bind(styles);
 
 export const MentorsWanted = () => {
   return (
-    <section className={cx('mentors-wanted', 'container')}>
+    <section className={cx('mentors-wanted', 'container')} data-testid="mentors-wanted">
       <article className={cx('mentors-wanted-content', 'content')}>
         <div className={cx('content-left')}>
           <WidgetTitle id="mentors-wanted" mods="lines">
@@ -21,7 +21,7 @@ export const MentorsWanted = () => {
           <Paragraph>
             If&nbsp;you are interested in mentoring our students, please go through the
             {' '}
-            <LinkCustom href={LINKS.ANGULAR_MENTORING} external>
+            <LinkCustom href={LINKS.ANGULAR_MENTORING} external data-testid="link-custom">
               Mentoring Documentation
             </LinkCustom>
             {' '}
@@ -32,6 +32,7 @@ export const MentorsWanted = () => {
           className={cx('sloth-mascot')}
           src={mentorImg}
           alt="Sloth - mascot dresses as a detective"
+          data-testid="sloth-mascot"
         />
       </article>
     </section>
