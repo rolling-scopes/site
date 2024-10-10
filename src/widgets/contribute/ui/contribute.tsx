0554f6cfa2e3808417017659618aa2d6256ec40a
YulikK <yulikpoch@gmail.com>
2024-10-10 09:44:36 +0200

    refactor: 523 - add styles for option-items

--- src/widgets/contribute/ui/contribute.tsx
+++ src/widgets/contribute/ui/contribute.tsx
@@ -37,11 +37,9 @@ export const Contribute = () => (
           <article key={title} className={cx('option-item')} data-testid="option-item">
             <Subtitle>{title}</Subtitle>
             <Paragraph fontSize="large">{description}</Paragraph>
-            {linkLabel && (
-              <LinkCustom href={href} variant="primary" external>
-                {linkLabel}
-              </LinkCustom>
-            )}
+            <LinkCustom href={href} variant="primary" external>
+              {linkLabel}
+            </LinkCustom>
           </article>
         ))}
       </div>
