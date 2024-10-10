3c74f8588acada21c47dce9c5b304ccf630de0d4
Mikhail Aleinik <84644126+MikAleinik@users.noreply.github.com>
2024-10-10 11:42:14 +0300

    Update src/widgets/requirements/ui/requirements.test.tsx
    Co-authored-by: MielomankA <79061120+MielomankA@users.noreply.github.com>

--- src/widgets/requirements/ui/requirements.test.tsx
+++ src/widgets/requirements/ui/requirements.test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from 'vitest';
-import { Requirements } from './requirements.tsx';
+import { Requirements } from './requirements';
 import { renderWithRouter } from '@/shared/__tests__/utils';
 
 const registerLink = 'https://app.rs.school/registry/mentor';
