be7e9d7d437c4f23efedd6f6804c9e0577e501cd
Mikhail Aleinik <6424156@tut.by>
2024-10-13 15:22:07 +0300

    refactor: 580 - scss module added in about mentorship widget

--- src/widgets/about-mentorship/ui/about-mentorship.tsx
+++ src/widgets/about-mentorship/ui/about-mentorship.tsx
@@ -1,6 +1,9 @@
+import classNames from 'classnames/bind';
 import { WidgetTitle } from '@/shared/ui/widget-title';
 
-import './about-mentorship.scss';
+import styles from './about-mentorship.module.scss';
+
+const cx = classNames.bind(styles);
 
 type AboutMentorshipProps = {
   benefits: string[];
@@ -15,15 +18,15 @@ const textContent = {
 
 export const AboutMentorship = ({ benefits, lang = 'en' }: AboutMentorshipProps) => {
   return (
-    <section className="about-mentorship container">
-      <div className="about-mentorship content">
-        <WidgetTitle size="small">{textContent[lang].header}</WidgetTitle>
-        <ul className="benefits">
+    <section className={cx('container')}>
+      <div className={cx('about-mentorship', 'content')}>
+        <WidgetTitle className={cx('title')}>{textContent[lang].header}</WidgetTitle>
+        <ul className={cx('benefits')}>
           {benefits.map((info, index) => {
-            let className = 'benefit' + ((info.length > COUNT_SYMBOLS) ? ' benefit-long' : ' benefit-short');
+            let classNameWidth = ((info.length > COUNT_SYMBOLS) ? 'benefit-long' : 'benefit-short');
 
             return (
-              <li key={index} className={className}>
+              <li key={index} className={cx('benefit', classNameWidth)}>
                 {info}
               </li>
             );
