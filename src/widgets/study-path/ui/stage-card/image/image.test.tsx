9738f793e3f564c78583266bb63a899ed3ee7617
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-10-14 16:09:41 +0300

    fix: 554 - image test

--- src/widgets/study-path/ui/stage-card/image/image.test.tsx
+++ src/widgets/study-path/ui/stage-card/image/image.test.tsx
@@ -4,22 +4,22 @@ import { MOCKED_IMAGE_PATH } from '@/shared/__tests__/constants';
 
 describe('Actions Component', () => {
   it('renders image correctly', () => {
-    render(<Image imageSrc={MOCKED_IMAGE_PATH} title={MOCKED_IMAGE_PATH} />);
+    render(<Image imageSrc={MOCKED_IMAGE_PATH} title={MOCKED_IMAGE_PATH.src} />);
 
-    expect(screen.getByRole('img', { name: MOCKED_IMAGE_PATH })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: MOCKED_IMAGE_PATH.src })).toBeInTheDocument();
   });
 
   it('renders image src and alt attributes correctly', () => {
-    render(<Image imageSrc={MOCKED_IMAGE_PATH} title={MOCKED_IMAGE_PATH} />);
+    render(<Image imageSrc={MOCKED_IMAGE_PATH} title={MOCKED_IMAGE_PATH.src} />);
 
-    expect(screen.getByRole('img', { name: MOCKED_IMAGE_PATH })).toHaveAttribute(
+    expect(screen.getByRole('img', { name: MOCKED_IMAGE_PATH.src })).toHaveAttribute(
       'src',
-      MOCKED_IMAGE_PATH,
+      MOCKED_IMAGE_PATH.src,
     );
 
-    expect(screen.getByRole('img', { name: MOCKED_IMAGE_PATH })).toHaveAttribute(
+    expect(screen.getByRole('img', { name: MOCKED_IMAGE_PATH.src })).toHaveAttribute(
       'alt',
-      MOCKED_IMAGE_PATH,
+      MOCKED_IMAGE_PATH.src,
     );
   });
 });
