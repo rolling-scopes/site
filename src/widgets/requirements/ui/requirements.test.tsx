78633f683c9771910c33efb7daf182721c6f34f7
Mikhail Aleinik <6424156@tut.by>
2024-10-16 09:35:46 +0300

    refactor: 562 - render tests are collected in one block

--- src/widgets/requirements/ui/requirements.test.tsx
+++ src/widgets/requirements/ui/requirements.test.tsx
@@ -5,44 +5,33 @@ import { renderWithRouter } from '@/shared/__tests__/utils';
 
 const registerLink = 'https://app.rs.school/registry/mentor';
 const requirements = [
-  "Desire to help students. If you've been working with JS/TS in production for more than 6 months, then that's great",
+  'Desire to help students. If you\'ve been working with JS/TS in production for more than 6 months, then that\'s great',
   'Desire to mentor 2 to 6 students online or in person',
   'Ability to spend 3 to 5 hours per week',
 ];
 const responsibilities = [
   'Conducting an interview',
   'Code review tasks',
-  "Answers to students' questions",
+  'Answers to students\' questions',
 ];
 
 describe('Requirements', () => {
   beforeEach(() => {
     renderWithRouter(<Requirements />);
   });
-
-  it('renders without crashing', () => {
+  it('renders check', () => {
     const requirementWidget = screen.getByTestId('requirements');
 
     expect(requirementWidget).toBeVisible();
-  });
 
-  it('renders the "Requirements for mentors" title', () => {
     expect(screen.getByText('Requirements for mentors')).toBeVisible();
-  });
 
-  it.each([requirements])('renders the "Requirements for mentors" description with string %s', (s) => {
-    expect(screen.getByText(s)).toBeVisible();
-  });
+    requirements.forEach((s) => expect(screen.getByText(s)).toBeVisible());
 
-  it('renders the "Mentor responsibilities" title', () => {
     expect(screen.getByText('Mentor responsibilities')).toBeVisible();
-  });
 
-  it.each([responsibilities])('renders the "Mentor responsibilities" description with string %s', (s) => {
-    expect(screen.getByText(s)).toBeVisible();
-  });
+    responsibilities.forEach((s) => expect(screen.getByText(s)).toBeVisible());
 
-  it('renders the "Register as a mentor" button', () => {
     const button = screen.getByRole('link', { name: /Register as a mentor/i });
 
     expect(button).toHaveAttribute('href', registerLink);
