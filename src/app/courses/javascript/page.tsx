4ca6bb5d900ef0210e0ad2afe934b479b5f3ae7f
YulikK <yulikpoch@gmail.com>
2024-10-17 12:21:08 +0200

    refactor: 554 - make helper method for page title

--- src/app/courses/javascript/page.tsx
+++ src/app/courses/javascript/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { getCourseTitle } from '@/shared/helpers/get-course-title';
 import { selectCourse } from '@/shared/hooks/use-course-by-title/utils/select-course';
 import { JavaScriptEn } from '@/views/javascript-en';
 import { COURSE_TITLES, courses } from 'data';
@@ -6,10 +7,7 @@ import { COURSE_TITLES, courses } from 'data';
 const courseName = COURSE_TITLES.JS_EN;
 
 export async function generateMetadata(): Promise<Metadata> {
-  const course = selectCourse(courses, courseName);
-  const title = `${course?.title || ''} · The Rolling Scopes School`;
-
-  return { title };
+  return { title: getCourseTitle(courseName) };
 }
 
 export default function JsEnRoute() {
