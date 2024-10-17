c026bb88ffdbf28a7b6620ea009ec932ff198311
Mikhail Aleinik <6424156@tut.by>
2024-10-17 21:46:31 +0300

    fix: 580 - error tag a in paragraph

--- src/widgets/mentors-docs/ui/additional/ui/additional.tsx
+++ src/widgets/mentors-docs/ui/additional/ui/additional.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames/bind';
 import { DiscordIcon, TelegramIcon } from '@/shared/icons';
-import { Paragraph } from '@/shared/ui/paragraph';
 import { SocialMediaItem } from '@/shared/ui/social-media-item';
 
 import styles from './additional.module.scss';
@@ -15,10 +14,10 @@ type AdditionalProps = {
 
 export const Additional = ({ text, linkDiscord, linkTelegram }: AdditionalProps) => {
   return (
-    <Paragraph className={cx('additional')}>
+    <div className={cx('additional')}>
       {text}
       <SocialMediaItem title="Telegram" href={linkTelegram} icon={TelegramIcon()} />
       <SocialMediaItem title="Discord" href={linkDiscord} icon={DiscordIcon()} />
-    </Paragraph>
+    </div>
   );
 };
