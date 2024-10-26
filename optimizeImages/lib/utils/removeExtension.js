244b9581a4893080976dd7a5324d996c6f2328e6
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-10-26 18:32:50 +0300

    chore: 554 - remove image optimization script

--- optimizeImages/lib/utils/removeExtension.js
+++
@@ -1,9 +0,0 @@
-/**
- * Removes the extension from the given filename
- * @param {string} filename - The filename that needs to be processed
- * @return {string} - Returns the filename without the extension
- * @example removeExtension('img.jpt') // img.jpg -> img;
- */
-const removeExtension = (filename) => filename.slice(0, filename.lastIndexOf('.'));
-
-export default removeExtension;
