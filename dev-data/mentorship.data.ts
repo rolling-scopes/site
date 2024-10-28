361bbec9fbe8d3dfc732325377262b774e074bd1
Mikhail Aleinik <6424156@tut.by>
2024-10-28 23:58:43 +0300

    refactor: 580 - update text mentor activity

--- dev-data/mentorship.data.ts
+++ dev-data/mentorship.data.ts
@@ -52,22 +52,6 @@ export const mentorshipCoursesDefault: MentorshipCourse = {
   activities: [
     {
       id: 1,
-      title: 'Best practices',
-      description: 'The mentor shows what code constructs are best to use based on his experience',
-    },
-    {
-      id: 2,
-      title: 'Code review',
-      description: 'The mentor reviews the code, points out inaccuracies, suggests how to improve them. The code review helps the student to develop the necessary skills to work in a team',
-      links: [
-        {
-          href: 'https://github.com/rolling-scopes-school/docs/blob/master/docs/en/pull-request-review-process.md',
-          linkTitle: 'Read more',
-        },
-      ],
-    },
-    {
-      id: 3,
       title: 'Conducting technical interview',
       description: 'The mentor makes a decision about working with the student. Interviews are conducted with 2+ students - the total size of their team is determined by the mentor based on their workload',
       links: [
@@ -78,12 +62,23 @@ export const mentorshipCoursesDefault: MentorshipCourse = {
       ],
     },
     {
-      id: 4,
-      title: 'Conducting two CoreJS interviews',
-      description: 'The mentor conducts two interviews with students of other mentors on major topics of the course',
+      id: 2,
+      title: 'Best practices',
+      description: 'The mentor shows what code constructs are best to use based on his experience. Suggests how to better organize the structure of the application',
     },
     {
-      id: 5,
+      id: 3,
+      title: 'Code review',
+      description: 'The mentor reviews the code, points out inaccuracies, suggests how to improve them. The code review helps the student to develop the necessary skills to work in a team',
+      links: [
+        {
+          href: 'https://github.com/rolling-scopes-school/docs/blob/master/docs/en/pull-request-review-process.md',
+          linkTitle: 'Read more',
+        },
+      ],
+    },
+    {
+      id: 4,
       title: 'Supervising a team assignment',
       description: 'The mentor\'s experience and practical knowledge will help organize students to work on a team project',
       links: [
@@ -119,22 +114,6 @@ export const mentorshipCourses: MentorshipCourse[] = [
     activities: [
       {
         id: 1,
-        title: 'Best practices',
-        description: 'The mentor shows what code constructs are best to use based on his experience',
-      },
-      {
-        id: 2,
-        title: 'Code review',
-        description: 'The mentor reviews the code, points out inaccuracies, suggests how to improve them. The code review helps the student to develop the necessary skills to work in a team',
-        links: [
-          {
-            href: 'https://github.com/rolling-scopes-school/docs/blob/master/docs/en/pull-request-review-process.md',
-            linkTitle: 'Read more',
-          },
-        ],
-      },
-      {
-        id: 3,
         title: 'Conducting technical interview',
         description: 'The mentor makes a decision about working with the student. Interviews are conducted with 2+ students - the total size of their team is determined by the mentor based on their workload',
         links: [
@@ -145,9 +124,25 @@ export const mentorshipCourses: MentorshipCourse[] = [
         ],
       },
       {
+        id: 2,
+        title: 'Best practices',
+        description: 'The mentor shows what code constructs are best to use based on his experience. Suggests how to better organize the structure of the application',
+      },
+      {
+        id: 3,
+        title: 'Code review',
+        description: 'The mentor reviews the code, points out inaccuracies, suggests how to improve them. The code review helps the student to develop the necessary skills to work in a team',
+        links: [
+          {
+            href: 'https://github.com/rolling-scopes-school/docs/blob/master/docs/en/pull-request-review-process.md',
+            linkTitle: 'Read more',
+          },
+        ],
+      },
+      {
         id: 4,
         title: 'Conducting two CoreJS interviews',
-        description: 'The mentor conducts two interviews with students of other mentors on major topics of the course',
+        description: 'The mentor conducts two interviews with students of other mentors on major topics of the course. Participation in interviews allows students not only to see their knowledge gaps, but also to feel the atmosphere of such meetings',
       },
       {
         id: 5,
@@ -195,22 +190,6 @@ export const mentorshipCourses: MentorshipCourse[] = [
     activities: [
       {
         id: 1,
-        title: 'Лучшие практики',
-        description: 'Ментор показывает, какие конструкции кода лучше всего использовать, основываясь на своем опыте',
-      },
-      {
-        id: 2,
-        title: 'Ревью кода',
-        description: 'Ментор просматривает код, указывает на неточности, предлагает, как их улучшить. Ревью кода помогает студенту развить необходимые навыки для работы в команде',
-        links: [
-          {
-            href: 'https://github.com/rolling-scopes-school/docs/blob/master/docs/pull-request-review-process.md',
-            linkTitle: 'Подробнее',
-          },
-        ],
-      },
-      {
-        id: 3,
         title: 'Проведение технического интервью',
         description: 'По результату которого ментор принимает решение о работе со студентом. Интервью проводятся с 2+ студентами - общий размер команды определяется ментором в зависимости от его загруженности',
         links: [
@@ -221,9 +200,25 @@ export const mentorshipCourses: MentorshipCourse[] = [
         ],
       },
       {
+        id: 2,
+        title: 'Лучшие практики',
+        description: 'Ментор показывает, какие конструкции кода лучше всего использовать, основываясь на своем опыте. Подсказывает как лучше организовать структуру приложения',
+      },
+      {
+        id: 3,
+        title: 'Ревью кода',
+        description: 'Ментор просматривает код, указывает на неточности, предлагает, как их улучшить. Ревью кода помогает студенту развить необходимые навыки для работы в команде',
+        links: [
+          {
+            href: 'https://github.com/rolling-scopes-school/docs/blob/master/docs/pull-request-review-process.md',
+            linkTitle: 'Подробнее',
+          },
+        ],
+      },
+      {
         id: 4,
         title: 'Проведение двух CoreJS интервью',
-        description: 'Ментор проводит два интервью со студентами других менторов по основным темам курса',
+        description: 'Ментор проводит два интервью со студентами других менторов по основным темам курса. Участие в интервью позволяет студентам не только увидеть свои пробелы в знаниях, но почувствовать атмосферу подобных встреч ',
       },
       {
         id: 5,
@@ -256,6 +251,12 @@ export const mentorshipCourses: MentorshipCourse[] = [
         id: 1,
         title: 'Conducting technical interview',
         description: 'The mentor makes a decision about working with the student. Interviews are conducted with 2+ students - the total size of their team is determined by the mentor based on their workload',
+        links: [
+          {
+            href: 'https://github.com/rolling-scopes-school/tasks/blob/master/react/interview.md',
+            linkTitle: 'Read more',
+          },
+        ],
       },
       {
         id: 2,
@@ -275,17 +276,6 @@ export const mentorshipCourses: MentorshipCourse[] = [
       },
       {
         id: 4,
-        title: 'Conducting technical interview',
-        description: 'The mentor makes a decision about working with the student. Interviews are conducted with 2+ students - the total size of their team is determined by the mentor based on their workload',
-        links: [
-          {
-            href: 'https://github.com/rolling-scopes-school/tasks/blob/master/react/interview.md',
-            linkTitle: 'Read more',
-          },
-        ],
-      },
-      {
-        id: 5,
         title: 'Supervising a team assignment',
         description: 'The mentor\'s experience and practical knowledge will help organize students to work on a team project',
       },
@@ -313,13 +303,13 @@ export const mentorshipCourses: MentorshipCourse[] = [
     activities: [
       {
         id: 1,
-        title: 'Best practices used in the framework',
-        description: 'The mentor shows what code constructs are best to use based on his experience. Helps to understand possible future problems in the student\'s solution and ways to solve them',
+        title: 'Conducting technical interview',
+        description: 'The mentor makes a decision about working with the student. Interviews are conducted with 2+ students - the total size of their team is determined by the mentor based on their workload',
       },
       {
         id: 2,
-        title: 'Conducting technical interview',
-        description: 'The mentor makes a decision about working with the student. Interviews are conducted with 2+ students - the total size of their team is determined by the mentor based on their workload',
+        title: 'Best practices used in the framework',
+        description: 'The mentor shows what code constructs are best to use based on his experience. Helps to understand possible future problems in the student\'s solution and ways to solve them',
       },
       {
         id: 3,
