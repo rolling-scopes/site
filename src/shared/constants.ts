1f700189ce33ef9a1aa4dbea8aa04064ddbc43f4
Bogdan <bogdanscherbinadev@gmail.com>
2024-11-04 15:33:53 +0200

    refactor: 628 - remove unused env variables (#629)

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,3 @@
-export const TABLET_W = Number(process.env.NEXT_PUBLIC_TABLET);
-export const MOBILE_W = Number(process.env.NEXT_PUBLIC_MOBILE);
-export const DESKTOP_W = 1280;
 export const RS_INTRO_URL = 'https://www.youtube.com/embed/n4unZLVpnaU';
 export const RS_FOUNDATION_YEAR = '2013';
 
