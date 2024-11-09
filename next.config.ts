fe40b43026b7919c88f7df9ab874f52b8af8847f
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-11-09 22:15:39 +0200

    refactor: 634 - resolve sass deprecation warning

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,14 @@ const nextConfig: NextConfig = {
   distDir: './build',
   images: { unoptimized: true },
   sassOptions: {
+    // TODO: remove silenceDeprecations below, when the issue will be fixed
+    // https://github.com/vercel/next.js/issues/71638#issuecomment-2454463904
+    silenceDeprecations: ['legacy-js-api'],
     includePaths: [path.join(__dirname, 'src')],
     prependData: `
-      @import "@/core/styles/_constants.scss";
-      @import "@/core/styles/_mixins.scss";
-      @import "@/core/styles/_placeholders.scss";
+      @use '@/core/styles/constants' as *;
+      @use '@/core/styles/mixins' as *;
+      @use '@/core/styles/placeholders' as *;
     `,
   },
   devIndicators: { appIsrStatus: false },
