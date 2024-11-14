4593d542a8a320854bd305a56f5083df101ee9a2
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-11-14 22:22:58 +0200

    refactor: 646 - replace magic string with constant

--- src/shared/ui/date-start/ui/date-start.tsx
+++ src/shared/ui/date-start/ui/date-start.tsx
@@ -2,6 +2,7 @@
 
 import React, { HTMLProps } from 'react';
 import { COURSE_STALE_AFTER_DAYS } from '@/core/const';
+import { TO_BE_DETERMINED } from '@/shared/constants';
 import { dayJS } from '@/shared/helpers/dayJS';
 import { getCourseDate } from '@/shared/helpers/getCourseDate';
 
@@ -13,7 +14,7 @@ export const DateStart = ({ date, ...props }: DateStartProps) => {
   const freshDate = getCourseDate(date, COURSE_STALE_AFTER_DAYS);
   let dateAttr = undefined;
 
-  if (freshDate !== 'TBD') {
+  if (freshDate !== TO_BE_DETERMINED) {
     dateAttr = dayJS(date).toISOString();
   }
 
