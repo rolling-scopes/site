341672d16e1bae7a2fe01a70a4ad28653064f9cd
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-11-19 21:54:59 +0300

    improve search

--- src/app/docs/search.tsx
+++ src/app/docs/search.tsx
@@ -1,48 +1,49 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link.js';
+import mockedResults from './mocked_search.json';
+
+import './search.scss';
+
+declare global {
+  interface Window {
+    pagefind: {
+      options?: (opt: {
+        baseUrl?: string;
+        bundlePath?: string;
+        excerptLength?: number;
+        highlightParam?: 'highlight';
+      }) => Promise<void>;
+      search: (query: string) => Promise<{
+        results: unknown[];
+      }>;
+    };
+  }
+}
+
+const isRunningInDev = process.env.NODE_ENV === 'development';
 
 export default function Search() {
-  const [query, setQuery] = React.useState('');
-  const [results, setResults] = React.useState([]);
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function loadPagefind() {
-      // @ts-ignore
-      if (typeof window.pagefind === 'undefined') {
-        try {
+      if (!isRunningInDev) {
+        window.pagefind = await import(
+          // pagefind.js generated after build
           // @ts-ignore
-          // window.pagefind = await import(
-          //   // @ts-expect-error pagefind.js generated after build
-          //   // eslint-disable-next-line import/no-unresolved
-          //   /* webpackIgnore: true */ '/_next/static/pagefind/pagefind.js'
-          // );
-          // @ts-expect-error
-          await window.pagefind.options({
-            baseUrl: '/',
-            // ... more search options
-          });
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        } catch (e) {
-          // @ts-ignore
-          window.pagefind = {
-            search: () => ({
-              results: [
-                {
-                  id: 'dummy-id',
-                  data: async () => ({
-                    url: '/dummy-url',
-                    meta: { title: 'dummy title' },
-                    excerpt: 'dummy excerpt',
-                  }),
-                },
-              ],
-            }),
-          };
-        }
+          // eslint-disable-next-line import/no-unresolved, import/extensions
+          /* webpackIgnore: true */ '/_next/static/pagefind/pagefind.js'
+        );
+
+        await window.pagefind.options!({ baseUrl: '/' });
+      } else {
+        window.pagefind = { search: async () => ({ results: mockedResults }) };
       }
     }
+
     loadPagefind();
   }, []);
 
@@ -52,6 +53,7 @@ export default function Search() {
       // @ts-ignore
       const search = await window.pagefind.search(query);
 
+      // @ts-ignore
       setResults(search.results);
     }
   }
@@ -66,9 +68,9 @@ export default function Search() {
         onInput={handleSearch}
       />
       <div id="results">
-        {results.map((result) => (
+        {results.map((result, index) => (
           // @ts-ignore
-          <Result key={result.id} result={result} />
+          <Result key={index} result={result} />
         ))}
       </div>
     </div>
@@ -78,12 +80,17 @@ export default function Search() {
 function Result({ result }) {
   const [data, setData] = useState(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
-      const data = await result.data();
+      if (isRunningInDev) {
+        setData(result);
+      } else {
+        const data = await result.data();
 
-      setData(data);
+        setData(data);
+      }
     }
+
     fetchData();
   }, [result]);
 
@@ -92,12 +99,29 @@ function Result({ result }) {
   }
 
   return (
-    // @ts-ignore
-    <Link href={data.url}>
+    <div className="results">
       {/* @ts-ignore */}
-      <h3>{data.meta.title}</h3>
+      <Link href={data.url}>
+        {/* @ts-ignore */}
+        <h3>{data.meta.title}</h3>
+        {/* @ts-ignore */}
+        <p>{data.excerpt}</p>
+      </Link>
+
       {/* @ts-ignore */}
-      <p>{data.excerpt}</p>
-    </Link>
+      {data.sub_results && data.sub_results.length > 0 && (
+        <div className="subresults">
+          {/* @ts-ignore */}
+          {data.sub_results.map((subresult, index) => (
+            <div key={index} className="subresult">
+              <Link href={subresult.url}>
+                <h4>{subresult.title}</h4>
+                <p>{subresult.excerpt}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
