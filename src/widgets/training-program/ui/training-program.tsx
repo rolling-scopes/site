b83f720fea9464c43705262f027a42f3d0ad7f3a
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-11-23 17:40:38 +0200

    refactor: 569 - change scss to modules

--- src/widgets/training-program/ui/training-program.tsx
+++ src/widgets/training-program/ui/training-program.tsx
@@ -1,11 +1,14 @@
 import { cloneElement } from 'react';
+import classNames from 'classnames/bind';
 import Image from 'next/image';
 import type { Course } from '@/entities/course';
 import { LinkCustom } from '@/shared/ui/link-custom';
 import { WidgetTitle } from '@/shared/ui/widget-title';
 import { TrainingProgramType, contentMap } from 'data';
 
-import './training-program.scss';
+import styles from './training-program.module.scss';
+
+const cx = classNames.bind(styles);
 
 type TrainingProgramProps = {
   courseName: TrainingProgramType;
@@ -20,13 +23,14 @@ const localizedContent = {
 
 export const TrainingProgram = ({ courseName, lang = 'en', course }: TrainingProgramProps) => {
   const { title, content, image } = contentMap[courseName];
+  const isCourseWithBadge = courseName.includes('badge');
 
   // TODO remove 'cloneElement' on 37 line due 'Using cloneElement is uncommon and can lead to fragile code' https://react.dev/reference/react/cloneElement
 
   return (
-    <section className="training-program container">
-      <div className="training-program content column-2">
-        <div className="left">
+    <section className={cx('training-program', 'container')}>
+      <div className={cx('training-program', 'content', 'column-2')}>
+        <div className={cx('left')}>
           <WidgetTitle mods="asterisk">{title}</WidgetTitle>
 
           {content.map((component, index) => cloneElement(component, { key: index }))}
@@ -37,7 +41,7 @@ export const TrainingProgram = ({ courseName, lang = 'en', course }: TrainingPro
             </LinkCustom>
           )}
         </div>
-        <div className={`right ${courseName.includes('badge') ? 'badge' : ''}`}>
+        <div className={cx('right', { badge: isCourseWithBadge })}>
           <Image src={image} alt={course?.title} />
         </div>
       </div>
