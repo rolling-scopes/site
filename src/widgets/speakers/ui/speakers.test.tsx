893d41fc51bf15334def091fac6bbdc73d64a6ed
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-11-29 14:05:43 +0200

    refactor: 564 - move test to ui folder

--- src/widgets/speakers/ui/speakers.test.tsx
+++ src/widgets/speakers/ui/speakers.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from 'vitest';
-import { Speakers } from './ui/speakers';
+import { Speakers } from '@/widgets/speakers';
 
 describe('Speakers', () => {
   beforeEach(() => {
