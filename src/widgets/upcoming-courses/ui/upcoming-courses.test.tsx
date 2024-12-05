7f544fca0c683ef0066552070a7967dafb33101b
YulikK <yulikpoch@gmail.com>
2024-12-05 14:10:50 +0100

    feat: 642 - add test for empty course

--- src/widgets/upcoming-courses/ui/upcoming-courses.test.tsx
+++ src/widgets/upcoming-courses/ui/upcoming-courses.test.tsx
@@ -1,39 +1,58 @@
+/* eslint-disable import/no-namespace */
 import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { mockedCourses } from '@/shared/__tests__/constants';
+import * as helper from '@/shared/helpers/getActualData';
 import { UpcomingCourses } from '@/widgets/upcoming-courses';
 
-vi.mock('@/shared/helpers/getActualData', () => ({ getActualData: vi.fn().mockImplementation(() => mockedCourses) }));
+describe('UpcomingCourses', () => {
+  describe('with courses', () => {
+    beforeEach(async () => {
+      vi.spyOn(helper, 'getActualData').mockImplementation(() => mockedCourses);
+      const upcomingCourses = await UpcomingCourses();
 
-describe('Courses', () => {
-  beforeEach(async () => {
-    const upcomingCourses = await UpcomingCourses();
+      render(upcomingCourses);
+    });
 
-    render(upcomingCourses);
+    it('renders the title correctly', () => {
+      const titleElement = screen.getByText('Upcoming courses');
+
+      expect(titleElement).toBeInTheDocument();
+    });
+
+    it('renders no more than 5 course cards', () => {
+      const coursesList = screen.getByTestId('courses-list');
+      const courseCardsWithoutButton = coursesList.children.length - 1;
+
+      expect(courseCardsWithoutButton).toEqual(5);
+    });
+
+    it('renders the Go to RS School button', () => {
+      const goButton = screen.getByText('Go to courses');
+
+      expect(goButton).toBeInTheDocument();
+    });
+
+    it('should render RsBanner', () => {
+      const rsBanner = screen.getByTestId('rs-banner');
+
+      expect(rsBanner).toBeInTheDocument();
+    });
   });
 
-  it('renders the title correctly', () => {
-    const titleElement = screen.getByText('Upcoming courses');
+  describe('without courses', () => {
+    beforeEach(async () => {
+      mockedCourses.filter(() => []);
+      vi.spyOn(helper, 'getActualData').mockImplementation(() => []);
+      const upcomingCourses = await UpcomingCourses();
 
-    expect(titleElement).toBeInTheDocument();
-  });
+      render(upcomingCourses);
+    });
 
-  it('renders no more than 5 course cards', () => {
-    const coursesList = screen.getByTestId('courses-list');
-    const courseCardsWithoutButton = coursesList.children.length - 1;
+    it('does not render the component if courses array is empty', () => {
+      const titleElement = screen.queryByText('Upcoming courses');
 
-    expect(courseCardsWithoutButton).toEqual(5);
-  });
-
-  it('renders the Go to RS School button', () => {
-    const goButton = screen.getByText('Go to courses');
-
-    expect(goButton).toBeInTheDocument();
-  });
-
-  it('should render RsBanner', () => {
-    const rsBanner = screen.getByTestId('rs-banner');
-
-    expect(rsBanner).toBeInTheDocument();
+      expect(titleElement).not.toBeInTheDocument();
+    });
   });
 });
