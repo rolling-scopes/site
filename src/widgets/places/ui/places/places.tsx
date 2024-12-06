157e9ff76c42792ee7583714ae402f467ace98ba
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-12-06 23:03:24 +0200

    refactor: reduce code duplication

--- src/widgets/places/ui/places/places.tsx
+++ src/widgets/places/ui/places/places.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import PlaceItem from '../place-item/place-item';
-import { places } from '@/widgets/places/constants';
+import { MarqueeGroup } from '@/widgets/places/ui/marquee-group/marquee-group';
 
 import styles from './places.module.scss';
 
@@ -10,21 +9,9 @@ const cx = classNames.bind(styles);
 export const Places = () => (
   <section className={cx('places', 'container')} data-testid="places">
     <div className={cx('places', 'content')}>
-      <div className={cx('marquee-group')}>
-        {places.map((place) => (
-          <PlaceItem key={place}>{place}</PlaceItem>
-        ))}
-      </div>
-      <div aria-hidden="true" className={cx('marquee-group')}>
-        {places.map((place) => (
-          <PlaceItem key={place}>{place}</PlaceItem>
-        ))}
-      </div>
-      <div aria-hidden="true" className={cx('marquee-group')}>
-        {places.map((place) => (
-          <PlaceItem key={place}>{place}</PlaceItem>
-        ))}
-      </div>
+      <MarqueeGroup />
+      <MarqueeGroup hidden />
+      <MarqueeGroup hidden />
     </div>
   </section>
 );
