b197aa716af40caac48d3d88bfc503f653caec57
YulikK <yulikpoch@gmail.com>
2024-12-06 12:23:00 +0100

    refactor: 614 - update test

--- src/widgets/hero-course/ui/hero-course.test.tsx
+++ src/widgets/hero-course/ui/hero-course.test.tsx
@@ -27,6 +27,25 @@ const mockedCourse: Course = {
     accentColor: '#AEDF36',
   },
 };
+const mockedCourseNoSubtitle: Course = {
+  id: '6',
+  title: COURSE_TITLES.NODE,
+  subTitle: null,
+  alias: COURSE_ALIASES.NODE,
+  iconSrc: MOCKED_IMAGE_PATH,
+  iconSmall: MOCKED_IMAGE_PATH,
+  secondaryIcon: MOCKED_IMAGE_PATH,
+  startDate: dayJS().subtract(2, 'month').format('D MMM, YYYY'),
+  registrationEndDate: TO_BE_DETERMINED,
+  language: ['en'],
+  mode: 'online',
+  detailsUrl: `/${ROUTES.COURSES}/${ROUTES.NODE_JS}`,
+  enroll: 'https://test.com',
+  backgroundStyle: {
+    backgroundColor: '#F0F9F4',
+    accentColor: '#AEDF36',
+  },
+};
 
 describe('HeroCourse component', () => {
   describe('Render general content', () => {
@@ -34,12 +53,14 @@ describe('HeroCourse component', () => {
       renderWithRouter(<HeroCourse course={mockedCourse} />);
     });
 
-    it('renders the title and label correctly', async () => {
+    it('renders the title, label and subtitle correctly', async () => {
       const titleElement = await screen.findByText('Node.js Course');
       const labelElement = screen.getByText('planned');
+      const subtitleElement = await screen.findByText('Test Subtitle');
 
       expect(titleElement).toBeVisible();
       expect(labelElement).toBeVisible();
+      expect(subtitleElement).toBeVisible();
     });
 
     it('renders enroll button with correct label and href', () => {
@@ -57,17 +78,9 @@ describe('HeroCourse component', () => {
     });
   });
 
-  describe('Render subtitle', () => {
-    it('renders the subtitle when provided', async () => {
-      renderWithRouter(<HeroCourse course={mockedCourse} />);
-      const subtitleElement = await screen.findByText('Test Subtitle');
-
-      expect(subtitleElement).toBeVisible();
-    });
-
+  describe('Render widget with empty subtitle', () => {
     it('does not display subtitles if they are not provided', () => {
-      mockedCourse.subTitle = null;
-      renderWithRouter(<HeroCourse course={mockedCourse} />);
+      renderWithRouter(<HeroCourse course={mockedCourseNoSubtitle} />);
       const subtitleElement = screen.queryByText('Test Subtitle');
 
       expect(subtitleElement).toBeNull();
