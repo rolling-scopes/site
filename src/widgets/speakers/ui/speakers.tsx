5bc0b2c8b45004d3e545050e7891871e8024ab3b
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-12-07 12:24:10 +0200

    refactor: change email to link

--- src/widgets/speakers/ui/speakers.tsx
+++ src/widgets/speakers/ui/speakers.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import speakersWanted from '@/shared/assets/speakers-wanted.webp';
 import email from '@/shared/assets/svg/email.svg';
 import { RS_EMAIL } from '@/shared/constants';
+import { LinkCustom } from '@/shared/ui/link-custom';
 import { Paragraph } from '@/shared/ui/paragraph';
 import { Subtitle } from '@/shared/ui/subtitle';
 import { WidgetTitle } from '@/shared/ui/widget-title';
@@ -31,7 +32,9 @@ export const Speakers = () => (
         </Subtitle>
         <address className={cx('email-wrapper')}>
           <Image src={email} alt="" aria-hidden="true" />
-          <span className={cx('email')}>{RS_EMAIL}</span>
+          <LinkCustom href={`mailto:${RS_EMAIL}`} className={cx('email')}>
+            {RS_EMAIL}
+          </LinkCustom>
         </address>
       </article>
       <Image
