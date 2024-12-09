7b12f91a42b5da9a34ffa3c64feb2a8d30cafac8
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-12-09 15:49:50 +0300

    replace percy with argos

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,14 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'dot' : [['html', { outputFolder: 'src/shared/__tests__/report' }]],
+  reporter: [
+    process.env.CI ? ['dot'] : ['html', { outputFolder: 'src/shared/__tests__/report' }],
+    ['@argos-ci/playwright/reporter', { uploadToArgos: !!process.env.CI }],
+  ],
   use: {
     baseURL: 'http://localhost:3000',
-    trace: 'off',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
   outputDir: 'src/shared/__tests__/test-results',
   projects: [
