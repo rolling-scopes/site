1ea946cdbdf39f56169b45f068eab7a8253c4fea
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2024-12-14 12:58:05 +0200

    fix: 563 - minor issues

--- src/widgets/school-menu/ui/school-item/school-item.tsx
+++ src/widgets/school-menu/ui/school-item/school-item.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @stylistic/jsx-closing-bracket-location */
 import { HTMLProps } from 'react';
 import classNames from 'classnames/bind';
 import Image, { StaticImageData } from 'next/image';
@@ -25,18 +26,15 @@ export const SchoolItem = ({
   ...props
 }: SchoolItemProps) => {
   return (
-    <li key={title} {...props}>
+    <li {...props}>
       <Link href={url} className={cx('school-item')}>
-        {icon && (
-          <Image
-            className={cx('icon-wrapper')}
-            src={icon}
-            alt=""
-            width={32}
-            height={32}
-            data-testid="school-item-icon"
-          />
-        )}
+        {icon && <Image
+          src={icon}
+          alt=""
+          width={32}
+          height={32}
+          data-testid="school-item-icon"
+        />}
         <div className={cx('description-wrapper')}>
           <span className={cx('title', color)}>{title}</span>
           {description && (
