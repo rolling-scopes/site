97658a9dce21a3af378e58202dda08804f1437ca
MielomankA <mielomanka@mail.ru>
2024-12-16 19:44:54 +0300

    fix: 551 - change content and merch image

--- dev-data/merch.data.ts
+++ dev-data/merch.data.ts
@@ -2,6 +2,6 @@ export const merchData = {
   label: 'merch',
   title: 'RS merch',
   mainParagraph: 'Are you an RS sloth fan and looking for RS merch?',
-  description: 'The wait is almost over as we are gearing up for the catalog of free web and print assets where you will find all merch collections and can print your own Rolling Scopes t-shirts, stickers etc.',
+  description: 'The wait is almost over as we are gearing up for the catalog of free web and print assets where you will find all merch collections and can print your own Rolling Scopes stickers, t-shirts etc.',
   linkTitle: 'Discover merch assets',
 };
