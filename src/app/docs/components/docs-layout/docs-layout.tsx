398547effe86c523e0f9fb3e742f7c245aec8899
Dzmitry Yarmoshkin <spanb4@gmail.com>
2025-01-03 17:14:25 +0300

    650-feat: Add docs (#683)
    * feat: add docs
    
    * add ts-ignore
    
    * add missing css file
    
    * refactor
    
    * refactor: 650 - mentorship route
    
    * feat: 650 - add anchor icon
    
    * fix: submenu
    
    * feat: add lang switcher
    
    * improve search
    
    * change docs file structure
    
    * use remote for docs menu
    
    * add burgger menu for docs
    
    * refactor menu fetching
    
    * fix slugs
    
    * refactor docs
    
    * fix docs burger
    
    * clean up search
    
    * refactor css
    
    * fix css issue
    
    * fix mobile menu css
    
    * fix test
    
    * remove unused deps
    
    * apply stylelint:fix
    
    * remove scroll from docs menu
    
    * update search input styles
    
    * fix search results
    
    * fix issue with links
    
    * fix issue with images
    
    * ignore markdown comments
    
    * fix multilingual search
    
    * fix eslint
    
    * peer review fixes
    
    * fix active menu item
    
    * more fixes
    
    * start fixin menu
    
    * mobile menu fixes
    
    * add docs to the mobile menu
    
    * fix link behavior
    
    * fix tests
    
    * fix link icon
    
    * update language switcher
    
    * fix eslint
    
    * fix stylelint
    
    * fix alignement
    
    * use css for chevron
    
    * fix active menu
    
    * peer review fixes
    
    * remove useless styles
    
    * sticky menu
    
    * fix search
    
    * fix vertical scrool on small screens
    
    * fix docs horizontal scroll
    
    * fix stylelint
    
    * fix docs content styles
    
    * fix search result urls
    
    * remove .html extension from search results urls
    
    * update scroll-padding-top

---
+++ src/app/docs/components/docs-layout/docs-layout.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import { useRef, useState } from 'react';
+import classNames from 'classnames/bind';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import remarkEmoji from 'remark-emoji';
+import remarkGfm from 'remark-gfm';
+import remarkRemoveComments from 'remark-remove-comments';
+import remarkToc from 'remark-toc';
+import { GITHUB_RAW_ROOT } from '../../constants';
+import { Menu } from '../../types';
+import { DocsMenu } from '../docs-menu/docs-menu';
+import { LangSwitcher } from '../lang-switcher/lang-switcher';
+import Search from '../search/search';
+import { isValidUrl } from '@/shared/helpers/isValidUrl';
+import { Language } from '@/shared/types';
+
+import styles from './docs-layout.module.scss';
+
+const cx = classNames.bind(styles);
+
+const GITHUB_IMAGE_BASE = `${GITHUB_RAW_ROOT}/images`;
+
+type DocsLayoutProps = {
+  menu: Menu;
+  markdownContent: string;
+  lang: Language;
+};
+
+export function DocsLayout({ menu, markdownContent, lang }: DocsLayoutProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const resultsRef = useRef(null);
+
+  const handleMenuToggle = (isOpen: boolean) => {
+    setIsMenuOpen(isOpen);
+  };
+
+  return (
+    <main className={cx('container', 'content', 'docs-layout')}>
+      <nav className={cx('menu-wrapper')}>
+        <div className={cx('menu')}>
+          <DocsMenu menu={menu} lang={lang} isOpen={isMenuOpen} onMenuToggle={handleMenuToggle} />
+        </div>
+      </nav>
+      {!isMenuOpen && (
+        <div className={cx('docs-content')}>
+          <div>
+            <div className={cx('docs-top')}>
+              <Search lang={lang} resultsRef={resultsRef} />
+              <LangSwitcher />
+            </div>
+            <div ref={resultsRef}></div>
+          </div>
+          <article className={cx('markdown-body')} data-pagefind-body>
+            <ReactMarkdown
+              remarkPlugins={[
+                remarkGfm,
+                remarkToc,
+                [remarkEmoji, { accessible: true }],
+                remarkRemoveComments,
+              ]}
+              rehypePlugins={[rehypeSlug, rehypeAutolinkHeadings]}
+              components={{
+                img({ _, ...props }) {
+                  const { src } = props;
+
+                  const isExternalImage = isValidUrl(src);
+                  const newSrc =
+                    !isExternalImage && src ? `${GITHUB_IMAGE_BASE}/${src.split('/').pop()}` : src;
+
+                  return <img {...props} src={newSrc} />;
+                },
+                a({ children, href, ...props }) {
+                  let newHref = href;
+
+                  if (href.endsWith('.md')) {
+                    newHref = href.slice(0, -3);
+                  }
+
+                  if (
+                    href.startsWith('https://docs.rs.school')
+                    && !(href.endsWith('.png') || href.endsWith('.jpg'))
+                  ) {
+                    const transformedHref = href.replace('?id=', '#');
+                    const lastPathSegment = transformedHref.split('/').pop();
+
+                    newHref = `/docs/${lang}/${lastPathSegment}`;
+                  }
+
+                  return (
+                    <Link href={newHref} {...props}>
+                      {children}
+                    </Link>
+                  );
+                },
+              }}
+            >
+              {markdownContent}
+            </ReactMarkdown>
+          </article>
+        </div>
+      )}
+    </main>
+  );
+}
