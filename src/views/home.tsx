ada1bdb00211b4c22d2701ef3d20c83b5db7176b
Aleh Kuis <pestleralex@gmail.com>
2025-01-18 17:20:44 +0300

    644-refactor: Remove the section for mentors on rs school page (#712)
    * refactor: 644 - Remove unnecessary components MentorsWanted and Benefits from the main page
    
    * feat: 644 - del Requirements and add MentorsWanted

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -3,12 +3,10 @@ import { FC } from 'react';
 import { PAGE_NAMES } from '@/shared/constants';
 import { AboutSchool } from '@/widgets/about-school';
 import { Alumni } from '@/widgets/alumni';
-import { Benefits } from '@/widgets/benefits';
 import { Breadcrumbs } from '@/widgets/breadcrumbs';
 import { HeroPage } from '@/widgets/hero-page';
 import { MentorsWanted } from '@/widgets/mentors-wanted';
 import { Principles } from '@/widgets/principles';
-import { Requirements } from '@/widgets/requirements';
 import { StudyWithUs } from '@/widgets/study-with-us';
 import { UpcomingCourses } from '@/widgets/upcoming-courses';
 
@@ -23,8 +21,6 @@ export const Home: FC = () => {
       <UpcomingCourses />
       <Alumni />
       <MentorsWanted />
-      <Benefits />
-      <Requirements />
     </>
   );
 };
