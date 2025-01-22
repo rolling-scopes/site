8e650bbc48cc3242afb8a0c0c2f69e49ed3d5291
Anna Zhuravleva <56927722+ansivgit@users.noreply.github.com>
2025-01-22 21:04:22 +0300

    720-refactor: After completing registration section (#748)
    * feat: 720 - add component & styles
    
    * feat: 720 - add tests
    
    * fix: 720 - change components' dest folder
    
    * fix: 720 - after Rabbit review
    
    * fix: 720 - add data-testId

---
+++ dev-data/mentors-after-register.data.ts
@@ -0,0 +1,42 @@
+export const mentorsAfterRegisterData = {
+  en: {
+    title: 'After Completing the Registration',
+    steps: [
+      {
+        id: 1,
+        subtitle: 'An email with instructions',
+        content: 'will be sent to you before mentoring begins',
+      },
+      {
+        id: 2,
+        subtitle: 'Confirm your participation',
+        content: 'in the mentoring program by clicking on the link in the email',
+      },
+      {
+        id: 3,
+        subtitle: 'Follow the links',
+        content: 'from the email to join the selected course community on Telegram and Discord',
+      },
+    ],
+  },
+  ru: {
+    title: 'После Регистрации',
+    steps: [
+      {
+        id: 1,
+        subtitle: 'Письмо с инструкциями',
+        content: 'будет отправлено вам на электронную почту перед началом менторинга',
+      },
+      {
+        id: 2,
+        subtitle: 'Подтвердите участие',
+        content: 'в программе менторинга, перейдя по ссылке в письме',
+      },
+      {
+        id: 3,
+        subtitle: 'Подключайтесь по ссылкам',
+        content: 'из письма к сообществу выбранного курса в Телеграм и Дискорд',
+      },
+    ],
+  },
+};
