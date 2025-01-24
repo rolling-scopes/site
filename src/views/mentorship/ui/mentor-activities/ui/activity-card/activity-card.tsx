7d5cccef8f8d3ebd8e20a9c015ea18c3acab9ebd
ansivgit <ansiv@yandex.ru>
2025-01-24 13:25:41 +0300

    feat: 715 - add activity card

---
+++ src/views/mentorship/ui/mentor-activities/ui/activity-card/activity-card.tsx
@@ -0,0 +1,36 @@
+import classnames from 'classnames/bind';
+import Image from 'next/image';
+
+import { LinkCustom } from '@/shared/ui/link-custom';
+import { Paragraph } from '@/shared/ui/paragraph';
+import { Subtitle } from '@/shared/ui/subtitle';
+import type { MentorActivity } from 'data';
+
+import styles from './activity-card.module.scss';
+
+const cx = classnames.bind(styles);
+
+type ActivityCardProps = Omit<MentorActivity, 'id'>;
+
+export const ActivityCard = ({ title, description, icon, links }: ActivityCardProps) => (
+  <article className={cx('activity-card')} data-testid="activity-card">
+    <div className={cx('icon')}>
+      <div className={cx('icon-accent')}></div>
+      <Image src={icon} alt="" />
+    </div>
+    <Subtitle
+      className={cx('card-title')}
+      fontSize="large"
+      weight="bold"
+      color="black"
+    >
+      {title}
+    </Subtitle>
+    <Paragraph className={cx('card-description')}>{description}</Paragraph>
+    {links?.length && links.map(({ href, linkTitle }) => (
+      <LinkCustom className={cx('gray-link')} href={href} key={linkTitle}>
+        {linkTitle}
+      </LinkCustom>
+    ))}
+  </article>
+);
