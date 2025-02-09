e9addf4ecdcdb1713733887edc0597db14ca133f
ansivgit <ansiv@yandex.ru>
2025-02-09 18:05:29 +0300

    refactor: styles

--- src/widgets/study-path/ui/study-path/study-path.tsx
+++ src/widgets/study-path/ui/study-path/study-path.tsx
@@ -1,4 +1,4 @@
-import classNames from 'classnames/bind';
+import cn from 'classnames';
 
 import { Stages } from '../stages/stages';
 import { Paragraph } from '@/shared/ui/paragraph';
@@ -6,16 +6,12 @@ import { WidgetTitle } from '@/shared/ui/widget-title';
 import type { StagePathPage } from '@/widgets/study-path/types';
 import { studyPath } from 'data';
 
-import styles from './study-path.module.scss';
-
-const cx = classNames.bind(styles);
-
 export const StudyPath = ({ page = 'courses' }: StagePathPage) => {
   const { sectionTitle, sectionIntro, stages } = studyPath[page];
 
   return (
-    <section className={cx('study-path', 'container')} id="study-path">
-      <article className={cx('content')}>
+    <section className={cn('study-path', 'container')} data-testid="study-path">
+      <article className={cn('content')}>
         <WidgetTitle size="small" mods="asterisk">
           {sectionTitle}
         </WidgetTitle>
