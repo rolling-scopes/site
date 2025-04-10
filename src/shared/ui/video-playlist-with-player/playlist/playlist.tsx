cad043b0d4f92e50b4e3648d046ccabcca72786a
Pavel Bodal <pavelbodal@gmail.com>
2025-04-10 10:30:01 +0200

    fix: 742 - add bold style to playlist title

--- src/shared/ui/video-playlist-with-player/playlist/playlist.tsx
+++ src/shared/ui/video-playlist-with-player/playlist/playlist.tsx
@@ -16,12 +16,23 @@ type PlaylistProps = {
   style?: CSSProperties;
 };
 
-export const Playlist = ({ title, videos, onSelectVideo, selectedVideoId, style }: PlaylistProps) => {
+export const Playlist = ({
+  title,
+  videos,
+  onSelectVideo,
+  selectedVideoId,
+  style,
+}: PlaylistProps) => {
   const isSelected = (videoId: string) => videoId === selectedVideoId;
 
   return (
     <div className={cx('playlist')} style={style} data-testid="playlist">
-      <Subtitle fontSize="small" className={cx('playlist-title')} data-testid="playlist-title">
+      <Subtitle
+        fontSize="small"
+        fontWeight="bold"
+        className={cx('playlist-title')}
+        data-testid="playlist-title"
+      >
         {`${videos.length} ${title}`}
       </Subtitle>
       <div className={cx('videos-container')} data-testid="videos-container">
@@ -32,9 +43,19 @@ export const Playlist = ({ title, videos, onSelectVideo, selectedVideoId, style 
             onClick={() => onSelectVideo?.(video)}
             data-testid={`playlist-item-${video.id}`}
           >
-            <div className={cx('playlist-item-thumbnail-container')} data-testid={`thumbnail-container-${video.id}`}>
+            <div
+              className={cx('playlist-item-thumbnail-container')}
+              data-testid={`thumbnail-container-${video.id}`}
+            >
               <img src={video.thumbnail} alt={video.title} data-testid={`thumbnail-${video.id}`} />
-              {isSelected(video.id) && <div className={cx('now-playing-overlay')} data-testid={`now-playing-overlay-${video.id}`}>Now playing</div>}
+              {isSelected(video.id) && (
+                <div
+                  className={cx('now-playing-overlay')}
+                  data-testid={`now-playing-overlay-${video.id}`}
+                >
+                  Now playing
+                </div>
+              )}
             </div>
             <p data-testid={`video-title-${video.id}`}>{video.title}</p>
           </div>
