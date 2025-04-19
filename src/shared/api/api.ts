9c938de36b470e581751743c71c52ed15cc86682
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-04-19 15:14:30 +0300

    refactor: 732 - move api entry to core layer

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,17 +1,3 @@
-import { TrainerApi } from '@/entities/trainer/api/trainer-api';
-import { ApiBaseClass } from '@/shared/api/api-base-class';
-import { ApiServices } from '@/shared/types';
-
-class Api {
-  public readonly services: ApiServices;
-
-  public readonly trainer: TrainerApi;
-
-  constructor(private readonly baseURI: string) {
-    this.services = { rest: new ApiBaseClass(this.baseURI) };
-
-    this.trainer = new TrainerApi(this.services);
-  }
-}
+import { Api } from '@/core/api/app-api';
 
 export const api = new Api(process.env.API_BASE_URL);
