45b757900a4efe11433cdb04e433791a02c6a6ac
YulikK <yulikpoch@gmail.com>
2025-04-30 14:09:50 +0200

    refactor: 754 - format widget-communication.data.ts for readability

--- dev-data/widget-communication.data.ts
+++ dev-data/widget-communication.data.ts
@@ -4,19 +4,15 @@ export const communicationText = {
     subTitle: 'Discord is the main communication channel in RS School',
     subTitleJs: 'RS School uses two main communication channels:',
     firstParagraphFirstHalf: 'Here is link for the ',
-    discordParagraphTextJs:
-      ': Join the Discord server to see the latest news and chat with students.',
+    discordParagraphTextJs: ': Join the Discord server to see the latest news and chat with students.',
     discordLink: 'course Discord server',
     discordLinkJs: 'Discord',
     firstParagraphSecondHalf: ', where you can see latest news and chat with students.',
-    telegramParagraphTextJs:
-      ': You can also join the official Telegram channel for updates and discussions.',
-    discordNote:
-      'Attention! In some countries, access to Discord requires the use of a VPN. If you are having trouble connecting, please try using a reliable VPN service.',
+    telegramParagraphTextJs: ': You can also join the official Telegram channel for updates and discussions.',
+    discordNote: 'Attention! In some countries, access to Discord requires the use of a VPN. If you are having trouble connecting, please try using a reliable VPN service.',
     secondParagraphFirstHalf: 'There are channels in ',
     telegramLink: 'Telegram',
-    secondParagraphSecondHalf:
-      ' for discussing events related to your location. For example, offline lectures or just informal chats among students from the same location.',
+    secondParagraphSecondHalf: ' for discussing events related to your location. For example, offline lectures or just informal chats among students from the same location.',
     thirdParagraphFirstHalf: 'Please read the information about communication in RS School in the ',
     rsDocsLink: 'RS Docs',
     thirdParagraphSecondHalf: ', where you can find rules, descriptions of channels, FAQ.',
@@ -29,15 +25,12 @@ export const communicationText = {
     discordParagraphTextJs: '',
     discordLink: 'Дискорд сервер курса',
     discordLinkJs: '',
-    firstParagraphSecondHalf:
-      ', где вы можете посмотреть последние новости, задать вопросы и общаться со студентами.',
+    firstParagraphSecondHalf: ', где вы можете посмотреть последние новости, задать вопросы и общаться со студентами.',
     telegramParagraphTextJs: '',
-    discordNote:
-      'Внимание! В некоторых странах для доступа к Discord требуется использование VPN. Если у вас возникают трудности с подключением, попробуйте использовать надёжный VPN-сервис.',
+    discordNote: 'Внимание! В некоторых странах для доступа к Discord требуется использование VPN. Если у вас возникают трудности с подключением, попробуйте использовать надёжный VPN-сервис.',
     secondParagraphFirstHalf: 'Также есть каналы в ',
     telegramLink: 'Телеграм',
-    secondParagraphSecondHalf:
-      ' для обсуждения мероприятий, относящихся к вашему городу. Например, офлайн лекции или просто для общения студентов из одной локации.',
+    secondParagraphSecondHalf: ' для обсуждения мероприятий, относящихся к вашему городу. Например, офлайн лекции или просто для общения студентов из одной локации.',
     thirdParagraphFirstHalf: 'Обязательно прочитайте информацию об общении в RS School в ',
     rsDocsLink: 'RS Docs',
     thirdParagraphSecondHalf: ', где вы можете найти правила, описание каналов, FAQ.',
