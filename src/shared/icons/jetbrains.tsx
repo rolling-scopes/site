a98a7d022291a265ab276d92f0626ac26a7c0350
tatyana <beatytanya@gmail.com>
2025-05-07 23:19:03 +0500

    feat: 873 - add link to jetBrains website

--- src/shared/icons/jetbrains.tsx
+++ src/shared/icons/jetbrains.tsx
@@ -3,5 +3,10 @@ import Image from 'next/image';
 import jetbrains from '@/shared/assets/svg/jetbrains.svg';
 
 export const JetBrainsLogo = () => {
-  return <Image src={jetbrains} width={200} alt="jetbrains icon" />;
+  return (
+    <a href="https://www.jetbrains.com" target="_blank" rel="noopener noreferrer">
+      <Image src={jetbrains} width={200} alt="jetbrains icon" />
+      ;
+    </a>
+  );
 };
