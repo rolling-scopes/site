a9a926bbc68f1506fa7d0c689eff5c4c41030e16
tatyana <beatytanya@gmail.com>
2025-05-08 00:38:28 +0500

    feat: 873 - apply grayscale and lower contrast to JetBrains logo

--- src/shared/icons/jetbrains.tsx
+++ src/shared/icons/jetbrains.tsx
@@ -5,7 +5,12 @@ import jetbrains from '@/shared/assets/svg/jetbrains.svg';
 export const JetBrainsLogo = () => {
   return (
     <a href="https://www.jetbrains.com" target="_blank" rel="noopener noreferrer">
-      <Image src={jetbrains} width={200} alt="jetbrains icon" />
+      <Image
+        src={jetbrains}
+        style={{ filter: 'grayscale(100%) contrast(15%)' }}
+        width={200}
+        alt="jetbrains icon"
+      />
     </a>
   );
 };
