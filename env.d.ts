cf950fcf6d820e051f4b5e50a0517e5216e2c79b
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-05-07 22:15:20 +0300

    chore: 871 - remove redundant env variable type

--- env.d.ts
+++ env.d.ts
@@ -4,7 +4,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_API_BASE_URL: string;
     NEXT_PUBLIC_YOUTUBE_API_BASE_URL: string;
     NEXT_PUBLIC_YOUTUBE_API_KEY: string;
-    API_BASE_URL: string;
     LOG_QUERY: string;
   }
 }
