76a1c97a56044988a7ba8d24f91727dc8bcd22e5
tatyana <tatyanali.official@gmail.com>
2025-05-10 15:06:13 +0500

    feat: 873 - add link to aws website

--- src/shared/icons/aws.tsx
+++ src/shared/icons/aws.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image';
 
 import aws from '@/shared/assets/icons/aws-gray.svg';
+import { LinkCustom } from '@/shared/ui/link-custom';
 
 export const AwsLogo = () => {
-  return <Image src={aws} alt="AWS icon" />;
+  return (
+    <LinkCustom
+      href="https://aws.amazon.com/"
+      external
+      icon={null}
+    >
+      <Image src={aws} alt="AWS icon" />
+    </LinkCustom>
+  );
 };
