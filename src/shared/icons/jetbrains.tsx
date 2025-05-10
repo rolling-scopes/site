9d501c1bb7633f4bd3bcc15994fa270b1b2f70e2
tatyana <tatyanali.official@gmail.com>
2025-05-10 14:58:16 +0500

    refactor: 873 - use link-custom component

--- src/shared/icons/jetbrains.tsx
+++ src/shared/icons/jetbrains.tsx
@@ -1,16 +1,21 @@
 import Image from 'next/image';
 
 import jetbrains from '@/shared/assets/svg/jetbrains.svg';
+import { LinkCustom } from '@/shared/ui/link-custom';
 
 export const JetBrainsLogo = () => {
   return (
-    <a href="https://www.jetbrains.com" target="_blank" rel="noopener noreferrer">
+    <LinkCustom
+      href="https://www.jetbrains.com"
+      external
+      icon={null}
+    >
       <Image
         src={jetbrains}
         style={{ filter: 'grayscale(100%) contrast(15%)' }}
         width={200}
         alt="jetbrains icon"
       />
-    </a>
+    </LinkCustom>
   );
 };
