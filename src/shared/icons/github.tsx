39b478d9d20801c4940ec1cb99024fd9088f1961
tatyana <tatyanali.official@gmail.com>
2025-05-10 15:11:03 +0500

    feat: 873 - add link to github website

--- src/shared/icons/github.tsx
+++ src/shared/icons/github.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image';
 
 import github from '@/shared/assets/svg/github.svg';
+import { LinkCustom } from '@/shared/ui/link-custom';
 
 export const GithubLogo = () => {
-  return <Image src={github} alt=" github icon" />;
+  return (
+    <LinkCustom
+      href="https://github.com/"
+      external
+      icon={null}
+    >
+      <Image src={github} alt=" github icon" />
+    </LinkCustom>
+  );
 };
