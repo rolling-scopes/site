37959214c0f0c08f1b60320e8a961c8c8e88ab45
HelenaIsh <elena-ishmatova@mail.ru>
2025-05-13 10:51:17 +0200

    refactor: 887 - refactor partnered module

--- src/widgets/partnered/ui/partnered.test.tsx
+++ src/widgets/partnered/ui/partnered.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { Partnered } from '.';
+import { Partnered } from './partnered';
 
 describe('Partnered', () => {
   it('should render Partnered component', () => {
