04a8d357ab268964568a1183af873254cf61cec6
HelenaIsh <elena-ishmatova@mail.ru>
2025-05-15 11:24:17 +0200

    feat: 887 - add meaningful tests for partnered

--- src/shared/icons/github.tsx
+++ src/shared/icons/github.tsx
@@ -3,5 +3,5 @@ import Image from 'next/image';
 import github from '@/shared/assets/svg/github.svg';
 
 export const GithubLogo = () => {
-  return <Image src={github} alt=" github icon" />;
+  return <Image src={github} alt="github icon" />;
 };
--- src/widgets/partnered/ui/partnered.test.tsx
+++ src/widgets/partnered/ui/partnered.test.tsx
@@ -1,13 +1,29 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import { Partnered } from './partnered';
+import aws from '@/shared/assets/icons/aws-gray.svg';
+import github from '@/shared/assets/svg/github.svg';
+import jetbrains from '@/shared/assets/svg/jetbrains.svg';
 
-describe('Partnered', () => {
-  it('should render Partnered component', () => {
-    const { getByTestId } = render(<Partnered />);
-    const partnered = getByTestId('partnered');
+describe('Partnered component', () => {
+  it('should render the component with correct title', () => {
+    render(<Partnered />);
 
-    expect(partnered).toBeInTheDocument();
+    expect(screen.getByText('Partnered with')).toBeInTheDocument();
+
+    expect(screen.getByTestId('widget-title')).toBeInTheDocument();
+  });
+
+  it('renders the title and logos correctly', () => {
+    render(<Partnered />);
+
+    const logos = screen.getAllByRole('img');
+
+    expect(logos).toHaveLength(3);
+
+    expect(screen.getByAltText('jetbrains icon')).toHaveAttribute('src', jetbrains.src);
+    expect(screen.getByAltText('AWS icon')).toHaveAttribute('src', aws.src);
+    expect(screen.getByAltText('github icon')).toHaveAttribute('src', github.src);
   });
 });
