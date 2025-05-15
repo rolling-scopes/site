181b67c37dfdb95073ca77d496414963f06845de
Larisa Novozhilova <kara4516@gmail.com>
2025-05-15 21:29:28 +0300

    refactor: 390 - replace if-else with ternary for date assignment

--- scripts/utils/course-info.ts
+++ scripts/utils/course-info.ts
@@ -3,15 +3,11 @@ import dayjs from 'dayjs';
 import type { ApiCourse } from '../types';
 
 export const getCourseInfo = (coursesList: ApiCourse[], slug: string): string => {
-  const courseInfo: ApiCourse | undefined = coursesList.find((c) => c.descriptionUrl.toLowerCase().endsWith(slug));
+  const courseInfo: ApiCourse | undefined = coursesList.find((c) =>
+    c.descriptionUrl.toLowerCase().endsWith(slug),
+  );
 
-  let rawDate: string;
-
-  if (!courseInfo) {
-    rawDate = '';
-  } else {
-    rawDate = courseInfo.startDate;
-  }
+  const rawDate: string = courseInfo ? courseInfo.startDate : '';
 
   const formattedDate: string = rawDate ? dayjs(rawDate).format('MMM DD, YYYY') : 'TBD';
 
