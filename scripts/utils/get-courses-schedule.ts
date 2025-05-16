687abec2786ecf90be9b9b2d959b8854b2586afa
Larisa Novozhilova <kara4516@gmail.com>
2025-05-16 21:23:43 +0300

    refactor: 390 - enhance schedule data with name and formatted dates

--- scripts/utils/get-courses-schedule.ts
+++ scripts/utils/get-courses-schedule.ts
@@ -1,13 +1,22 @@
+import dayjs from 'dayjs';
+
 import { CoursesScheduleResponse } from '../../src/entities/course/types';
 import { api } from '../../src/shared/api/api';
 
-export async function getCoursesSchedule() {
+export async function getCoursesSchedule(): Promise<
+  {
+    name: string;
+    startDate: string;
+    descriptionUrl: string;
+  }[]
+> {
   try {
     const response = await api.course.queryCoursesSchedule();
     const courses = response.result as CoursesScheduleResponse;
 
     return courses.map((course) => ({
-      startDate: course.startDate || '',
+      name: course.name || '',
+      startDate: dayjs(course.startDate).format('MMM DD, YYYY') || '',
       descriptionUrl: course.descriptionUrl || '',
     }));
   } catch (error) {
