9c7d0ec7a1426b052dfd0d598ab6df8b55033382
Larisa Novozhilova <kara4516@gmail.com>
2025-05-17 16:20:30 +0300

    refactor: 390 - add error handling for file operations

--- scripts/utils/load-fonts.ts
+++ scripts/utils/load-fonts.ts
@@ -11,13 +11,19 @@ export type Font = {
 export const loadFont = async (weight: 400 | 700): Promise<Font> => {
   const fileName = weight === 700 ? 'Inter_28pt-Bold.ttf' : 'Inter_28pt-Regular.ttf';
   const fontPath = path.join(process.cwd(), 'public', 'fonts', fileName);
-  const buffer = await fs.readFile(fontPath);
-  const arrayBuffer = new Uint8Array(buffer).buffer;
 
-  return {
-    name: 'Inter',
-    data: arrayBuffer,
-    weight,
-    style: 'normal',
-  };
+  try {
+    const buffer = await fs.readFile(fontPath);
+    const arrayBuffer = new Uint8Array(buffer).buffer;
+
+    return {
+      name: 'Inter',
+      data: arrayBuffer,
+      weight,
+      style: 'normal',
+    };
+  } catch (error) {
+    console.error(`Error loading font (${fileName}):`, error);
+    throw new Error(`Failed to load font: ${fileName}`);
+  }
 };
