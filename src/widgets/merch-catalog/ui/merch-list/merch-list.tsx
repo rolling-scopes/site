8c91805381ac59ebbbce782d9d96f46e783acec7
TeymurDev <teymur.ff@gmail.com>
2025-05-20 10:27:24 +0400

    feat: 787 - implement pure pagination

--- src/widgets/merch-catalog/ui/merch-list/merch-list.tsx
+++ src/widgets/merch-catalog/ui/merch-list/merch-list.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 
 import { MerchItem } from '../merch-item/merch-item';
@@ -10,12 +12,41 @@ const cx = classNames.bind(styles);
 export type MerchListProps = {
   products: MerchProduct[];
 };
-export const MerchList = ({ products }: MerchListProps) => (
-  <div className={cx('wrapper')}>
-    <div className={cx('list')}>
-      {products.map((product) => (
-        <MerchItem key={product.id} {...product} />
-      ))}
+
+const ITEMS_PER_PAGE = 10;
+
+export const MerchList = ({ products }: MerchListProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentProducts = products.slice(startIndex, endIndex);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('list')}>
+        {currentProducts.map((product) => (
+          <MerchItem key={product.id} {...product} />
+        ))}
+      </div>
+      {totalPages > 1 && (
+        <div className={cx('pagination')}>
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+            <button
+              key={page}
+              className={cx('pagination-button', { active: currentPage === page })}
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
