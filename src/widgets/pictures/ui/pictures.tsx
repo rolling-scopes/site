adeb7d33d9508e96385eaff557b6abf5d72a6eb6
gbogdanova <gaaalo44ka@gmail.com>
2025-05-23 12:03:52 -0700

    refactor: 877 - replace Community carousel with Slider component

--- src/widgets/pictures/ui/pictures.tsx
+++ src/widgets/pictures/ui/pictures.tsx
@@ -2,9 +2,9 @@
 
 import classNames from 'classnames/bind';
 import Image from 'next/image';
-import { Carousel } from 'react-responsive-carousel';
 
 import { Paragraph } from '@/shared/ui/paragraph';
+import { Slider } from '@/shared/ui/slider';
 import { SocialMediaItem } from '@/shared/ui/social-media-item';
 import { WidgetTitle } from '@/shared/ui/widget-title';
 import { picturesSocialMediaLinks, sliderPhotos } from 'data';
@@ -13,44 +13,56 @@ import styles from './pictures.module.scss';
 
 const cx = classNames.bind(styles);
 
-export const Pictures = () => (
-  <section className={cx('container')}>
-    <div className={cx('content')}>
-      <WidgetTitle mods="asterisk">The Rolling Scopes in pictures</WidgetTitle>
-      <Carousel
-        className={cx('carousel')}
-        showArrows={true}
-        showThumbs={false}
-        showStatus={false}
-        centerMode={true}
-        emulateTouch={true}
-        autoPlay={true}
-        transitionTime={1000}
-        useKeyboardArrows={true}
-        infiniteLoop={true}
-        renderIndicator={(onClickHandler, isSelected, index) => (
-          <li
-            className={cx('dot', { selected: isSelected })}
-            onClick={onClickHandler}
-            value={index}
-            key={index}
-            tabIndex={0}
-          />
-        )}
-      >
-        {sliderPhotos.map(({ id, src, alt }) => (
-          <Image key={id} src={src} alt={alt} data-testid="carousel-image" />
-        ))}
-      </Carousel>
-      <Paragraph>
-        Want to see photos of our community? A vast collection of photographs from our events is
-        available on our Facebook Albums and Instagram pages.
-      </Paragraph>
-      <div className={cx('social-media-container')}>
-        {picturesSocialMediaLinks.map(({ title, href, icon }) => (
-          <SocialMediaItem key={title} title={title} href={href} icon={icon} />
-        ))}
-      </div>
+export const Pictures = () => {
+  const slides = sliderPhotos.map(({ id, src, alt }) => (
+    <div
+      key={id}
+      style={{
+        display: 'block',
+        width: '100%',
+        aspectRatio: '8/5',
+        position: 'relative',
+      }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        data-testid="carousel-image"
+        fill
+      />
     </div>
-  </section>
-);
+  ));
+
+  return (
+    <section className={cx('container')}>
+      <div className={cx('content')}>
+        <WidgetTitle mods="asterisk">The Rolling Scopes in pictures</WidgetTitle>
+        <Slider
+          slides={slides}
+          sliderProps={{
+            navigation: true,
+            loop: true,
+            centeredSlides: true,
+            keyboard: { enabled: true },
+            speed: 1000,
+            slidesPerView: 1.25,
+            spaceBetween: 10,
+            autoplay: {
+              delay: 3000,
+              pauseOnMouseEnter: true,
+            },
+          }}
+        />
+        <Paragraph>
+          Want to see photos of our community? A vast collection of photographs from our events is
+          available on our Facebook Albums and Instagram pages.
+        </Paragraph>
+        <div className={cx('social-media-container')}>
+          {picturesSocialMediaLinks.map(({ title, href, icon }) => (
+            <SocialMediaItem key={title} title={title} href={href} icon={icon} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
