9c298b592e8b67668fa1fecfdb45f441a4e13256
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-05-28 22:25:46 +0300

    refactor: get rid of get course title helper

--- src/shared/helpers/get-course-title.ts
+++
@@ -1,8 +0,0 @@
-import { selectCourse } from '../hooks/use-course-by-title/utils/select-course';
-import { Course } from '@/entities/course';
-
-export async function getCourseTitle(courseName: Course['title']) {
-  const course = await selectCourse(courseName);
-
-  return `${course.title} · The Rolling Scopes School`;
-}
