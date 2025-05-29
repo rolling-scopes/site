8c97e499ab1905cb83ae95c7004ebbe293e65325
Larisa Novozhilova <kara4516@gmail.com>
2025-05-29 09:45:02 +0300

    refactor: 390 - clean up code formatting

--- src/shared/og/view/pages-tree/generate-pages-tree.tsx
+++ src/shared/og/view/pages-tree/generate-pages-tree.tsx
@@ -22,34 +22,26 @@ export async function createPageTree({
     mapBgPromise,
   ]);
 
-  return new ImageResponse(
-    (
-      <div
-        style={stylesPageTree.container}
-      >
-        <img src={mapBgUri} alt="RS Logo" style={stylesPageTree.mapBg} />
-
-        <div
-          style={stylesPageTree.content}
-        >
-          <h1 style={stylesPageTree.title}>{title}</h1>
-          <p style={stylesPageTree.description}>{description}</p>
-        </div>
-
-        <div
-          style={stylesPageTree.mascotsContainer}
-        >
-          <img src={rsMascotsUri} alt="RS mascots" style={stylesPageTree.mascots} />
-        </div>
-
-        <div style={stylesPageTree.yellowBar} />
-
-        <img src={rsBannerUri} alt="RS Logo" style={stylesPageTree.banner} />
+  return new ImageResponse((
+    <div style={stylesPageTree.container}>
+      <img src={mapBgUri} alt="RS Logo" style={stylesPageTree.mapBg} />
+      <div style={stylesPageTree.content}>
+        <h1 style={stylesPageTree.title}>{title}</h1>
+        <p style={stylesPageTree.description}>{description}</p>
       </div>
-    ), {
-      width: 1200,
-      height: 630,
-      fonts,
-    },
+
+      <div style={stylesPageTree.mascotsContainer}>
+        <img src={rsMascotsUri} alt="RS mascots" style={stylesPageTree.mascots} />
+      </div>
+
+      <div style={stylesPageTree.yellowBar} />
+
+      <img src={rsBannerUri} alt="RS Logo" style={stylesPageTree.banner} />
+    </div>
+  ), {
+    width: 1200,
+    height: 630,
+    fonts,
+  },
   );
 }
