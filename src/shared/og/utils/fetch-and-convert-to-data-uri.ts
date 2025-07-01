7eb4b6e5014b522ab4e7a42d7ed972b1818d5157
Larisa Novozhilova <kara4516@gmail.com>
2025-07-01 21:35:00 +0300

    refactor: 390 - simplify function fetchAndConvertToDataUri

--- src/shared/og/utils/fetch-and-convert-to-data-uri.ts
+++ src/shared/og/utils/fetch-and-convert-to-data-uri.ts
@@ -1,6 +1,6 @@
 import sharp from 'sharp';
 
-export async function fetchAndConvertToDataUri(url: string): Promise<string> {
+export const fetchAndConvertToDataUri = async (url: string): Promise<string> => {
   try {
     const res = await fetch(url);
 
@@ -18,11 +18,9 @@ export async function fetchAndConvertToDataUri(url: string): Promise<string> {
       })
       .toBuffer();
 
-    const base64 = pngBuffer.toString('base64');
-
-    return `data:image/png;base64,${base64}`;
+    return `data:image/png;base64,${pngBuffer.toString('base64')}`;
   } catch (err) {
     console.error(`[fetchAndConvertToDataUri] Error for URL "${url}":`, err);
     throw err;
   }
-}
+};
