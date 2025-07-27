77ff5ac9b0e64c366e1cc70322e526f9d97694ca
YulikK <yulikpoch@gmail.com>
2025-07-27 14:21:22 +0200

    feat: 786 - improve MerchStore to cache loaded merch catalog for better performance

--- src/entities/merch/model/store.ts
+++ src/entities/merch/model/store.ts
@@ -1,12 +1,19 @@
 import { transformMerchCatalog } from '../helpers/transform-merch-catalog';
+import { MerchProduct } from '../types';
 import { api } from '@/shared/api/api';
 
 class MerchStore {
+  private cachedProducts: MerchProduct[] | null = null;
   public loadMerchCatalog = async () => {
+    if (this.cachedProducts) {
+      return this.cachedProducts;
+    }
+
     const res = await api.merch.queryMerchCatalog();
 
     if (res.isSuccess) {
-      return transformMerchCatalog(res.result);
+      this.cachedProducts = transformMerchCatalog(res.result);
+      return this.cachedProducts;
     }
 
     throw new Error('Error while loading merch catalog.');
