c28168c3a63a75c64e867af571431062560c50ec
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-07-27 22:46:57 +0300

    feat: 905 - add breadcrumbs to home page

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,7 +1,11 @@
+import { Fragment } from 'react';
+
 import { courseStore } from '@/entities/course';
 import { LANDING_PAGE_SLUG } from '@/entities/landing-page/constants';
 import { landingPageStore } from '@/entities/landing-page/model/store';
+import { Breadcrumbs } from '@/widgets/breadcrumbs';
 import { SectionResolver } from '@/widgets/section-resolver';
+import { SECTION_TYPE } from '@/widgets/section-resolver/constants';
 
 export const Home = async () => {
   const [{ sections }, courses] = await Promise.all([
@@ -10,11 +14,9 @@ export const Home = async () => {
   ]);
 
   return sections.map((section, index) => (
-    <SectionResolver
-      courses={courses}
-      anchorId={index.toString()}
-      key={section.id}
-      section={section}
-    />
+    <Fragment key={section.id}>
+      <SectionResolver courses={courses} anchorId={index.toString()} section={section} />
+      {section.name === SECTION_TYPE.HERO && <Breadcrumbs />}
+    </Fragment>
   ));
 };
