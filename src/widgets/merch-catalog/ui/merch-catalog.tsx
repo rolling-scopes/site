2a942b9654ba098dc64374699858985b06be4571
TeymurDev <teymur.ff@gmail.com>
2025-08-05 22:09:28 +0400

    fix: 787 - rename component

--- src/widgets/merch-catalog/ui/merch-catalog.tsx
+++ src/widgets/merch-catalog/ui/merch-catalog.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import classNames from 'classnames/bind';
 
-import { Loading } from './loading/loading';
+import { Loader } from './loader/loader';
 import { MerchList } from './merch-list/merch-list';
 import { getMerchData } from '@/entities/merch/api/merch-api';
 
@@ -15,7 +15,7 @@ export const MerchCatalog = async () => {
   return (
     <section className={cx('container')}>
       <div className={cx('content', 'merch-catalog')}>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loader />}>
           <MerchList products={products} />
         </Suspense>
       </div>
