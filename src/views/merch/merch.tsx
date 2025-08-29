79784becc02a9f078f7d8976d3dff173cbaba59b
TeymurDev <teymur.ff@gmail.com>
2025-08-29 23:25:12 +0400

    fix: 787 - merch suspense

--- src/views/merch/merch.tsx
+++ src/views/merch/merch.tsx
@@ -4,13 +4,14 @@ import welcome from '@/shared/assets/welcome.webp';
 import { Breadcrumbs } from '@/widgets/breadcrumbs';
 import { Hero } from '@/widgets/hero';
 import { MerchCatalog } from '@/widgets/merch-catalog';
+import { Loader } from '@/widgets/merch-catalog/ui/loader/loader';
 
 export const Merch = async () => {
   return (
     <>
-      <Suspense>
-        <Hero heading="Merch" subHeading="Free assets for your design" image={welcome} />
-        <Breadcrumbs />
+      <Hero heading="Merch" subHeading="Free assets for your design" image={welcome} />
+      <Breadcrumbs />
+      <Suspense fallback={<Loader />}>
         <MerchCatalog />
       </Suspense>
     </>
