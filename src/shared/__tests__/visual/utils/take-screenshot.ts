01381fa50c84e0e009f77d1d662b153872ebeb54
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-09-30 11:13:19 +0300

    fix: 956 - playwright tests

--- src/shared/__tests__/visual/utils/take-screenshot.ts
+++ src/shared/__tests__/visual/utils/take-screenshot.ts
@@ -3,8 +3,7 @@ import percyScreenshot from '@percy/playwright';
 import { Page } from '@playwright/test';
 
 export async function takeScreenshot(page: Page, name: string, options?: SnapshotOptions) {
-  await page.waitForLoadState('networkidle');
-  await page.waitForLoadState('domcontentloaded');
+  await Promise.all([page.waitForLoadState('load'), page.waitForLoadState('domcontentloaded')]);
 
   await percyScreenshot(page, name, options);
 }
