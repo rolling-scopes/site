323c5d88791a737282b785b86bbee64511e85f47
antonina220590 <tonya2290@gmail.com>
2025-10-13 18:45:26 +0300

    refactor: 788 - move loader component to shared components

--- src/views/merch.tsx
+++ src/views/merch.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 
 import welcome from '@/shared/assets/welcome.webp';
+import { Loader } from '@/shared/ui/loader/loader';
 import { Breadcrumbs } from '@/widgets/breadcrumbs';
 import { Hero } from '@/widgets/hero';
 import { MerchSection } from '@/widgets/merch-section';
-import { Loader } from '@/widgets/merch-section/ui/loader/loader';
 
 export const Merch = async () => {
   return (
