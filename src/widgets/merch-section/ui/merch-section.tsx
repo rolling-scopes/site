99c07d50badab102e4b07351f8fc7d13d4e0c24d
antonina220590 <tonya2290@gmail.com>
2025-10-15 08:48:55 +0300

    refactor: 788 - delete unused class from merchSection component

--- src/widgets/merch-section/ui/merch-section.tsx
+++ src/widgets/merch-section/ui/merch-section.tsx
@@ -1,22 +1,16 @@
-import classNames from 'classnames/bind';
-
 import { MerchCatalog } from './merch-catalog/merch-catalog';
 import NoMerch from './merch-catalog/no-merch/no-merch';
 import { MerchProduct, merchStore } from '@/entities/merch';
-
-import styles from './merch-section.module.scss';
-
-const cx = classNames.bind(styles);
 
 export const MerchSection = async () => {
   const products: MerchProduct[] | [] = await merchStore.loadMerchCatalog();
 
   return (
-    <section className={cx('container')}>
+    <section className="container">
       {(!products || products.length === 0) && <NoMerch />}
 
       {products?.length > 0 && (
-        <div className={cx('content', 'merch-catalog')}>
+        <div className="content">
           <MerchCatalog initialProducts={products} />
         </div>
       )}
