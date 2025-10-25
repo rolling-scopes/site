93bc7353ab1d58676b521f9459cb66c68518e933
antonina220590 <tonya2290@gmail.com>
2025-10-25 11:24:25 +0300

    refactor: 788 - update tests for MerchTags component and use constants

--- src/widgets/merch-catalog/ui/merch-filter/merch-tags/merch-tags.test.tsx
+++ src/widgets/merch-catalog/ui/merch-filter/merch-tags/merch-tags.test.tsx
@@ -3,13 +3,15 @@ import userEvent from '@testing-library/user-event';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { MerchTags } from './merch-tags';
-import { MOCKED_ROUTER, MOCKED_TAGS } from '@/shared/__tests__/constants';
+import { MOCKED_TAGS } from '@/shared/__tests__/constants';
+import { ROUTES, URL_PARAMS } from '@/shared/constants';
 
 let mockedSearchParams: URLSearchParams = new URLSearchParams();
+const MOCKED_ROUTER = { replace: vi.fn() };
 
 vi.mock('next/navigation', () => ({
   useRouter: () => MOCKED_ROUTER,
-  usePathname: () => '/merch',
+  usePathname: () => ROUTES.MERCH,
   useSearchParams: () => mockedSearchParams,
 }));
 
@@ -29,7 +31,7 @@ describe('MerchTags', () => {
   });
 
   it('should check the correct checkbox based on a  URL "type" param', () => {
-    mockedSearchParams = new URLSearchParams('type=t-shirts');
+    mockedSearchParams = new URLSearchParams(`${URL_PARAMS.TYPE}=t-shirts`);
     render(<MerchTags tags={MOCKED_TAGS} />);
     expect(screen.getByLabelText('t-shirts')).toBeChecked();
     expect(screen.getByLabelText('hoodie')).not.toBeChecked();
@@ -41,30 +43,39 @@ describe('MerchTags', () => {
     render(<MerchTags tags={MOCKED_TAGS} />);
     await user.click(screen.getByLabelText('hoodie'));
     expect(MOCKED_ROUTER.replace).toHaveBeenCalledTimes(1);
-    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith('/merch?type=hoodie', { scroll: false });
+    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith(
+      `${ROUTES.MERCH}?${URL_PARAMS.TYPE}=hoodie`,
+      { scroll: false },
+    );
   });
 
   it('should remove the type parameter from URL when a checked checkbox is clicked', async () => {
-    mockedSearchParams = new URLSearchParams('type=hoodie');
+    mockedSearchParams = new URLSearchParams(`${URL_PARAMS.TYPE}=hoodie`);
     render(<MerchTags tags={MOCKED_TAGS} />);
     await user.click(screen.getByLabelText('hoodie'));
     expect(MOCKED_ROUTER.replace).toHaveBeenCalledTimes(1);
-    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith('/merch', { scroll: false });
+    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith(`${ROUTES.MERCH}`, { scroll: false });
   });
 
   it('should correctly remove one of multiple type parameters from URL', async () => {
-    mockedSearchParams = new URLSearchParams('type=t-shirts&type=cups');
+    mockedSearchParams = new URLSearchParams(`${URL_PARAMS.TYPE}=t-shirts&${URL_PARAMS.TYPE}=cups`);
     render(<MerchTags tags={MOCKED_TAGS} />);
     await user.click(screen.getByLabelText('t-shirts'));
     expect(MOCKED_ROUTER.replace).toHaveBeenCalledTimes(1);
-    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith('/merch?type=cups', { scroll: false });
+    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith(
+      `${ROUTES.MERCH}?${URL_PARAMS.TYPE}=cups`,
+      { scroll: false },
+    );
   });
 
   it('should remove the "page" parameter from URL when a filter is changed', async () => {
-    mockedSearchParams = new URLSearchParams('type=hoodie&page=3');
+    mockedSearchParams = new URLSearchParams(`${URL_PARAMS.TYPE}=hoodie&${URL_PARAMS.PAGE}=3`);
     render(<MerchTags tags={MOCKED_TAGS} />);
     await user.click(screen.getByLabelText('cups'));
     expect(MOCKED_ROUTER.replace).toHaveBeenCalledTimes(1);
-    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith('/merch?type=hoodie&type=cups', { scroll: false });
+    expect(MOCKED_ROUTER.replace).toHaveBeenCalledExactlyOnceWith(
+      `${ROUTES.MERCH}?${URL_PARAMS.TYPE}=hoodie&${URL_PARAMS.TYPE}=cups`,
+      { scroll: false },
+    );
   });
 });
--- src/widgets/merch-catalog/ui/merch-filter/merch-tags/merch-tags.tsx
+++ src/widgets/merch-catalog/ui/merch-filter/merch-tags/merch-tags.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { ReadonlyURLSearchParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
 
+import { URL_PARAMS } from '@/shared/constants';
 import { Checkbox } from '@/shared/ui/checkbox/index';
 
 import styles from './merch-tags.module.scss';
@@ -17,21 +18,21 @@ export const MerchTags = ({ tags }: MerchTagsProps) => {
   const pathname: string = usePathname();
   const searchParams: ReadonlyURLSearchParams = useSearchParams();
 
-  const selectedTypes: string[] = searchParams.getAll('type');
+  const selectedTypes: string[] = searchParams.getAll(URL_PARAMS.TYPE);
 
   const handleTypeChange = (typeValue: string) => {
     const params: URLSearchParams = new URLSearchParams(searchParams);
 
-    params.delete('page');
+    params.delete(URL_PARAMS.PAGE);
 
     const newSelectedTypes: string[] = selectedTypes.includes(typeValue)
       ? selectedTypes.filter((type) => type !== typeValue)
       : [...selectedTypes, typeValue];
 
-    params.delete('type');
+    params.delete(URL_PARAMS.TYPE);
 
     newSelectedTypes.forEach((type) => {
-      params.append('type', type);
+      params.append(URL_PARAMS.TYPE, type);
     });
 
     const newQuery: string = params.toString();
