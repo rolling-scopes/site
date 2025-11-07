260328d1b534cefb8fc947d790a0330413e82b89
Dzmitry Yarmoshkin <spanb4@gmail.com>
2025-11-07 23:39:06 +0300

    chore: return ts extension for next.config back

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { NextConfig } from 'next';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'export',
   distDir: './build',
   images: { unoptimized: true },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src')],
     additionalData: `
       @use '@/core/styles/constants' as *;
       @use '@/core/styles/mixins' as *;
